d46cd7b442b6ac0108f5304d271de7c098538990
Eric Reinecke <ereinecke@pixsystem.com>
2016-10-11 17:09:38 -0700

    PEP 8 pass

--- tests/test_builtin_adapters.py
+++ tests/test_builtin_adapters.py
@@ -18,6 +18,7 @@ SAMPLE_DATA_DIR = os.path.join(os.path.dirname(__file__), "sample_data")
 SCREENING_EXAMPLE_PATH = os.path.join(SAMPLE_DATA_DIR, "screening_example.edl")
 HLS_EXAMPLE_PATH = os.path.join(SAMPLE_DATA_DIR, "v1_prog_index.m3u8")
 
+
 class BuiltInAdapterTest(unittest.TestCase):
 
     def test_disk_io(self):
@@ -72,19 +73,19 @@ class HLSPlaylistAdapterTest(unittest.TestCase):
     def test_hls_attribute_list(self):
         """ Test the HLS adapter's attribute list parser """
         attribute_list_string = 'HEXTHING=0xFAF,FIRST-NEGFLOAT=-1.25,'\
-                'STRTHING="foo, bar",DECIMALTHING=123456,FLOATTHING=1.233,'\
-                'ENUMTHING=0xeS1,NEGFLOAT=-3.14'
+            'STRTHING="foo, bar",DECIMALTHING=123456,FLOATTHING=1.233,'\
+            'ENUMTHING=0xeS1,NEGFLOAT=-3.14'
         attribute_list_dictionary = {
-                "HEXTHING": 0xFAF,
-                "FIRST-NEGFLOAT": -1.25,
-                "STRTHING": "foo, bar",
-                "DECIMALTHING": 123456,
-                "FLOATTHING": 1.233,
-                "ENUMTHING": hls_playlist.AttributeListEnum("0xeS1"),
-                "NEGFLOAT": -3.14
-                }
+            "HEXTHING": 0xFAF,
+            "FIRST-NEGFLOAT": -1.25,
+            "STRTHING": "foo, bar",
+            "DECIMALTHING": 123456,
+            "FLOATTHING": 1.233,
+            "ENUMTHING": hls_playlist.AttributeListEnum("0xeS1"),
+            "NEGFLOAT": -3.14
+        }
         attr_list = hls_playlist.AttributeList.from_string(
-                attribute_list_string
+            attribute_list_string
         )
 
         self.assertEquals(len(attr_list), len(attribute_list_dictionary))
@@ -94,18 +95,18 @@ class HLSPlaylistAdapterTest(unittest.TestCase):
         # test converting the dict to a string
         # TODO: this doesn't work because order is not preserved
         # self.assertEquals(str(attr_list), attribute_list_string)
-    
+
     def test_playlist_tag_exclusivity(self):
         """ Test that mutually-exclusive tag types don't overlap """
-        # see sections 4.3.2, 4.3.3, and 4.3.4 of 
+        # see sections 4.3.2, 4.3.3, and 4.3.4 of
         # draft-pantos-http-live-streaming for more information about these
         # constraints
 
         non_master_tags = hls_playlist.MEDIA_SEGMENT_TAGS.union(
-                hls_playlist.MEDIA_PLAYLIST_TAGS)
-        
+            hls_playlist.MEDIA_PLAYLIST_TAGS)
+
         common_tags = non_master_tags.intersection(
-                hls_playlist.MASTER_PLAYLIST_TAGS)
+            hls_playlist.MASTER_PLAYLIST_TAGS)
         self.assertEquals(len(common_tags), 0)
 
     def test_hls_playlist(self):

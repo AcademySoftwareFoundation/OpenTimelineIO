f09d8ed4c2b3c32c54f2d60dbd4af5a9085a2284
Stephan Steinbach <steinbach@pixar.com>
2016-10-25 17:23:26 -0700

    set_parent -> _set_parent

--- opentimelineio/core/item.py
+++ opentimelineio/core/item.py
@@ -145,8 +145,11 @@ class Item(serializeable_object.SerializeableObject):
             str(self.metadata)
         )
 
-    def set_parent(self, new_parent):
-        if self._parent is not None and self in self._parent:
+    def _set_parent(self, new_parent):
+        if self._parent is not None and (
+            hasattr(self._parent, "remove") and 
+            self in self._parent
+        ):
             self._parent.remove(self)
 
         self._parent = new_parent

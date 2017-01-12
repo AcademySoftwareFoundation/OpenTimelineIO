7b68b25fa51eaff80f8650615eba4cf3043a11f5
Eric Reinecke <ereinecke@pixsystem.com>
2017-01-12 13:46:31 -0800

    Extended HLS unittests to be more in-depth

--- contrib/adapters/tests/test_hls_playlist_adapter.py
+++ contrib/adapters/tests/test_hls_playlist_adapter.py
@@ -1,5 +1,6 @@
 import json
 import os
+import tempfile
 import unittest
 
 import opentimelineio as otio
@@ -32,6 +33,18 @@ otio.adapters.MANIFEST.adapters.append(
 
 # Load the adapter module using otio
 hls_playlist = otio.adapters.from_name("hls_playlist").module()
+
+MEM_PLAYLIST_REF_VALUE = '''#EXTM3U
+#EXT-X-VERSION:7
+#EXT-X-TARGETDURATION:2
+#EXT-X-PLAYLIST-TYPE:VOD
+#EXT-X-INDEPENDENT-SEGMENTS
+#EXT-X-MEDIA-SEQUENCE:1
+#EXT-X-MAP:BYTERANGE="729@0",URI="media-video-1.mp4"
+#EXTINF:2.00200,
+#EXT-X-BYTERANGE:534220@1361
+video1.mp4
+#EXT-X-ENDLIST'''
 
 
 class HLSPlaylistDataStructuresTest(unittest.TestCase):
@@ -76,9 +89,154 @@ class HLSPlaylistDataStructuresTest(unittest.TestCase):
 class HLSPlaylistAdapterTest(unittest.TestCase):
     """ Test the HLS Playlist adapter itself """
 
-    def test_hls_playlist(self):
+    def test_media_pl_from_mem(self):
+        t = otio.schema.Timeline()
+        track = otio.schema.Sequence("v1")
+        track.metadata['HLS'] = {
+            "EXT-X-INDEPENDENT-SEGMENTS": None,
+            "EXT-X-PLAYLIST-TYPE": "VOD"
+        }
+        t.tracks.append(track)
+
+        # Make a prototype media ref with the segment's initialization metadata
+        segmented_media_ref = otio.media_reference.External(
+            target_url='video1.mp4',
+            metadata={
+                "init_byterange": {
+                    "byte_count": 729,
+                    "byte_offset": 0
+                },
+                "init_uri": "media-video-1.mp4"
+            }
+        )
+
+        # Make a copy of the media ref specifying the byte range for the
+        # segment
+        media_ref1 = segmented_media_ref.copy()
+        media_ref1.available_range = otio.opentime.TimeRange(
+            otio.opentime.RationalTime(0, 1),
+            otio.opentime.RationalTime(2.002, 1)
+        )
+        media_ref1.metadata.update(
+            {
+                "byte_count": 534220,
+                "byte_offset": 1361
+            }
+        )
+
+        # make the segment and append it
+        segment1 = otio.schema.Clip(media_reference=media_ref1)
+        track.append(segment1)
+
+        # Write out and validate the playlist
+        media_pl_tmp_path = tempfile.mkstemp(suffix=".m3u8", text=True)[1]
+        otio.adapters.write_to_file(t, media_pl_tmp_path)
+
+        with open(media_pl_tmp_path) as f:
+            pl_string = f.read()
+
+        os.remove(media_pl_tmp_path)
+
+        self.assertEqual(pl_string, MEM_PLAYLIST_REF_VALUE)
+
+    def _validate_sample_playlist(self, timeline):
+        # Validate the track count
+        self.assertEqual(len(timeline.tracks), 1)
+        track = timeline.tracks[0]
+
+        # Validate the track global metadata
+        self.assertNotEqual(track.metadata['HLS'], {})
+        track_metadata = track.metadata['HLS']
+        self.assertEqual(track_metadata, {
+            'EXT-X-INDEPENDENT-SEGMENTS': None,
+            'EXT-X-VERSION': '7',
+            'EXT-X-PLAYLIST-TYPE': 'VOD'}
+        )
+
+        # There are 50 segments (clips)
+        # Validate the count, "sequence_num", and durations
+        self.assertEqual(len(track), 50)
+        start_seq_num = int(track[0].metadata['sequence_num'])
+        segment_durations = otio.opentime.RationalTime(1.001, 1)
+        for seq_num, clip in enumerate(track, start_seq_num):
+            self.assertEqual(clip.metadata['sequence_num'], seq_num)
+            if seq_num < 50:
+                self.assertEqual(clip.duration(), segment_durations)
+            else:
+                # The last segment has a shorter duration
+                self.assertEqual(
+                    clip.duration(),
+                    otio.opentime.RationalTime(0.83417, 1)
+                )
+
+        # Spot-check a segment
+        segment_5 = track[4]
+        segment_5_media_ref = segment_5.media_reference
+        self.assertEqual(
+            segment_5_media_ref.metadata['byte_count'],
+            593718
+        )
+        self.assertEqual(
+            segment_5_media_ref.metadata['byte_offset'],
+            2430668
+        )
+        self.assertEqual(
+            segment_5_media_ref.metadata['init_byterange']['byte_count'],
+            729
+        )
+        self.assertEqual(
+            segment_5_media_ref.metadata['init_byterange']['byte_offset'],
+            0
+        )
+        self.assertEqual(
+            segment_5_media_ref.metadata['init_uri'],
+            "media-video-1.mp4"
+        )
+        self.assertEqual(
+            segment_5_media_ref.target_url,
+            "media-video-1.mp4"
+        )
+
+    def test_media_roundtrip(self):
         hls_path = HLS_EXAMPLE_PATH
         timeline = otio.adapters.read_from_file(hls_path)
+
+        # validate the read-in playlist matches reference data
+        self._validate_sample_playlist(timeline)
+
+        # Write out and validate both playlists have the same lines
+        media_pl_tmp_path = tempfile.mkstemp(suffix=".m3u8", text=True)[1]
+        otio.adapters.write_to_file(timeline, media_pl_tmp_path)
+
+        # Read in both playlists
+        with open(hls_path) as f:
+            reference_lines = f.readlines()
+
+        with open(media_pl_tmp_path) as f:
+            adapter_out_lines = f.readlines()
+
+        # Using otio as well
+        in_timeline = otio.adapters.read_from_file(media_pl_tmp_path)
+
+        # Remove the temp out file
+        os.remove(media_pl_tmp_path)
+
+        # Strip newline chars
+        reference_lines = [l.strip('\n') for l in reference_lines]
+        adapter_out_lines = [l.strip('\n') for l in adapter_out_lines]
+
+        # while imperfect, do a superficial comparison
+        self.assertEqual(len(reference_lines), len(adapter_out_lines))
+        diff_lines = set(reference_lines).symmetric_difference(
+            adapter_out_lines
+        )
+        # ignore the EXT-X-MAP entry because attribute order is
+        # non-deterministic
+        diff_lines = [l for l in diff_lines if not l.startswith('#EXT-X-MAP')]
+        self.assertEqual(len(diff_lines), 0)
+
+        # validate the otio of the playlist we wrote
+        self._validate_sample_playlist(in_timeline)
 
 if __name__ == '__main__':
     unittest.main()

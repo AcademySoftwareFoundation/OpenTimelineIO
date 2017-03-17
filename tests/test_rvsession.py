8538f68d28d02353ccd8f8fc7c5c5697687d31ca
Stephan Steinbach <steinbach@pixar.com>
2017-03-17 08:47:59 -0700

    Add baseline comparison to rv unit test.

--- tests/test_rvsession.py
+++ tests/test_rvsession.py
@@ -8,23 +8,26 @@ import opentimelineio as otio
 
 SAMPLE_DATA_DIR = os.path.join(os.path.dirname(__file__), "sample_data")
 SCREENING_EXAMPLE_PATH = os.path.join(SAMPLE_DATA_DIR, "screening_example.edl")
+BASELINE_PATH = os.path.join(SAMPLE_DATA_DIR, "screening_example.rv")
 
 
 @unittest.skipIf(
-    "OTIO_RV_PYTHON_DIR" not in os.environ or
+    "OTIO_RV_PYTHON_LIB" not in os.environ or
     "OTIO_RV_PYTHON_BIN" not in os.environ,
-    "OTIO_RV_PYTHON_BIN or OTIO_RV_PYTHON_DIR not set."
+    "OTIO_RV_PYTHON_BIN or OTIO_RV_PYTHON_LIB not set."
 )
 class RVSessionAdapterReadTest(unittest.TestCase):
     def test_basic_rvsession_read(self):
         timeline = otio.adapters.read_from_file(SCREENING_EXAMPLE_PATH)
         tmp_path = tempfile.mkstemp(suffix=".rv", text=True)[1]
-        try:
-            otio.adapters.write_to_file(timeline, tmp_path)
 
-            self.assertTrue(os.path.exists(tmp_path))
-            with open(tmp_path) as fo:
-                stuff = fo.read()
-                self.assertTrue(stuff.startswith("GTO"))
-        except ImportError:
-            pass
+        otio.adapters.write_to_file(timeline, tmp_path)
+        self.assertTrue(os.path.exists(tmp_path))
+
+        with open(tmp_path) as fo:
+            test_data = fo.read()
+
+        with open(BASELINE_PATH) as fo:
+            baseline_data = fo.read()
+
+        self.assertMultiLineEqual(baseline_data, test_data)

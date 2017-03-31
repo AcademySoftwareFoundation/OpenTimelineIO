2e380e46cf16a418523f01aaf0870704673aed1e
ssteinbach <ssteinbach@github.com>
2017-03-31 14:42:45 -0700

    Use the () syntax for import.

--- opentimelineio/__init__.py
+++ opentimelineio/__init__.py
@@ -6,9 +6,12 @@
 # flake8: noqa
 
 # in dependency hierarchy
-from . import opentime
-from . import exceptions
-from . import core
-from . import media_reference
-from . import schema
-from . import adapters
+from . import (
+    opentime,
+    exceptions,
+    core,
+    media_reference,
+    schema,
+    adapters,
+    utilities,
+)

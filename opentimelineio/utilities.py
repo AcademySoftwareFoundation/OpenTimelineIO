bad980514fd7a0ca1dd180212fb5ee334961d0d2
ssteinbach <ssteinbach@github.com>
2017-03-31 14:45:51 -0700

    Add utilities library.
    Adds function for expanding transitions + unit test.

---
+++ opentimelineio/utilities.py
@@ -0,0 +1,128 @@
+"""
+Utility functions for OTIO
+"""
+
+import copy
+
+from . import (
+    adapters,
+    schema,
+)
+
+def sequence_with_expanded_transitions(in_seq):
+    """
+    Expands transitions such that neighboring clips are trimmed into regions of
+    overlap.  For example, if your sequence is:
+        Clip1, T, Clip2
+
+    will return:
+        Clip1', Clip1_t, T, Clip2_t, Clip2'
+
+    """
+
+    sequence_to_modify = copy.deepcopy(in_seq)
+
+    # we want to copy all the top level parameters and settings
+    result_sequence = []
+
+    iterable = iter(sequence_to_modify)
+    prev_thing = None
+    thing = next(iterable, None)
+    next_thing = next(iterable, None)
+
+    while thing is not None:
+        if isinstance(thing, schema.Transition):
+            expanded_trx = _expand_transition(thing, sequence_to_modify)
+            adapters.write_to_file(expanded_trx, "/var/tmp/expanded_transition.otio")
+            result_sequence.append(expanded_trx)
+        else:
+            # not a transition, but might be trimmed by one coming up
+            pre_transition = None
+            next_transition = None
+
+            if isinstance(prev_thing, schema.Transition):
+                pre_transition = prev_thing
+
+            if isinstance(next_thing, schema.Transition):
+                next_transition = next_thing
+
+            result_sequence.append(
+                _trim_from_transitions(
+                    thing,
+                    pre=pre_transition,
+                    post=next_transition
+                )
+            )
+        prev_thing = thing
+        thing = next_thing
+        next_thing = next(iterable, None)
+
+    return result_sequence
+
+
+def _expand_transition(transition, from_sequence):
+    result = from_sequence.neighbors_of(
+        transition,
+        schema.NeighborFillerPolicy.around_transitions
+    )
+
+    trx_duration = transition.in_offset + transition.out_offset
+
+    # make copies of the before and after, and modify their in/out points
+    pre = copy.deepcopy(result[0])
+    pre.name = pre.name + "_transition_pre"
+    if not pre.source_range:
+        # @TODO: a method to create a default source range?
+        pre.source_range = copy.deepcopy(pre.media_reference.available_range)
+
+    if transition.in_offset is None:
+        raise RuntimeError("in_offset is None on: {}".format(transition))
+
+    if transition.out_offset is None:
+        raise RuntimeError("in_offset is None on: {}".format(transition))
+
+    pre.source_range.start_time = (
+        pre.source_range.end_time_exclusive() 
+        - transition.in_offset
+    )
+    pre.source_range.duration = trx_duration.rescaled_to(
+        pre.source_range.start_time
+    )
+
+    post = copy.deepcopy(result[2])
+    post.name = post.name + "_transition_post"
+    if not post.source_range:
+        post.source_range = copy.deepcopy(post.media_reference.available_range)
+
+    post.source_range.start_time = (
+        post.source_range.start_time - transition.in_offset
+    ).rescaled_to(post.source_range.start_time)
+    post.source_range.duration = trx_duration.rescaled_to(
+        post.source_range.start_time
+    )
+
+    return (pre, transition, post)
+
+
+def _trim_from_transitions(thing, pre=None, post=None):
+    result = copy.deepcopy(thing)
+
+    if not result.source_range:
+        result.source_range = copy.deepcopy(
+            result.media_reference.available_range
+        )
+
+    if pre:
+        result.source_range.start_time = (
+            result.source_range.start_time + pre.out_offset
+        )
+        result.source_range.duration = (
+            result.source_range.duration - pre.out_offset
+        )
+
+    if post:
+        result.source_range.duration = (
+            result.source_range.duration - post.in_offset
+        )
+
+    return result
---
+++ tests/test_utilities.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+import unittest
+import copy
+
+import opentimelineio as otio
+
+# for debugging
+# def print_expanded_tree(seq):
+#     for thing in seq:
+#         if isinstance(thing, tuple):
+#             for i in thing:
+#                 print "  ", i
+#         else:
+#             print thing
+
+class TimelineTests(unittest.TestCase):
+
+    def test_expand_transitions(self):
+        name = "test"
+        rt = otio.opentime.RationalTime(5, 24)
+        rt_2 = otio.opentime.RationalTime(1, 24)
+        tr = otio.opentime.TimeRange(rt, rt + rt)
+        avail_tr = otio.opentime.TimeRange(otio.opentime.RationalTime(0, 24), otio.opentime.RationalTime(50, 24))
+        mr = otio.media_reference.External(
+            available_range = avail_tr,
+            target_url = "/var/tmp/test.mov"
+        )
+
+        cl = otio.schema.Clip(
+            name=name + "_pre",
+            media_reference=mr,
+            source_range = tr,
+        )
+
+        seq = otio.schema.Sequence()
+        seq.append(cl)
+        in_offset = rt
+        out_offset = rt_2
+        trx = otio.schema.Transition(
+            name="AtoB",
+            transition_type="SMPTE.Dissolve",
+            parameters={
+                "amount" : 0.5
+            },
+            in_offset = in_offset,
+            out_offset = out_offset,
+            metadata={
+                "foo" : "bar"
+            }
+        )
+        seq.append(trx)
+        cl_2 = copy.deepcopy(cl)
+        cl_2.name = name + "_post"
+        seq.append(copy.copy(cl))
+
+        pre_duration = copy.deepcopy(seq[0].source_range.duration)
+
+        # print
+        # print "BEFORE:"
+        # print_expanded_tree(seq)
+        seq2 = otio.utilities.sequence_with_expanded_transitions(seq)
+        # print
+        # print "AFTER:"
+        # print_expanded_tree(seq2)
+
+        self.assertNotEquals(pre_duration, seq2[0].source_range.duration)
+
+        # check ranges on results
+
+        # first clip is trimmed
+        self.assertEquals(
+            seq2[0].source_range.duration,
+            cl_2.source_range.duration - in_offset
+        )
+
+        self.assertEquals(
+            seq2[1][0].source_range.start_time,
+            cl.source_range.end_time_exclusive() - in_offset
+        )
+        self.assertEquals(
+            seq2[1][0].source_range.duration,
+            in_offset + out_offset
+        )
+        self.assertEquals(
+            seq2[1][2].source_range.start_time,
+            cl_2.source_range.start_time - in_offset
+        )
+        self.assertEquals(
+            seq2[1][2].source_range.duration,
+            in_offset + out_offset
+        )
+
+        # final clip is trimmed
+        self.assertEquals(
+            seq2[2].source_range.duration,
+            cl_2.source_range.duration - out_offset
+        )
+
+        # make sure that both transition clips are the same length
+        self.assertEquals(
+            seq2[1][0].source_range.duration,
+            seq2[1][2].source_range.duration
+        )
+
+        # big hammer stuff
+        self.assertNotEquals(seq, seq2)
+        self.assertNotEquals(seq[0], seq2[0])
+        self.assertNotEquals(seq[-1], seq2[-1])
+        self.assertNotEquals(seq[-1].source_range, seq2[-1].source_range)
+

5dfd2cb5e74f2172b7f842dc18eb9ae309a7b6d9
ssteinbach <ssteinbach@github.com>
2017-06-26 15:20:12 -0700

    Add item to the type registry in unit tests.
    Since core.Item is no longer directly serializeable, for unit testing, add it to the registry at the top of the file
    
    Related: have the serializeableContainer tests use Clip instead.

--- tests/test_item.py
+++ tests/test_item.py
@@ -7,6 +7,10 @@ import unittest
 import opentimelineio as otio
 
 
+# add Item to the type registry for the purposes of unit testing
+otio.core.register_type(otio.core.Item)
+
+
 class FillerTester(unittest.TestCase):
 
     def test_str_filler(self):
@@ -161,7 +165,8 @@ class ItemTests(unittest.TestCase):
 
     def test_metadata(self):
         tr = otio.opentime.TimeRange(
-            duration=otio.opentime.RationalTime(10, 1))
+            duration=otio.opentime.RationalTime(10, 1)
+        )
         it = otio.core.Item(source_range=tr)
         it.metadata["foo"] = "bar"
         encoded = otio.adapters.otio_json.write_to_string(it)

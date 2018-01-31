a2a4621587d70da2c97ff7942a511fadbad5fc6c
ssteinbach <ssteinbach@users.noreply.github.com>
2018-01-30 17:24:18 -0800

    First piece of range_in

---
+++ opentimelineio/algorithms/time_transforms.py
@@ -0,0 +1,47 @@
+#
+# Copyright 2018 Pixar Animation Studios
+#
+# Licensed under the Apache License, Version 2.0 (the "Apache License")
+# with the following modification; you may not use this file except in
+# compliance with the Apache License and the following modification to it:
+# Section 6. Trademarks. is deleted and replaced with:
+#
+# 6. Trademarks. This License does not grant permission to use the trade
+#    names, trademarks, service marks, or product names of the Licensor
+#    and its affiliates, except as required to comply with Section 4(c) of
+#    the License and to reproduce the content of the NOTICE file.
+#
+# You may obtain a copy of the Apache License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Apache License with the above modification is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the Apache License for the specific
+# language governing permissions and limitations under the Apache License.
+#
+
+__doc__ = """Implementation of functions for query time."""
+
+from .. import (
+    opentime
+)
+
+def range_of(
+    item,
+    in_scope=None, # must be a parent or child of item
+    trimmed_to=None, # must be a parent of item
+    # with_transitions=False, #todo make this an enum
+):
+    range_in_item_space = item.trimmed_range()
+    if trimmed_to is not None and trimmed_to is not item:
+        range_in_item_space = _trim_to(item, trimmed_to)
+
+    if in_scope is None or in_scope is item:
+        return range_in_item_space
+
+    return _transform_range(range_in_item_space, item, in_scope)
+
+
+
---
+++ tests/test_time_transforms_algo.py
@@ -0,0 +1,75 @@
+#
+# Copyright 2018 Pixar Animation Studios
+#
+# Licensed under the Apache License, Version 2.0 (the "Apache License")
+# with the following modification; you may not use this file except in
+# compliance with the Apache License and the following modification to it:
+# Section 6. Trademarks. is deleted and replaced with:
+#
+# 6. Trademarks. This License does not grant permission to use the trade
+#    names, trademarks, service marks, or product names of the Licensor
+#    and its affiliates, except as required to comply with Section 4(c) of
+#    the License and to reproduce the content of the NOTICE file.
+#
+# You may obtain a copy of the Apache License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Apache License with the above modification is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the Apache License for the specific
+# language governing permissions and limitations under the Apache License.
+#
+
+import unittest
+
+import opentimelineio as otio
+
+__doc__ = """Test range_of function from algorithms."""
+
+class RangeInTests(unittest.TestCase):
+    def test_source_range(self):
+        """Test using range_of to query for clip space."""
+
+        tr = otio.schema.Track()
+        rn = otio.opentime.TimeRange(
+            otio.opentime.RationalTime(0, 24),
+            otio.opentime.RationalTime(10, 24),
+        )
+        src_range = otio.opentime.TimeRange(
+            otio.opentime.RationalTime(5, 24),
+            otio.opentime.RationalTime(3, 24),
+        )
+        mr_1 = otio.schema.ExternalReference(available_range=rn)
+        mr_2 = otio.schema.ExternalReference(available_range=rn)
+        tr.append(
+            otio.schema.Clip(
+                name='cl1',
+                source_range=src_range,
+                media_reference=mr_1
+            )
+        )
+        tr.append(
+            otio.schema.Clip(
+                name='cl2',
+                source_range=src_range,
+                media_reference=mr_2
+            )
+        )
+        cl_1 = tr[0]
+        cl_2 = tr[1]
+
+        result = otio.range_of(
+            cl_1,
+            in_scope=cl_1,
+            trimmed_to=cl_1,
+            # include_transitions=True,
+        )
+
+        self.assertEqual(cl_1.source_range, otio.range_of(cl_1))
+        self.assertEqual(result, otio.range_of(cl_1))
+
+
+if __name__ == '__main__':
+    unittest.main()

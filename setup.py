bf6b9926102886073049d65cb323f6c7130b094b
Alex Widener <alexwidener@gmail.com>
2018-07-22 20:15:44 -0700

    Update build for Python 3.6

--- setup.py
+++ setup.py
@@ -100,6 +100,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Operating System :: OS Independent',
         'Natural Language :: English',
     ],

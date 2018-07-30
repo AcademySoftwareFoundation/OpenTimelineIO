05ba32e25bae4d4d60a44da9afdbe3261cab6ae4
Alex Widener <alexwidener@users.noreply.github.com>
2018-07-30 16:53:47 -0700

    Updated build to include Python 3.6 (#291)

--- setup.py
+++ setup.py
@@ -146,6 +146,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Operating System :: OS Independent',
         'Natural Language :: English',
     ],

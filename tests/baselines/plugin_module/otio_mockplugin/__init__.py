49033334cf6e0bcf46227330cc9ffe0545f54ba2
Stephan Steinbach <steinbach@pixar.com>
2018-10-09 17:16:17 -0700

    Make sure its a string before deserializing it.

--- tests/baselines/plugin_module/otio_mockplugin/__init__.py
+++ tests/baselines/plugin_module/otio_mockplugin/__init__.py
@@ -5,5 +5,10 @@ from opentimelineio.plugins import manifest
 
 def plugin_manifest():
     return manifest.manifest_from_string(
-        pkg_resources.resource_string(__name__, 'unusually_named_plugin_manifest.json')
+        str(
+            pkg_resources.resource_string(
+                __name__,
+                'unusually_named_plugin_manifest.json'
+            )
+        )
     )

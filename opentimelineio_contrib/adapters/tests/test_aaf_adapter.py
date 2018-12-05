061eb45f94d27a6e6df0e4c04299f3c0d6a17472
Freeson Wang <freeson@pixar.com>
2018-12-05 15:08:40 -0800

    Test should use aaf2 instead of aaf

--- opentimelineio_contrib/adapters/tests/test_aaf_adapter.py
+++ opentimelineio_contrib/adapters/tests/test_aaf_adapter.py
@@ -85,8 +85,8 @@ ESSENCE_GROUP_PATH = os.path.join(
 try:
     lib_path = os.environ.get("OTIO_AAF_PYTHON_LIB")
     if lib_path and lib_path not in sys.path:
-        sys.path += [lib_path]
-    import aaf # noqa
+        sys.path.insert(0, lib_path)
+    import aaf2 # noqa
     could_import_aaf = True
 except (ImportError):
     could_import_aaf = False

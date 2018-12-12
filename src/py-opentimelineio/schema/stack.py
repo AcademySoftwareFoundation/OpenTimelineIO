5ad2e219656e26c992f58324813e92e0d368ba44
David Baraff <nosuch@nowhere>
2018-12-12 12:02:10 -0800

    python methods for stack, track

---
+++ src/py-opentimelineio/schema/stack.py
@@ -0,0 +1,9 @@
+from opentimelineio.core._core_utils import add_method
+from opentimelineio import _otio
+
+@add_method(_otio.Stack)
+def each_clip(self, search_range=None):
+    return self.each_child(search_range, _otio.Clip)
+
+
+

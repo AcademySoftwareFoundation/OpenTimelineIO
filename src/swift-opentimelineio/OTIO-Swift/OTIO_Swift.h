5e486f4bb94c22817486d8e123a7f6465a8dad84
Julian Yu-Chung Chen <jchen@pixar.com>
2019-04-26 14:21:12 -0700

    Built OTIO_Swift.framework

---
+++ src/swift-opentimelineio/OTIO-Swift/OTIO_Swift.h
@@ -0,0 +1,20 @@
+//
+//  OTIO_Swift.h
+//  OTIO-Swift
+//
+//  Created by Julian Yu-Chung Chen on 4/26/19.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OTIO_Swift.
+FOUNDATION_EXPORT double OTIO_SwiftVersionNumber;
+
+//! Project version string for OTIO_Swift.
+FOUNDATION_EXPORT const unsigned char OTIO_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTIO_Swift/PublicHeader.h>
+
+#import "errorStruct.h"
+#import "opentime.h"
+#import "opentimelineio.h"

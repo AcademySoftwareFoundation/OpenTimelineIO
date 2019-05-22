39a64c2dfecd6b96d80bb4b2852e6410d37fe7cb
Freeson Wang <freeson@pixar.com>
2019-05-22 16:15:09 -0700

    Adding a test for just gaps.otio file

--- opentimelineio_contrib/adapters/tests/test_aaf_adapter.py
+++ opentimelineio_contrib/adapters/tests/test_aaf_adapter.py
@@ -126,7 +126,10 @@ MULTIPLE_TOP_LEVEL_MOBS_CLIP_PATH = os.path.join(
     SAMPLE_DATA_DIR,
     "multiple_top_level_mobs.aaf"
 )
-
+GAPS_OTIO_PATH = os.path.join(
+    SAMPLE_DATA_DIR,
+    "gaps.otio"
+)
 
 try:
     lib_path = os.environ.get("OTIO_AAF_PYTHON_LIB")
@@ -807,6 +810,12 @@ class AAFReaderTests(unittest.TestCase):
 
 
 class AAFWriterTests(unittest.TestCase):
+    def test_aaf_writer_gaps(self):
+        otio_timeline = otio.adapters.read_from_file(GAPS_OTIO_PATH)
+        fd, tmp_aaf_path = tempfile.mkstemp(suffix='.aaf')
+        otio.adapters.write_to_file(otio_timeline, tmp_aaf_path)
+        self._verify_aaf(tmp_aaf_path)
+
     def test_aaf_writer_simple(self):
         self._verify_aaf(SIMPLE_EXAMPLE_PATH)
 

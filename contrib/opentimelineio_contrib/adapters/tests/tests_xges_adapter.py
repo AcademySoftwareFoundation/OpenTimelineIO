0ed36f07c312795c01f98ed21ab0924829706db8
Thibault Saunier <tsaunier@igalia.com>
2020-03-10 16:18:47 -0300

    Updating Copyright notices
    Fixes https://github.com/PixarAnimationStudios/OpenTimelineIO/issues/657

--- contrib/opentimelineio_contrib/adapters/tests/tests_xges_adapter.py
+++ contrib/opentimelineio_contrib/adapters/tests/tests_xges_adapter.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 Igalia S.L
+# Copyright Contributors to the OpenTimelineIO project
 #
 # Licensed under the Apache License, Version 2.0 (the "Apache License")
 # with the following modification; you may not use this file except in

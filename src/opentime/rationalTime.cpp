765d3c6336b2f8a3606c46740072b1ba125ba76a
Stephan Steinbach <61017+ssteinbach@users.noreply.github.com>
2020-03-13 15:20:02 -0700

    Timecode rate is ignored (#612)
    * Fix bug as written
    
    Co-authored-by: ssteinbach <ssteinbach@users.noreply.github.com>

--- src/opentime/rationalTime.cpp
+++ src/opentime/rationalTime.cpp
@@ -193,8 +193,10 @@ RationalTime::to_timecode(
 ) const {
 
     *error_status = ErrorStatus();
+
+    double frames_in_target_rate = this->value_rescaled_to(rate);
     
-    if (_value < 0) {
+    if (frames_in_target_rate < 0) {
         *error_status = ErrorStatus(ErrorStatus::NEGATIVE_VALUE);
         return std::string();
     }
@@ -252,7 +254,7 @@ RationalTime::to_timecode(
             (std::round(rate) * 60) - dropframes);
 
     // If the number of frames is more than 24 hours, roll over clock
-    double value = std::fmod(_value, frames_per_24_hours);
+    double value = std::fmod(frames_in_target_rate, frames_per_24_hours);
 
     if (rate_is_dropframe) {
         int ten_minute_chunks = static_cast<int>(std::floor(value/frames_per_10_minutes));

37f5b45e19755ff15450f5cebd033513878a6254
Vincent Pinon <vpinon@kde.org>
2020-03-15 23:58:25 +0100

    fix #656 (copyright)

--- contrib/opentimelineio_contrib/adapters/kdenlive.py
+++ contrib/opentimelineio_contrib/adapters/kdenlive.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 Kdenlive developers <kdenlive@kde.org>
+# Copyright Contributors to the OpenTimelineIO project
 #
 # Licensed under the Apache License, Version 2.0 (the "Apache License")
 # with the following modification; you may not use this file except in

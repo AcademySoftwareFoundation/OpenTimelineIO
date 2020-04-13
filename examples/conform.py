841de4e8fd1900ad4324b07debe1dc5d6f44c9de
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2020-04-13 17:09:55 -0400

    Add List of Supported Formats to Conform.py Help Text (#676)
    * Add list of supported adapters to --help for the conform.py example script.

--- examples/conform.py
+++ examples/conform.py
@@ -58,8 +58,8 @@ def parse_args():
         'input',
         type=str,
         required=True,
-        help='Timeline file(s) to read. Any format supported by OTIO will'
-        ' work.'
+        help='Timeline file(s) to read. Supported formats: {adapters}'
+             ''.format(adapters=otio.adapters.available_adapter_names())
     )
     parser.add_argument(
         '-f',
@@ -127,7 +127,7 @@ def _conform_timeline(timeline, folder):
         # if we found one, then relink to the new path
         clip.media_reference = otio.schema.ExternalReference(
             target_url="file://" + new_path,
-            available_range=None    # we don't know the available range
+            available_range=None  # we don't know the available range
         )
         count += 1
 

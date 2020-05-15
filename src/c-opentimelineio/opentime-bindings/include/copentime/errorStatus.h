ad682e4fc2f1bc82936a8f260a782419c7a0ecd3
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-05-16 00:34:36 +0530

    Reformat files

--- src/c-opentimelineio/opentime-bindings/include/copentime/errorStatus.h
+++ src/c-opentimelineio/opentime-bindings/include/copentime/errorStatus.h
@@ -1,27 +1,29 @@
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 #include <stdbool.h>
-struct ErrorStatus;
-typedef struct ErrorStatus ErrorStatus;
+    struct ErrorStatus;
+    typedef struct ErrorStatus ErrorStatus;
 
-
-typedef enum {
-    OK = 0,
-    INVALID_TIMECODE_RATE = 1,
-    NON_DROPFRAME_RATE = 2,
-    INVALID_TIMECODE_STRING = 3,
-    INVALID_TIME_STRING = 4,
-    TIMECODE_RATE_MISMATCH = 5,
-    NEGATIVE_VALUE = 6,
-    INVALID_RATE_FOR_DROP_FRAME_TIMECODE = 7,
-} Outcome_;
-typedef int Outcome;
-ErrorStatus *ErrorStatus_create();
-ErrorStatus *ErrorStatus_create_with_outcome(Outcome in_outcome);
-ErrorStatus *ErrorStatus_create_with_outcome_and_details(Outcome in_outcome, const char *in_details);
-const char *ErrorStatus_outcome_to_string(ErrorStatus *self, Outcome var1);
-void ErrorStatus_destroy(ErrorStatus *self);
+    typedef enum
+    {
+        OK                                   = 0,
+        INVALID_TIMECODE_RATE                = 1,
+        NON_DROPFRAME_RATE                   = 2,
+        INVALID_TIMECODE_STRING              = 3,
+        INVALID_TIME_STRING                  = 4,
+        TIMECODE_RATE_MISMATCH               = 5,
+        NEGATIVE_VALUE                       = 6,
+        INVALID_RATE_FOR_DROP_FRAME_TIMECODE = 7,
+    } Outcome_;
+    typedef int  Outcome;
+    ErrorStatus* ErrorStatus_create ();
+    ErrorStatus* ErrorStatus_create_with_outcome (Outcome in_outcome);
+    ErrorStatus* ErrorStatus_create_with_outcome_and_details (
+        Outcome in_outcome, const char* in_details);
+    const char* ErrorStatus_outcome_to_string (ErrorStatus* self, Outcome var1);
+    void        ErrorStatus_destroy (ErrorStatus* self);
 #ifdef __cplusplus
 }
 #endif

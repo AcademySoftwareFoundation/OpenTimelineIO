c7bdd34f1c98248f0c5babd3f7d97c8ad0dcdb57
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-06-15 01:58:41 +0530

    Wrap inherited functions in clip

--- src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/clip.h
+++ src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/clip.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include "anyDictionary.h"
+#include "composition.h"
 #include "copentime/timeRange.h"
+#include "effectRetainerVector.h"
 #include "errorStatus.h"
+#include "item.h"
+#include "markerRetainerVector.h"
 #include "mediaReference.h"
 #include <stdbool.h>
 
@@ -21,6 +25,29 @@ extern "C"
     void Clip_set_media_reference(Clip* self, MediaReference* media_reference);
     MediaReference* Clip_media_reference(Clip* self);
     TimeRange* Clip_available_range(Clip* self, OTIOErrorStatus* error_status);
+    TimeRange* Clip_source_range(Clip* self);
+    void       Clip_set_source_range(Clip* self, TimeRange* source_range);
+    EffectRetainerVector* Clip_effects(Clip* self);
+    MarkerRetainerVector* Clip_markers(Clip* self);
+    RationalTime* Clip_duration(Clip* self, OTIOErrorStatus* error_status);
+    TimeRange*    Clip_trimmed_range(Clip* self, OTIOErrorStatus* error_status);
+    TimeRange*    Clip_visible_range(Clip* self, OTIOErrorStatus* error_status);
+    TimeRange*
+               Clip_trimmed_range_in_parent(Clip* self, OTIOErrorStatus* error_status);
+    TimeRange* Clip_range_in_parent(Clip* self, OTIOErrorStatus* error_status);
+    RationalTime* Clip_transformed_time(
+        Clip*            self,
+        RationalTime*    time,
+        Item*            to_item,
+        OTIOErrorStatus* error_status);
+    TimeRange* Clip_transformed_time_range(
+        Clip*            self,
+        TimeRange*       time_range,
+        Item*            to_item,
+        OTIOErrorStatus* error_status);
+    _Bool        Clip_visible(Clip* self);
+    _Bool        Clip_overlapping(Clip* self);
+    Composition* Clip_parent(Clip* self);
 #ifdef __cplusplus
 }
 #endif
--- src/c-opentimelineio/opentimelineio-bindings/src/clip.cpp
+++ src/c-opentimelineio/opentimelineio-bindings/src/clip.cpp
@@ -1,4 +1,6 @@
 #include "copentimelineio/clip.h"
+#include <copentimelineio/composable.h>
+#include <copentimelineio/item.h>
 #include <opentime/timeRange.h>
 #include <opentimelineio/anyDictionary.h>
 #include <opentimelineio/clip.h>
@@ -52,6 +54,69 @@ extern "C"
             reinterpret_cast<OTIO_NS::Clip*>(self)->available_range(
                 reinterpret_cast<OTIO_NS::ErrorStatus*>(error_status));
         return reinterpret_cast<TimeRange*>(new opentime::TimeRange(timeRange));
+    }
+    TimeRange* Clip_source_range(Clip* self)
+    {
+        return Item_source_range((Item*) self);
+    }
+    void Clip_set_source_range(Clip* self, TimeRange* source_range)
+    {
+        Item_set_source_range((Item*) self, source_range);
+    }
+    EffectRetainerVector* Clip_effects(Clip* self)
+    {
+        return Item_effects((Item*) self);
+    }
+    MarkerRetainerVector* Clip_markers(Clip* self)
+    {
+        return Item_markers((Item*) self);
+    }
+    RationalTime* Clip_duration(Clip* self, OTIOErrorStatus* error_status)
+    {
+        return Item_duration((Item*) self, error_status);
+    }
+    TimeRange* Clip_trimmed_range(Clip* self, OTIOErrorStatus* error_status)
+    {
+        return Item_trimmed_range((Item*) self, error_status);
+    }
+    TimeRange* Clip_visible_range(Clip* self, OTIOErrorStatus* error_status)
+    {
+        return Item_visible_range((Item*) self, error_status);
+    }
+    TimeRange*
+    Clip_trimmed_range_in_parent(Clip* self, OTIOErrorStatus* error_status)
+    {
+        return Item_trimmed_range_in_parent((Item*) self, error_status);
+    }
+    TimeRange* Clip_range_in_parent(Clip* self, OTIOErrorStatus* error_status)
+    {
+        return Item_range_in_parent((Item*) self, error_status);
+    }
+    RationalTime* Clip_transformed_time(
+        Clip*            self,
+        RationalTime*    time,
+        Item*            to_item,
+        OTIOErrorStatus* error_status)
+    {
+        return Item_transformed_time((Item*) self, time, to_item, error_status);
+    }
+    TimeRange* Clip_transformed_time_range(
+        Clip*            self,
+        TimeRange*       time_range,
+        Item*            to_item,
+        OTIOErrorStatus* error_status)
+    {
+        return Item_transformed_time_range(
+            (Item*) self, time_range, to_item, error_status);
+    }
+    _Bool Clip_visible(Clip* self) { return Item_visible((Item*) self); }
+    _Bool Clip_overlapping(Clip* self)
+    {
+        return Item_overlapping((Item*) self);
+    }
+    Composition* Clip_parent(Clip* self)
+    {
+        return Composable_parent((Composable*) self);
     }
 #ifdef __cplusplus
 }

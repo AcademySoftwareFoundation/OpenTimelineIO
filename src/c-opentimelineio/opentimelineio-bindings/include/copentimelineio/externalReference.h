3bdaf88e8e2048e7c7e1b11c37e3198217243314
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-06-21 02:17:21 +0530

    Wrap inherited methods in ExternalReference

--- src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/externalReference.h
+++ src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/externalReference.h
@@ -2,6 +2,7 @@
 
 #include "anyDictionary.h"
 #include "copentime/timeRange.h"
+#include "errorStatus.h"
 #include <stdbool.h>
 
 #ifdef __cplusplus
@@ -17,6 +18,27 @@ extern "C"
     const char* ExternalReference_target_url(ExternalReference* self);
     void        ExternalReference_set_target_url(
                ExternalReference* self, const char* target_url);
+    TimeRange* ExternalReference_available_range(ExternalReference* self);
+    void       ExternalReference_set_available_range(
+              ExternalReference* self, TimeRange* available_range);
+    _Bool       ExternalReference_is_missing_reference(ExternalReference* self);
+    const char* ExternalReference_name(ExternalReference* self);
+    void ExternalReference_set_name(ExternalReference* self, const char* name);
+    AnyDictionary* ExternalReference_metadata(ExternalReference* self);
+    _Bool          ExternalReference_possibly_delete(ExternalReference* self);
+    _Bool          ExternalReference_to_json_file(
+                 ExternalReference* self,
+                 const char*        file_name,
+                 OTIOErrorStatus*   error_status,
+                 int                indent);
+    const char* ExternalReference_to_json_string(
+        ExternalReference* self, OTIOErrorStatus* error_status, int indent);
+    _Bool ExternalReference_is_equivalent_to(
+        ExternalReference* self, SerializableObject* other);
+    ExternalReference* ExternalReference_clone(
+        ExternalReference* self, OTIOErrorStatus* error_status);
+    const char* ExternalReference_schema_name(ExternalReference* self);
+    int         ExternalReference_schema_version(ExternalReference* self);
 #ifdef __cplusplus
 }
 #endif
--- src/c-opentimelineio/opentimelineio-bindings/src/externalReference.cpp
+++ src/c-opentimelineio/opentimelineio-bindings/src/externalReference.cpp
@@ -1,4 +1,7 @@
 #include "copentimelineio/externalReference.h"
+#include <copentimelineio/errorStatus.h>
+#include <copentimelineio/mediaReference.h>
+#include <copentimelineio/serializableObjectWithMetadata.h>
 #include <opentime/timeRange.h>
 #include <opentimelineio/anyDictionary.h>
 #include <opentimelineio/externalReference.h>
@@ -45,6 +48,74 @@ extern "C"
     {
         reinterpret_cast<OTIO_NS::ExternalReference*>(self)->set_target_url(
             target_url);
+    }
+    TimeRange* ExternalReference_available_range(ExternalReference* self)
+    {
+        return MediaReference_available_range((MediaReference*) self);
+    }
+    void ExternalReference_set_available_range(
+        ExternalReference* self, TimeRange* available_range)
+    {
+        MediaReference_set_available_range(
+            (MediaReference*) self, available_range);
+    }
+    _Bool ExternalReference_is_missing_reference(ExternalReference* self)
+    {
+        return MediaReference_is_missing_reference((MediaReference*) self);
+    }
+    const char* ExternalReference_name(ExternalReference* self)
+    {
+        return SerializableObjectWithMetadata_name(
+            (SerializableObjectWithMetadata*) self);
+    }
+    void ExternalReference_set_name(ExternalReference* self, const char* name)
+    {
+        SerializableObjectWithMetadata_set_name(
+            (SerializableObjectWithMetadata*) self, name);
+    }
+    AnyDictionary* ExternalReference_metadata(ExternalReference* self)
+    {
+        return SerializableObjectWithMetadata_metadata(
+            (SerializableObjectWithMetadata*) self);
+    }
+    _Bool ExternalReference_possibly_delete(ExternalReference* self)
+    {
+        return SerializableObject_possibly_delete((SerializableObject*) self);
+    }
+    _Bool ExternalReference_to_json_file(
+        ExternalReference* self,
+        const char*        file_name,
+        OTIOErrorStatus*   error_status,
+        int                indent)
+    {
+        return SerializableObject_to_json_file(
+            (SerializableObject*) self, file_name, error_status, indent);
+    }
+    const char* ExternalReference_to_json_string(
+        ExternalReference* self, OTIOErrorStatus* error_status, int indent)
+    {
+        return SerializableObject_to_json_string(
+            (SerializableObject*) self, error_status, indent);
+    }
+    _Bool ExternalReference_is_equivalent_to(
+        ExternalReference* self, SerializableObject* other)
+    {
+        return SerializableObject_is_equivalent_to(
+            (SerializableObject*) self, other);
+    }
+    ExternalReference* ExternalReference_clone(
+        ExternalReference* self, OTIOErrorStatus* error_status)
+    {
+        return (ExternalReference*) SerializableObject_clone(
+            (SerializableObject*) self, error_status);
+    }
+    const char* ExternalReference_schema_name(ExternalReference* self)
+    {
+        return SerializableObject_schema_name((SerializableObject*) self);
+    }
+    int ExternalReference_schema_version(ExternalReference* self)
+    {
+        return SerializableObject_schema_version((SerializableObject*) self);
     }
 #ifdef __cplusplus
 }

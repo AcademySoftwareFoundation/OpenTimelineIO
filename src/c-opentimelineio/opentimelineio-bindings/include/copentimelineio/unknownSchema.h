019b0ae97905d2100858c01ef9f02987693961ea
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-06-22 02:09:25 +0530

    Wrap inherited methods in UnknownSchema

--- src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/unknownSchema.h
+++ src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/unknownSchema.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "errorStatus.h"
 #include <stdbool.h>
 
 #ifdef __cplusplus
@@ -8,11 +9,25 @@ extern "C"
 #endif
     struct UnknownSchema;
     typedef struct UnknownSchema UnknownSchema;
-    UnknownSchema*        UnknownSchema_create(
-               const char* original_schema_name, int original_schema_version);
+    UnknownSchema*               UnknownSchema_create(
+                      const char* original_schema_name, int original_schema_version);
     const char* UnknownSchema_original_schema_name(UnknownSchema* self);
     int         UnknownSchema_original_schema_version(UnknownSchema* self);
     _Bool       UnknownSchema_is_unknown_schema(UnknownSchema* self);
+    _Bool       UnknownSchema_possibly_delete(UnknownSchema* self);
+    _Bool       UnknownSchema_to_json_file(
+              UnknownSchema*   self,
+              const char*      file_name,
+              OTIOErrorStatus* error_status,
+              int              indent);
+    const char* UnknownSchema_to_json_string(
+        UnknownSchema* self, OTIOErrorStatus* error_status, int indent);
+    _Bool UnknownSchema_is_equivalent_to(
+        UnknownSchema* self, SerializableObject* other);
+    UnknownSchema*
+                UnknownSchema_clone(UnknownSchema* self, OTIOErrorStatus* error_status);
+    const char* UnknownSchema_schema_name(UnknownSchema* self);
+    int         UnknownSchema_schema_version(UnknownSchema* self);
 #ifdef __cplusplus
 }
 #endif
--- src/c-opentimelineio/opentimelineio-bindings/src/unknownSchema.cpp
+++ src/c-opentimelineio/opentimelineio-bindings/src/unknownSchema.cpp
@@ -1,4 +1,5 @@
 #include "copentimelineio/unknownSchema.h"
+#include <copentimelineio/serializableObject.h>
 #include <opentimelineio/unknownSchema.h>
 #include <string.h>
 
@@ -29,6 +30,45 @@ extern "C"
     {
         return reinterpret_cast<OTIO_NS::UnknownSchema*>(self)
             ->is_unknown_schema();
+    }
+    _Bool UnknownSchema_possibly_delete(UnknownSchema* self)
+    {
+        return SerializableObject_possibly_delete((SerializableObject*) self);
+    }
+    _Bool UnknownSchema_to_json_file(
+        UnknownSchema*   self,
+        const char*      file_name,
+        OTIOErrorStatus* error_status,
+        int              indent)
+    {
+        return SerializableObject_to_json_file(
+            (SerializableObject*) self, file_name, error_status, indent);
+    }
+    const char* UnknownSchema_to_json_string(
+        UnknownSchema* self, OTIOErrorStatus* error_status, int indent)
+    {
+        return SerializableObject_to_json_string(
+            (SerializableObject*) self, error_status, indent);
+    }
+    _Bool UnknownSchema_is_equivalent_to(
+        UnknownSchema* self, SerializableObject* other)
+    {
+        return SerializableObject_is_equivalent_to(
+            (SerializableObject*) self, other);
+    }
+    UnknownSchema*
+    UnknownSchema_clone(UnknownSchema* self, OTIOErrorStatus* error_status)
+    {
+        return (UnknownSchema*) SerializableObject_clone(
+            (SerializableObject*) self, error_status);
+    }
+    const char* UnknownSchema_schema_name(UnknownSchema* self)
+    {
+        return SerializableObject_schema_name((SerializableObject*) self);
+    }
+    int UnknownSchema_schema_version(UnknownSchema* self)
+    {
+        return SerializableObject_schema_version((SerializableObject*) self);
     }
 #ifdef __cplusplus
 }

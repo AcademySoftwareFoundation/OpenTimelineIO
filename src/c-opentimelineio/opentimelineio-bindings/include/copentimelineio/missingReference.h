20d432bce7167e98a4177eb59a4b3cb27236a64e
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-06-22 00:24:11 +0530

    Wrap inherited methods in MissingReference

--- src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/missingReference.h
+++ src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/missingReference.h
@@ -2,6 +2,7 @@
 
 #include "anyDictionary.h"
 #include "copentime/timeRange.h"
+#include "errorStatus.h"
 #include <stdbool.h>
 
 #ifdef __cplusplus
@@ -13,7 +14,27 @@ extern "C"
 
     MissingReference* MissingReference_create(
         const char* name, TimeRange* available_range, AnyDictionary* metadata);
-    _Bool MissingReference_is_missing_reference(MissingReference* self);
+    _Bool      MissingReference_is_missing_reference(MissingReference* self);
+    TimeRange* MissingReference_available_range(MissingReference* self);
+    void       MissingReference_set_available_range(
+              MissingReference* self, TimeRange* available_range);
+    const char* MissingReference_name(MissingReference* self);
+    void MissingReference_set_name(MissingReference* self, const char* name);
+    AnyDictionary* MissingReference_metadata(MissingReference* self);
+    _Bool          MissingReference_possibly_delete(MissingReference* self);
+    _Bool          MissingReference_to_json_file(
+                 MissingReference* self,
+                 const char*       file_name,
+                 OTIOErrorStatus*  error_status,
+                 int               indent);
+    const char* MissingReference_to_json_string(
+        MissingReference* self, OTIOErrorStatus* error_status, int indent);
+    _Bool MissingReference_is_equivalent_to(
+        MissingReference* self, SerializableObject* other);
+    MissingReference* MissingReference_clone(
+        MissingReference* self, OTIOErrorStatus* error_status);
+    const char* MissingReference_schema_name(MissingReference* self);
+    int         MissingReference_schema_version(MissingReference* self);
 #ifdef __cplusplus
 }
 #endif
--- src/c-opentimelineio/opentimelineio-bindings/src/missingReference.cpp
+++ src/c-opentimelineio/opentimelineio-bindings/src/missingReference.cpp
@@ -1,4 +1,7 @@
 #include "copentimelineio/missingReference.h"
+#include <copentimelineio/mediaReference.h>
+#include <copentimelineio/serializableObject.h>
+#include <copentimelineio/serializableObjectWithMetadata.h>
 #include <opentime/timeRange.h>
 #include <opentimelineio/anyDictionary.h>
 #include <opentimelineio/missingReference.h>
@@ -32,6 +35,69 @@ extern "C"
     {
         return reinterpret_cast<OTIO_NS::MissingReference*>(self)
             ->is_missing_reference();
+    }
+    TimeRange* MissingReference_available_range(MissingReference* self)
+    {
+        return MediaReference_available_range((MediaReference*) self);
+    }
+    void MissingReference_set_available_range(
+        MissingReference* self, TimeRange* available_range)
+    {
+        MediaReference_set_available_range(
+            (MediaReference*) self, available_range);
+    }
+    const char* MissingReference_name(MissingReference* self)
+    {
+        return MediaReference_name((MediaReference*) self);
+    }
+    void MissingReference_set_name(MissingReference* self, const char* name)
+    {
+        SerializableObjectWithMetadata_set_name(
+            (SerializableObjectWithMetadata*) self, name);
+    }
+    AnyDictionary* MissingReference_metadata(MissingReference* self)
+    {
+        return SerializableObjectWithMetadata_metadata(
+            (SerializableObjectWithMetadata*) self);
+    }
+    _Bool MissingReference_possibly_delete(MissingReference* self)
+    {
+        return SerializableObject_possibly_delete((SerializableObject*) self);
+    }
+    _Bool MissingReference_to_json_file(
+        MissingReference* self,
+        const char*       file_name,
+        OTIOErrorStatus*  error_status,
+        int               indent)
+    {
+        return SerializableObject_to_json_file(
+            (SerializableObject*) self, file_name, error_status, indent);
+    }
+    const char* MissingReference_to_json_string(
+        MissingReference* self, OTIOErrorStatus* error_status, int indent)
+    {
+        return SerializableObject_to_json_string(
+            (SerializableObject*) self, error_status, indent);
+    }
+    _Bool MissingReference_is_equivalent_to(
+        MissingReference* self, SerializableObject* other)
+    {
+        return SerializableObject_is_equivalent_to(
+            (SerializableObject*) self, other);
+    }
+    MissingReference* MissingReference_clone(
+        MissingReference* self, OTIOErrorStatus* error_status)
+    {
+        return (MissingReference*) SerializableObject_clone(
+            (SerializableObject*) self, error_status);
+    }
+    const char* MissingReference_schema_name(MissingReference* self)
+    {
+        return SerializableObject_schema_name((SerializableObject*) self);
+    }
+    int MissingReference_schema_version(MissingReference* self)
+    {
+        return SerializableObject_schema_version((SerializableObject*) self);
     }
 #ifdef __cplusplus
 }

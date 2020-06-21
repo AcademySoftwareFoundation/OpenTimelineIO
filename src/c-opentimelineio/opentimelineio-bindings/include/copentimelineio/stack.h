2f79ab0b94d23ae1129bf051c448eb597044796c
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-06-22 01:02:10 +0530

    Wrap inherited methods in stack

--- src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/stack.h
+++ src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/stack.h
@@ -1,11 +1,17 @@
 #pragma once
 
 #include "anyDictionary.h"
+#include "composableRetainerVector.h"
+#include "composableVector.h"
 #include "copentime/timeRange.h"
+#include "effectRetainerVector.h"
 #include "effectVector.h"
 #include "errorStatus.h"
+#include "item.h"
 #include "mapComposableTimeRange.h"
+#include "markerRetainerVector.h"
 #include "markerVector.h"
+#include "optionalPairRationalTime.h"
 #include <stdbool.h>
 
 #ifdef __cplusplus
@@ -29,6 +35,75 @@ extern "C"
     Stack_available_range(Stack* self, OTIOErrorStatus* error_status);
     MapComposableTimeRange*
     Stack_range_of_all_children(Stack* self, OTIOErrorStatus* error_status);
+
+    const char*               Stack_composition_kind(Stack* self);
+    ComposableRetainerVector* Stack_children(Stack* self);
+    void                      Stack_clear_children(Stack* self);
+    _Bool                     Stack_set_children(
+                            Stack* self, ComposableVector* children, OTIOErrorStatus* error_status);
+    _Bool Stack_insert_child(
+        Stack*           self,
+        int              index,
+        Composable*      child,
+        OTIOErrorStatus* error_status);
+    _Bool Stack_set_child(
+        Stack*           self,
+        int              index,
+        Composable*      child,
+        OTIOErrorStatus* error_status);
+    _Bool
+          Stack_remove_child(Stack* self, int index, OTIOErrorStatus* error_status);
+    _Bool Stack_append_child(
+        Stack* self, Composable* child, OTIOErrorStatus* error_status);
+    _Bool Stack_is_parent_of(Stack* self, Composable* other);
+    OptionalPairRationalTime* Stack_handles_of_child(
+        Stack* self, Composable* child, OTIOErrorStatus* error_status);
+    TimeRange* Stack_range_of_child(
+        Stack* self, Composable* child, OTIOErrorStatus* error_status);
+    TimeRange* Stack_trimmed_range_of_child(
+        Stack* self, Composable* child, OTIOErrorStatus* error_status);
+    TimeRange* Stack_trim_child_range(Stack* self, TimeRange* child_range);
+    _Bool      Stack_has_child(Stack* self, Composable* child);
+
+    _Bool      Stack_visible(Stack* self);
+    _Bool      Stack_overlapping(Stack* self);
+    TimeRange* Stack_source_range(Stack* self);
+    void       Stack_set_source_range(Stack* self, TimeRange* source_range);
+    EffectRetainerVector* Stack_effects(Stack* self);
+    MarkerRetainerVector* Stack_markers(Stack* self);
+    RationalTime* Stack_duration(Stack* self, OTIOErrorStatus* error_status);
+    TimeRange* Stack_trimmed_range(Stack* self, OTIOErrorStatus* error_status);
+    TimeRange* Stack_visible_range(Stack* self, OTIOErrorStatus* error_status);
+    TimeRange*
+    Stack_trimmed_range_in_parent(Stack* self, OTIOErrorStatus* error_status);
+    TimeRange*
+                  Stack_range_in_parent(Stack* self, OTIOErrorStatus* error_status);
+    RationalTime* Stack_transformed_time(
+        Stack*           self,
+        RationalTime*    time,
+        Item*            to_item,
+        OTIOErrorStatus* error_status);
+    TimeRange* Stack_transformed_time_range(
+        Stack*           self,
+        TimeRange*       time_range,
+        Item*            to_item,
+        OTIOErrorStatus* error_status);
+    Composition*   Stack_parent(Stack* self);
+    const char*    Stack_name(Stack* self);
+    AnyDictionary* Stack_metadata(Stack* self);
+    void           Stack_set_name(Stack* self, const char* name);
+    _Bool          Stack_possibly_delete(Stack* self);
+    _Bool          Stack_to_json_file(
+                 Stack*           self,
+                 const char*      file_name,
+                 OTIOErrorStatus* error_status,
+                 int              indent);
+    const char* Stack_to_json_string(
+        Stack* self, OTIOErrorStatus* error_status, int indent);
+    _Bool       Stack_is_equivalent_to(Stack* self, SerializableObject* other);
+    Stack*      Stack_clone(Stack* self, OTIOErrorStatus* error_status);
+    const char* Stack_schema_name(Stack* self);
+    int         Stack_schema_version(Stack* self);
 #ifdef __cplusplus
 }
 #endif
--- src/c-opentimelineio/opentimelineio-bindings/src/stack.cpp
+++ src/c-opentimelineio/opentimelineio-bindings/src/stack.cpp
@@ -1,4 +1,5 @@
 #include "copentimelineio/stack.h"
+#include <copentimelineio/composition.h>
 #include <map>
 #include <opentime/timeRange.h>
 #include <opentimelineio/anyDictionary.h>
@@ -90,6 +91,199 @@ extern "C"
             reinterpret_cast<OTIO_NS::Stack*>(self)->range_of_all_children(
                 reinterpret_cast<OTIO_NS::ErrorStatus*>(error_status));
         return reinterpret_cast<MapComposableTimeRange*>(new MapDef(mapDef));
+    }
+    const char* Stack_composition_kind(Stack* self)
+    {
+        return Composition_composition_kind((Composition*) self);
+    }
+    ComposableRetainerVector* Stack_children(Stack* self)
+    {
+        return Composition_children((Composition*) self);
+    }
+    void Stack_clear_children(Stack* self)
+    {
+        Composition_clear_children((Composition*) self);
+    }
+    _Bool Stack_set_children(
+        Stack* self, ComposableVector* children, OTIOErrorStatus* error_status)
+    {
+        return Composition_set_children(
+            (Composition*) self, children, error_status);
+    }
+    _Bool Stack_insert_child(
+        Stack*           self,
+        int              index,
+        Composable*      child,
+        OTIOErrorStatus* error_status)
+    {
+        return Composition_insert_child(
+            (Composition*) self, index, child, error_status);
+    }
+    _Bool Stack_set_child(
+        Stack*           self,
+        int              index,
+        Composable*      child,
+        OTIOErrorStatus* error_status)
+    {
+        return Composition_set_child(
+            (Composition*) self, index, child, error_status);
+    }
+    _Bool
+    Stack_remove_child(Stack* self, int index, OTIOErrorStatus* error_status)
+    {
+        return Composition_remove_child(
+            (Composition*) self, index, error_status);
+    }
+    _Bool Stack_append_child(
+        Stack* self, Composable* child, OTIOErrorStatus* error_status)
+    {
+        return Composition_append_child(
+            (Composition*) self, child, error_status);
+    }
+    _Bool Stack_is_parent_of(Stack* self, Composable* other)
+    {
+        return Composition_is_parent_of((Composition*) self, other);
+    }
+    OptionalPairRationalTime* Stack_handles_of_child(
+        Stack* self, Composable* child, OTIOErrorStatus* error_status)
+    {
+        return Composition_handles_of_child(
+            (Composition*) self, child, error_status);
+    }
+    TimeRange* Stack_range_of_child(
+        Stack* self, Composable* child, OTIOErrorStatus* error_status)
+    {
+        return Composition_range_of_child(
+            (Composition*) self, child, error_status);
+    }
+    TimeRange* Stack_trimmed_range_of_child(
+        Stack* self, Composable* child, OTIOErrorStatus* error_status)
+    {
+        return Composition_trimmed_range_of_child(
+            (Composition*) self, child, error_status);
+    }
+    TimeRange* Stack_trim_child_range(Stack* self, TimeRange* child_range)
+    {
+        return Composition_trim_child_range((Composition*) self, child_range);
+    }
+    _Bool Stack_has_child(Stack* self, Composable* child)
+    {
+        return Composition_has_child((Composition*) self, child);
+    }
+    _Bool Stack_visible(Stack* self)
+    {
+        return Composition_visible((Composition*) self);
+    }
+    _Bool Stack_overlapping(Stack* self)
+    {
+        return Composition_overlapping((Composition*) self);
+    }
+    TimeRange* Stack_source_range(Stack* self)
+    {
+        return Composition_source_range((Composition*) self);
+    }
+    void Stack_set_source_range(Stack* self, TimeRange* source_range)
+    {
+        Composition_set_source_range((Composition*) self, source_range);
+    }
+    EffectRetainerVector* Stack_effects(Stack* self)
+    {
+        return Composition_effects((Composition*) self);
+    }
+    MarkerRetainerVector* Stack_markers(Stack* self)
+    {
+        return Composition_markers((Composition*) self);
+    }
+    RationalTime* Stack_duration(Stack* self, OTIOErrorStatus* error_status)
+    {
+        return Composition_duration((Composition*) self, error_status);
+    }
+    TimeRange* Stack_trimmed_range(Stack* self, OTIOErrorStatus* error_status)
+    {
+        return Composition_trimmed_range((Composition*) self, error_status);
+    }
+    TimeRange* Stack_visible_range(Stack* self, OTIOErrorStatus* error_status)
+    {
+        return Composition_visible_range((Composition*) self, error_status);
+    }
+    TimeRange*
+    Stack_trimmed_range_in_parent(Stack* self, OTIOErrorStatus* error_status)
+    {
+        return Composition_trimmed_range_in_parent(
+            (Composition*) self, error_status);
+    }
+    TimeRange* Stack_range_in_parent(Stack* self, OTIOErrorStatus* error_status)
+    {
+        return Composition_range_in_parent((Composition*) self, error_status);
+    }
+    RationalTime* Stack_transformed_time(
+        Stack*           self,
+        RationalTime*    time,
+        Item*            to_item,
+        OTIOErrorStatus* error_status)
+    {
+        return Composition_transformed_time(
+            (Composition*) self, time, to_item, error_status);
+    }
+    TimeRange* Stack_transformed_time_range(
+        Stack*           self,
+        TimeRange*       time_range,
+        Item*            to_item,
+        OTIOErrorStatus* error_status)
+    {
+        return Composition_transformed_time_range(
+            (Composition*) self, time_range, to_item, error_status);
+    }
+    Composition* Stack_parent(Stack* self)
+    {
+        return Composition_parent((Composition*) self);
+    }
+    const char* Stack_name(Stack* self)
+    {
+        return Composition_name((Composition*) self);
+    }
+    AnyDictionary* Stack_metadata(Stack* self)
+    {
+        return Composition_metadata((Composition*) self);
+    }
+    void Stack_set_name(Stack* self, const char* name)
+    {
+        Composition_set_name((Composition*) self, name);
+    }
+    _Bool Stack_possibly_delete(Stack* self)
+    {
+        return Composition_possibly_delete((Composition*) self);
+    }
+    _Bool Stack_to_json_file(
+        Stack*           self,
+        const char*      file_name,
+        OTIOErrorStatus* error_status,
+        int              indent)
+    {
+        return Composition_to_json_file(
+            (Composition*) self, file_name, error_status, indent);
+    }
+    const char*
+    Stack_to_json_string(Stack* self, OTIOErrorStatus* error_status, int indent)
+    {
+        return Composition_to_json_string(
+            (Composition*) self, error_status, indent);
+    }
+    _Bool Stack_is_equivalent_to(Stack* self, SerializableObject* other)
+    {
+        return Composition_is_equivalent_to((Composition*) self, other);
+    }
+    Stack* Stack_clone(Stack* self, OTIOErrorStatus* error_status)
+    {
+        return (Stack*) Composition_clone((Composition*) self, error_status);
+    }
+    const char* Stack_schema_name(Stack* self)
+    {
+        return Composition_schema_name((Composition*) self);
+    }
+    int Stack_schema_version(Stack* self)
+    {
+        return Composition_schema_version((Composition*) self);
     }
 #ifdef __cplusplus
 }

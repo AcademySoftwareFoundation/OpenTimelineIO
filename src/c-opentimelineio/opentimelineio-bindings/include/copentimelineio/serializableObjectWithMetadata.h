8b627e0823bccaa74d2d9402a6a3e573ba4d02fd
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-06-22 00:41:02 +0530

    Wrap inherited methods in SerializableObjectWithMetadata

--- src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/serializableObjectWithMetadata.h
+++ src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/serializableObjectWithMetadata.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "anyDictionary.h"
+#include "errorStatus.h"
+#include "serializableObject.h"
 
 #ifdef __cplusplus
 extern "C"
@@ -16,6 +18,27 @@ extern "C"
     void SerializableObjectWithMetadata_set_name(
         SerializableObjectWithMetadata* self, const char* name);
     AnyDictionary* SerializableObjectWithMetadata_metadata(
+        SerializableObjectWithMetadata* self);
+    _Bool SerializableObjectWithMetadata_possibly_delete(
+        SerializableObjectWithMetadata* self);
+    _Bool SerializableObjectWithMetadata_to_json_file(
+        SerializableObjectWithMetadata* self,
+        const char*                     file_name,
+        OTIOErrorStatus*                error_status,
+        int                             indent);
+    const char* SerializableObjectWithMetadata_to_json_string(
+        SerializableObjectWithMetadata* self,
+        OTIOErrorStatus*                error_status,
+        int                             indent);
+    _Bool SerializableObjectWithMetadata_is_equivalent_to(
+        SerializableObjectWithMetadata* self, SerializableObject* other);
+    SerializableObjectWithMetadata* SerializableObjectWithMetadata_clone(
+        SerializableObjectWithMetadata* self, OTIOErrorStatus* error_status);
+    _Bool SerializableObjectWithMetadata_is_unknown_schema(
+        SerializableObjectWithMetadata* self);
+    const char* SerializableObjectWithMetadata_schema_name(
+        SerializableObjectWithMetadata* self);
+    int SerializableObjectWithMetadata_schema_version(
         SerializableObjectWithMetadata* self);
 #ifdef __cplusplus
 }
--- src/c-opentimelineio/opentimelineio-bindings/src/serializableObjectWithMetadata.cpp
+++ src/c-opentimelineio/opentimelineio-bindings/src/serializableObjectWithMetadata.cpp
@@ -38,6 +38,55 @@ extern "C"
         return reinterpret_cast<AnyDictionary*>(
             new OTIO_NS::AnyDictionary(anyDictionary));
     }
+    _Bool SerializableObjectWithMetadata_possibly_delete(
+        SerializableObjectWithMetadata* self)
+    {
+        return SerializableObject_possibly_delete((SerializableObject*) self);
+    }
+    _Bool SerializableObjectWithMetadata_to_json_file(
+        SerializableObjectWithMetadata* self,
+        const char*                     file_name,
+        OTIOErrorStatus*                error_status,
+        int                             indent)
+    {
+        return SerializableObject_to_json_file(
+            (SerializableObject*) self, file_name, error_status, indent);
+    }
+    const char* SerializableObjectWithMetadata_to_json_string(
+        SerializableObjectWithMetadata* self,
+        OTIOErrorStatus*                error_status,
+        int                             indent)
+    {
+        return SerializableObject_to_json_string(
+            (SerializableObject*) self, error_status, indent);
+    }
+    _Bool SerializableObjectWithMetadata_is_equivalent_to(
+        SerializableObjectWithMetadata* self, SerializableObject* other)
+    {
+        return SerializableObject_is_equivalent_to(
+            (SerializableObject*) self, other);
+    }
+    SerializableObjectWithMetadata* SerializableObjectWithMetadata_clone(
+        SerializableObjectWithMetadata* self, OTIOErrorStatus* error_status)
+    {
+        return (SerializableObjectWithMetadata*) SerializableObject_clone(
+            (SerializableObject*) self, error_status);
+    }
+    _Bool SerializableObjectWithMetadata_is_unknown_schema(
+        SerializableObjectWithMetadata* self)
+    {
+        return SerializableObject_is_unknown_schema((SerializableObject*) self);
+    }
+    const char* SerializableObjectWithMetadata_schema_name(
+        SerializableObjectWithMetadata* self)
+    {
+        return SerializableObject_schema_name((SerializableObject*) self);
+    }
+    int SerializableObjectWithMetadata_schema_version(
+        SerializableObjectWithMetadata* self)
+    {
+        return SerializableObject_schema_version((SerializableObject*) self);
+    }
 #ifdef __cplusplus
 }
 #endif

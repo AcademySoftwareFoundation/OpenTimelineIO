1fe2c1dd2bb30cd6ed692c580d9a5cc76e9cc95e
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-06-21 20:38:27 +0530

    Wrap inherited methods in LinearTimeWarp

--- src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/linearTimeWarp.h
+++ src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/linearTimeWarp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "anyDictionary.h"
+#include "errorStatus.h"
 #include <stdbool.h>
 
 #ifdef __cplusplus
@@ -17,7 +18,27 @@ extern "C"
         AnyDictionary* metadata);
     double LinearTimeWarp_time_scalar(LinearTimeWarp* self);
     void
-    LinearTimeWarp_set_time_scalar(LinearTimeWarp* self, double time_scalar);
+                LinearTimeWarp_set_time_scalar(LinearTimeWarp* self, double time_scalar);
+    const char* LinearTimeWarp_effect_name(LinearTimeWarp* self);
+    void        LinearTimeWarp_set_effect_name(
+               LinearTimeWarp* self, const char* effect_name);
+    const char* LinearTimeWarp_name(LinearTimeWarp* self);
+    void        LinearTimeWarp_set_name(LinearTimeWarp* self, const char* name);
+    AnyDictionary* LinearTimeWarp_metadata(LinearTimeWarp* self);
+    _Bool          LinearTimeWarp_possibly_delete(LinearTimeWarp* self);
+    _Bool          LinearTimeWarp_to_json_file(
+                 LinearTimeWarp*  self,
+                 const char*      file_name,
+                 OTIOErrorStatus* error_status,
+                 int              indent);
+    const char* LinearTimeWarp_to_json_string(
+        LinearTimeWarp* self, OTIOErrorStatus* error_status, int indent);
+    _Bool LinearTimeWarp_is_equivalent_to(
+        LinearTimeWarp* self, SerializableObject* other);
+    LinearTimeWarp*
+                LinearTimeWarp_clone(LinearTimeWarp* self, OTIOErrorStatus* error_status);
+    const char* LinearTimeWarp_schema_name(LinearTimeWarp* self);
+    int         LinearTimeWarp_schema_version(LinearTimeWarp* self);
 #ifdef __cplusplus
 }
 #endif
--- src/c-opentimelineio/opentimelineio-bindings/src/linearTimeWarp.cpp
+++ src/c-opentimelineio/opentimelineio-bindings/src/linearTimeWarp.cpp
@@ -1,4 +1,5 @@
 #include "copentimelineio/linearTimeWarp.h"
+#include <copentimelineio/effect.h>
 #include <opentimelineio/anyDictionary.h>
 #include <opentimelineio/linearTimeWarp.h>
 
@@ -35,6 +36,63 @@ extern "C"
     {
         reinterpret_cast<OTIO_NS::LinearTimeWarp*>(self)->set_time_scalar(
             time_scalar);
+    }
+    const char* LinearTimeWarp_effect_name(LinearTimeWarp* self)
+    {
+        return Effect_effect_name((Effect*) self);
+    }
+    void LinearTimeWarp_set_effect_name(
+        LinearTimeWarp* self, const char* effect_name)
+    {
+        Effect_set_name((Effect*) self, effect_name);
+    }
+    const char* LinearTimeWarp_name(LinearTimeWarp* self)
+    {
+        return Effect_name((Effect*) self);
+    }
+    void LinearTimeWarp_set_name(LinearTimeWarp* self, const char* name)
+    {
+        Effect_set_name((Effect*) self, name);
+    }
+    AnyDictionary* LinearTimeWarp_metadata(LinearTimeWarp* self)
+    {
+        return Effect_metadata((Effect*) self);
+    }
+    _Bool LinearTimeWarp_possibly_delete(LinearTimeWarp* self)
+    {
+        return Effect_possibly_delete((Effect*) self);
+    }
+    _Bool LinearTimeWarp_to_json_file(
+        LinearTimeWarp*  self,
+        const char*      file_name,
+        OTIOErrorStatus* error_status,
+        int              indent)
+    {
+        return Effect_to_json_file(
+            (Effect*) self, file_name, error_status, indent);
+    }
+    const char* LinearTimeWarp_to_json_string(
+        LinearTimeWarp* self, OTIOErrorStatus* error_status, int indent)
+    {
+        return Effect_to_json_string((Effect*) self, error_status, indent);
+    }
+    _Bool LinearTimeWarp_is_equivalent_to(
+        LinearTimeWarp* self, SerializableObject* other)
+    {
+        return Effect_is_equivalent_to((Effect*) self, other);
+    }
+    LinearTimeWarp*
+    LinearTimeWarp_clone(LinearTimeWarp* self, OTIOErrorStatus* error_status)
+    {
+        return (LinearTimeWarp*) Effect_clone((Effect*) self, error_status);
+    }
+    const char* LinearTimeWarp_schema_name(LinearTimeWarp* self)
+    {
+        return Effect_schema_name((Effect*) self);
+    }
+    int LinearTimeWarp_schema_version(LinearTimeWarp* self)
+    {
+        return Effect_schema_version((Effect*) self);
     }
 #ifdef __cplusplus
 }

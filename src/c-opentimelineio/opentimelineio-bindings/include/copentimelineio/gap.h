908e23b767e8351049a823796dbf4bc9983aac4c
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-06-21 13:10:11 +0530

    Wrap inherited methods in Gap

--- src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/gap.h
+++ src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/gap.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "anyDictionary.h"
+#include "composition.h"
 #include "copentime/rationalTime.h"
 #include "copentime/timeRange.h"
+#include "effectRetainerVector.h"
 #include "effectVector.h"
+#include "item.h"
+#include "markerRetainerVector.h"
 #include "markerVector.h"
 #include <stdbool.h>
 
@@ -26,7 +30,45 @@ extern "C"
         EffectVector*  effects,
         MarkerVector*  markers,
         AnyDictionary* metadata);
-    _Bool Gap_visible(Gap* self);
+    _Bool      Gap_visible(Gap* self);
+    _Bool      Gap_overlapping(Gap* self);
+    TimeRange* Gap_source_range(Gap* self);
+    void       Gap_set_source_range(Gap* self, TimeRange* source_range);
+    EffectRetainerVector* Gap_effects(Gap* self);
+    MarkerRetainerVector* Gap_markers(Gap* self);
+    RationalTime* Gap_duration(Gap* self, OTIOErrorStatus* error_status);
+    TimeRange*    Gap_available_range(Gap* self, OTIOErrorStatus* error_status);
+    TimeRange*    Gap_trimmed_range(Gap* self, OTIOErrorStatus* error_status);
+    TimeRange*    Gap_visible_range(Gap* self, OTIOErrorStatus* error_status);
+    TimeRange*
+                  Gap_trimmed_range_in_parent(Gap* self, OTIOErrorStatus* error_status);
+    TimeRange*    Gap_range_in_parent(Gap* self, OTIOErrorStatus* error_status);
+    RationalTime* Gap_transformed_time(
+        Gap*             self,
+        RationalTime*    time,
+        Item*            to_item,
+        OTIOErrorStatus* error_status);
+    TimeRange* Gap_transformed_time_range(
+        Gap*             self,
+        TimeRange*       time_range,
+        Item*            to_item,
+        OTIOErrorStatus* error_status);
+    Composition*   Gap_parent(Gap* self);
+    const char*    Gap_name(Gap* self);
+    AnyDictionary* Gap_metadata(Gap* self);
+    void           Gap_set_name(Gap* self, const char* name);
+    _Bool          Gap_possibly_delete(Gap* self);
+    _Bool          Gap_to_json_file(
+                 Gap*             self,
+                 const char*      file_name,
+                 OTIOErrorStatus* error_status,
+                 int              indent);
+    const char*
+                Gap_to_json_string(Gap* self, OTIOErrorStatus* error_status, int indent);
+    _Bool       Gap_is_equivalent_to(Gap* self, SerializableObject* other);
+    Gap*        Gap_clone(Gap* self, OTIOErrorStatus* error_status);
+    const char* Gap_schema_name(Gap* self);
+    int         Gap_schema_version(Gap* self);
 #ifdef __cplusplus
 }
 #endif
--- src/c-opentimelineio/opentimelineio-bindings/src/gap.cpp
+++ src/c-opentimelineio/opentimelineio-bindings/src/gap.cpp
@@ -61,6 +61,116 @@ extern "C"
     {
         return reinterpret_cast<OTIO_NS::Gap*>(self)->visible();
     }
+    _Bool Gap_overlapping(Gap* self) { return Item_overlapping((Item*) self); }
+    TimeRange* Gap_source_range(Gap* self)
+    {
+        return Item_source_range((Item*) self);
+    }
+    void Gap_set_source_range(Gap* self, TimeRange* source_range)
+    {
+        Item_set_source_range((Item*) self, source_range);
+    }
+    EffectRetainerVector* Gap_effects(Gap* self)
+    {
+        return Item_effects((Item*) self);
+    }
+    MarkerRetainerVector* Gap_markers(Gap* self)
+    {
+        return Item_markers((Item*) self);
+    }
+    RationalTime* Gap_duration(Gap* self, OTIOErrorStatus* error_status)
+    {
+        return Item_duration((Item*) self, error_status);
+    }
+    TimeRange* Gap_available_range(Gap* self, OTIOErrorStatus* error_status)
+    {
+        return Item_available_range((Item*) self, error_status);
+    }
+    TimeRange* Gap_trimmed_range(Gap* self, OTIOErrorStatus* error_status)
+    {
+        return Item_trimmed_range((Item*) self, error_status);
+    }
+    TimeRange* Gap_visible_range(Gap* self, OTIOErrorStatus* error_status)
+    {
+        return Item_visible_range((Item*) self, error_status);
+    }
+    TimeRange*
+    Gap_trimmed_range_in_parent(Gap* self, OTIOErrorStatus* error_status)
+    {
+        return Item_trimmed_range_in_parent((Item*) self, error_status);
+    }
+    TimeRange* Gap_range_in_parent(Gap* self, OTIOErrorStatus* error_status)
+    {
+        return Item_range_in_parent((Item*) self, error_status);
+    }
+    RationalTime* Gap_transformed_time(
+        Gap*             self,
+        RationalTime*    time,
+        Item*            to_item,
+        OTIOErrorStatus* error_status)
+    {
+        return Item_transformed_time((Item*) self, time, to_item, error_status);
+    }
+    TimeRange* Gap_transformed_time_range(
+        Gap*             self,
+        TimeRange*       time_range,
+        Item*            to_item,
+        OTIOErrorStatus* error_status)
+    {
+        return Item_transformed_time_range(
+            (Item*) self, time_range, to_item, error_status);
+    }
+    Composition* Gap_parent(Gap* self)
+    {
+        return Composable_parent((Composable*) self);
+    }
+    const char* Gap_name(Gap* self)
+    {
+        return Composable_name((Composable*) self);
+    }
+    AnyDictionary* Gap_metadata(Gap* self)
+    {
+        return Composable_metadata((Composable*) self);
+    }
+    void Gap_set_name(Gap* self, const char* name)
+    {
+        Composable_set_name((Composable*) self, name);
+    }
+    _Bool Gap_possibly_delete(Gap* self)
+    {
+        return Composable_possibly_delete((Composable*) self);
+    }
+    _Bool Gap_to_json_file(
+        Gap*             self,
+        const char*      file_name,
+        OTIOErrorStatus* error_status,
+        int              indent)
+    {
+        return Composable_to_json_file(
+            (Composable*) self, file_name, error_status, indent);
+    }
+    const char*
+    Gap_to_json_string(Gap* self, OTIOErrorStatus* error_status, int indent)
+    {
+        return Composable_to_json_string(
+            (Composable*) self, error_status, indent);
+    }
+    _Bool Gap_is_equivalent_to(Gap* self, SerializableObject* other)
+    {
+        return Composable_is_equivalent_to((Composable*) self, other);
+    }
+    Gap* Gap_clone(Gap* self, OTIOErrorStatus* error_status)
+    {
+        return (Gap*) Composable_clone((Composable*) self, error_status);
+    }
+    const char* Gap_schema_name(Gap* self)
+    {
+        return Composable_schema_name((Composable*) self);
+    }
+    int Gap_schema_version(Gap* self)
+    {
+        return Composable_schema_version((Composable*) self);
+    }
 #ifdef __cplusplus
 }
 #endif

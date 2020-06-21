b3d2e261af9f574f5c9bf1bd01760129aef38c40
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-06-22 01:55:48 +0530

    Wrap inherited methods in Track

--- src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/track.h
+++ src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/track.h
@@ -2,10 +2,14 @@
 
 #include "anyDictionary.h"
 #include "composable.h"
+#include "composableRetainerVector.h"
 #include "composableVector.h"
 #include "copentime/timeRange.h"
+#include "effectRetainerVector.h"
 #include "errorStatus.h"
+#include "item.h"
 #include "mapComposableTimeRange.h"
+#include "markerRetainerVector.h"
 #include "optionalPairRationalTime.h"
 #include "retainerPairComposable.h"
 
@@ -50,6 +54,75 @@ extern "C"
     ComposableVector* Track_each_clip(Track* self);
     MapComposableTimeRange*
     Track_range_of_all_children(Track* self, OTIOErrorStatus* error_status);
+
+    const char*               Track_composition_kind(Track* self);
+    ComposableRetainerVector* Track_children(Track* self);
+    void                      Track_clear_children(Track* self);
+    _Bool                     Track_set_children(
+                            Track* self, ComposableVector* children, OTIOErrorStatus* error_status);
+    _Bool Track_insert_child(
+        Track*           self,
+        int              index,
+        Composable*      child,
+        OTIOErrorStatus* error_status);
+    _Bool Track_set_child(
+        Track*           self,
+        int              index,
+        Composable*      child,
+        OTIOErrorStatus* error_status);
+    _Bool
+          Track_remove_child(Track* self, int index, OTIOErrorStatus* error_status);
+    _Bool Track_append_child(
+        Track* self, Composable* child, OTIOErrorStatus* error_status);
+    _Bool      Track_is_parent_of(Track* self, Composable* other);
+    TimeRange* Track_range_of_child(
+        Track* self, Composable* child, OTIOErrorStatus* error_status);
+    TimeRange* Track_trimmed_range_of_child(
+        Track* self, Composable* child, OTIOErrorStatus* error_status);
+    TimeRange* Track_trim_child_range(Track* self, TimeRange* child_range);
+    _Bool      Track_has_child(Track* self, Composable* child);
+
+    _Bool      Track_visible(Track* self);
+    _Bool      Track_overlapping(Track* self);
+    TimeRange* Track_source_range(Track* self);
+    void       Track_set_source_range(Track* self, TimeRange* source_range);
+    EffectRetainerVector* Track_effects(Track* self);
+    MarkerRetainerVector* Track_markers(Track* self);
+    RationalTime* Track_duration(Track* self, OTIOErrorStatus* error_status);
+    TimeRange*
+               Track_available_range(Track* self, OTIOErrorStatus* error_status);
+    TimeRange* Track_trimmed_range(Track* self, OTIOErrorStatus* error_status);
+    TimeRange* Track_visible_range(Track* self, OTIOErrorStatus* error_status);
+    TimeRange*
+    Track_trimmed_range_in_parent(Track* self, OTIOErrorStatus* error_status);
+    TimeRange*
+                  Track_range_in_parent(Track* self, OTIOErrorStatus* error_status);
+    RationalTime* Track_transformed_time(
+        Track*           self,
+        RationalTime*    time,
+        Item*            to_item,
+        OTIOErrorStatus* error_status);
+    TimeRange* Track_transformed_time_range(
+        Track*           self,
+        TimeRange*       time_range,
+        Item*            to_item,
+        OTIOErrorStatus* error_status);
+    Composition*   Track_parent(Track* self);
+    const char*    Track_name(Track* self);
+    AnyDictionary* Track_metadata(Track* self);
+    void           Track_set_name(Track* self, const char* name);
+    _Bool          Track_possibly_delete(Track* self);
+    _Bool          Track_to_json_file(
+                 Track*           self,
+                 const char*      file_name,
+                 OTIOErrorStatus* error_status,
+                 int              indent);
+    const char* Track_to_json_string(
+        Track* self, OTIOErrorStatus* error_status, int indent);
+    _Bool       Track_is_equivalent_to(Track* self, SerializableObject* other);
+    Track*      Track_clone(Track* self, OTIOErrorStatus* error_status);
+    const char* Track_schema_name(Track* self);
+    int         Track_schema_version(Track* self);
 #ifdef __cplusplus
 }
 #endif
--- src/c-opentimelineio/opentimelineio-bindings/src/track.cpp
+++ src/c-opentimelineio/opentimelineio-bindings/src/track.cpp
@@ -1,4 +1,5 @@
 #include "copentimelineio/track.h"
+#include <copentimelineio/composition.h>
 #include <opentime/rationalTime.h>
 #include <opentime/timeRange.h>
 #include <opentimelineio/anyDictionary.h>
@@ -153,6 +154,193 @@ extern "C"
                 reinterpret_cast<OTIO_NS::ErrorStatus*>(error_status));
         return reinterpret_cast<MapComposableTimeRange*>(
             new MapComposableTimeRangeDef(mapComposableTimeRange));
+    }
+    const char* Track_composition_kind(Track* self)
+    {
+        return Composition_composition_kind((Composition*) self);
+    }
+    ComposableRetainerVector* Track_children(Track* self)
+    {
+        return Composition_children((Composition*) self);
+    }
+    void Track_clear_children(Track* self)
+    {
+        Composition_clear_children((Composition*) self);
+    }
+    _Bool Track_set_children(
+        Track* self, ComposableVector* children, OTIOErrorStatus* error_status)
+    {
+        return Composition_set_children(
+            (Composition*) self, children, error_status);
+    }
+    _Bool Track_insert_child(
+        Track*           self,
+        int              index,
+        Composable*      child,
+        OTIOErrorStatus* error_status)
+    {
+        return Composition_insert_child(
+            (Composition*) self, index, child, error_status);
+    }
+    _Bool Track_set_child(
+        Track*           self,
+        int              index,
+        Composable*      child,
+        OTIOErrorStatus* error_status)
+    {
+        return Composition_set_child(
+            (Composition*) self, index, child, error_status);
+    }
+    _Bool
+    Track_remove_child(Track* self, int index, OTIOErrorStatus* error_status)
+    {
+        return Composition_remove_child(
+            (Composition*) self, index, error_status);
+    }
+    _Bool Track_append_child(
+        Track* self, Composable* child, OTIOErrorStatus* error_status)
+    {
+        return Composition_append_child(
+            (Composition*) self, child, error_status);
+    }
+    _Bool Track_is_parent_of(Track* self, Composable* other)
+    {
+        return Composition_is_parent_of((Composition*) self, other);
+    }
+    TimeRange* Track_range_of_child(
+        Track* self, Composable* child, OTIOErrorStatus* error_status)
+    {
+        return Composition_range_of_child(
+            (Composition*) self, child, error_status);
+    }
+    TimeRange* Track_trimmed_range_of_child(
+        Track* self, Composable* child, OTIOErrorStatus* error_status)
+    {
+        return Composition_trimmed_range_of_child(
+            (Composition*) self, child, error_status);
+    }
+    TimeRange* Track_trim_child_range(Track* self, TimeRange* child_range)
+    {
+        return Composition_trim_child_range((Composition*) self, child_range);
+    }
+    _Bool Track_has_child(Track* self, Composable* child)
+    {
+        return Composition_has_child((Composition*) self, child);
+    }
+    _Bool Track_visible(Track* self)
+    {
+        return Composition_visible((Composition*) self);
+    }
+    _Bool Track_overlapping(Track* self)
+    {
+        return Composition_overlapping((Composition*) self);
+    }
+    TimeRange* Track_source_range(Track* self)
+    {
+        return Composition_source_range((Composition*) self);
+    }
+    void Track_set_source_range(Track* self, TimeRange* source_range)
+    {
+        Composition_set_source_range((Composition*) self, source_range);
+    }
+    EffectRetainerVector* Track_effects(Track* self)
+    {
+        return Composition_effects((Composition*) self);
+    }
+    MarkerRetainerVector* Track_markers(Track* self)
+    {
+        return Composition_markers((Composition*) self);
+    }
+    RationalTime* Track_duration(Track* self, OTIOErrorStatus* error_status)
+    {
+        return Composition_duration((Composition*) self, error_status);
+    }
+    TimeRange* Track_trimmed_range(Track* self, OTIOErrorStatus* error_status)
+    {
+        return Composition_trimmed_range((Composition*) self, error_status);
+    }
+    TimeRange* Track_visible_range(Track* self, OTIOErrorStatus* error_status)
+    {
+        return Composition_visible_range((Composition*) self, error_status);
+    }
+    TimeRange*
+    Track_trimmed_range_in_parent(Track* self, OTIOErrorStatus* error_status)
+    {
+        return Composition_trimmed_range_in_parent(
+            (Composition*) self, error_status);
+    }
+    TimeRange* Track_range_in_parent(Track* self, OTIOErrorStatus* error_status)
+    {
+        return Composition_range_in_parent((Composition*) self, error_status);
+    }
+    RationalTime* Track_transformed_time(
+        Track*           self,
+        RationalTime*    time,
+        Item*            to_item,
+        OTIOErrorStatus* error_status)
+    {
+        return Composition_transformed_time(
+            (Composition*) self, time, to_item, error_status);
+    }
+    TimeRange* Track_transformed_time_range(
+        Track*           self,
+        TimeRange*       time_range,
+        Item*            to_item,
+        OTIOErrorStatus* error_status)
+    {
+        return Composition_transformed_time_range(
+            (Composition*) self, time_range, to_item, error_status);
+    }
+    Composition* Track_parent(Track* self)
+    {
+        return Composition_parent((Composition*) self);
+    }
+    const char* Track_name(Track* self)
+    {
+        return Composition_name((Composition*) self);
+    }
+    AnyDictionary* Track_metadata(Track* self)
+    {
+        return Composition_metadata((Composition*) self);
+    }
+    void Track_set_name(Track* self, const char* name)
+    {
+        return Composition_set_name((Composition*) self, name);
+    }
+    _Bool Track_possibly_delete(Track* self)
+    {
+        return Composition_possibly_delete((Composition*) self);
+    }
+    _Bool Track_to_json_file(
+        Track*           self,
+        const char*      file_name,
+        OTIOErrorStatus* error_status,
+        int              indent)
+    {
+        return Composition_to_json_file(
+            (Composition*) self, file_name, error_status, indent);
+    }
+    const char*
+    Track_to_json_string(Track* self, OTIOErrorStatus* error_status, int indent)
+    {
+        return Composition_to_json_string(
+            (Composition*) self, error_status, indent);
+    }
+    _Bool Track_is_equivalent_to(Track* self, SerializableObject* other)
+    {
+        return Composition_is_equivalent_to((Composition*) self, other);
+    }
+    Track* Track_clone(Track* self, OTIOErrorStatus* error_status)
+    {
+        return (Track*) Composition_clone((Composition*) self, error_status);
+    }
+    const char* Track_schema_name(Track* self)
+    {
+        return Composition_schema_name((Composition*) self);
+    }
+    int Track_schema_version(Track* self)
+    {
+        return Composition_schema_version((Composition*) self);
     }
 #ifdef __cplusplus
 }

1f877420d48cf0f2b5debdb5be05dd3f2b470d4d
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-06-21 12:55:57 +0530

    Wrap inherited methods in FreezeFrame

--- src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/freezeFrame.h
+++ src/c-opentimelineio/opentimelineio-bindings/include/copentimelineio/freezeFrame.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "anyDictionary.h"
+#include "errorStatus.h"
 #include <stdbool.h>
 
 #ifdef __cplusplus
@@ -11,6 +12,28 @@ extern "C"
     typedef struct FreezeFrame FreezeFrame;
 
     FreezeFrame* FreezeFrame_create(const char* name, AnyDictionary* metadata);
+    double       FreezeFrame_time_scalar(FreezeFrame* self);
+    void FreezeFrame_set_time_scalar(FreezeFrame* self, double time_scalar);
+    const char* FreezeFrame_effect_name(FreezeFrame* self);
+    void
+                   FreezeFrame_set_effect_name(FreezeFrame* self, const char* effect_name);
+    const char*    FreezeFrame_name(FreezeFrame* self);
+    void           FreezeFrame_set_name(FreezeFrame* self, const char* name);
+    AnyDictionary* FreezeFrame_metadata(FreezeFrame* self);
+    _Bool          FreezeFrame_possibly_delete(FreezeFrame* self);
+    _Bool          FreezeFrame_to_json_file(
+                 FreezeFrame*     self,
+                 const char*      file_name,
+                 OTIOErrorStatus* error_status,
+                 int              indent);
+    const char* FreezeFrame_to_json_string(
+        FreezeFrame* self, OTIOErrorStatus* error_status, int indent);
+    _Bool
+    FreezeFrame_is_equivalent_to(FreezeFrame* self, SerializableObject* other);
+    FreezeFrame*
+                FreezeFrame_clone(FreezeFrame* self, OTIOErrorStatus* error_status);
+    const char* FreezeFrame_schema_name(FreezeFrame* self);
+    int         FreezeFrame_schema_version(FreezeFrame* self);
 #ifdef __cplusplus
 }
 #endif
--- src/c-opentimelineio/opentimelineio-bindings/src/freezeFrame.cpp
+++ src/c-opentimelineio/opentimelineio-bindings/src/freezeFrame.cpp
@@ -1,4 +1,6 @@
 #include "copentimelineio/freezeFrame.h"
+#include <copentimelineio/effect.h>
+#include <copentimelineio/linearTimeWarp.h>
 #include <opentimelineio/anyDictionary.h>
 #include <opentimelineio/freezeFrame.h>
 
@@ -18,6 +20,70 @@ extern "C"
 
         return reinterpret_cast<FreezeFrame*>(
             new OTIO_NS::FreezeFrame(name_str, metadataDictionary));
+    }
+    double FreezeFrame_time_scalar(FreezeFrame* self)
+    {
+        return LinearTimeWarp_time_scalar((LinearTimeWarp*) self);
+    }
+    void FreezeFrame_set_time_scalar(FreezeFrame* self, double time_scalar)
+    {
+        LinearTimeWarp_set_time_scalar((LinearTimeWarp*) self, time_scalar);
+    }
+    const char* FreezeFrame_effect_name(FreezeFrame* self)
+    {
+        return Effect_effect_name((Effect*) self);
+    }
+    void FreezeFrame_set_effect_name(FreezeFrame* self, const char* effect_name)
+    {
+        Effect_set_effect_name((Effect*) self, effect_name);
+    }
+    const char* FreezeFrame_name(FreezeFrame* self)
+    {
+        return Effect_name((Effect*) self);
+    }
+    void FreezeFrame_set_name(FreezeFrame* self, const char* name)
+    {
+        Effect_set_name((Effect*) self, name);
+    }
+    AnyDictionary* FreezeFrame_metadata(FreezeFrame* self)
+    {
+        return Effect_metadata((Effect*) self);
+    }
+    _Bool FreezeFrame_possibly_delete(FreezeFrame* self)
+    {
+        return Effect_possibly_delete((Effect*) self);
+    }
+    _Bool FreezeFrame_to_json_file(
+        FreezeFrame*     self,
+        const char*      file_name,
+        OTIOErrorStatus* error_status,
+        int              indent)
+    {
+        return Effect_to_json_file(
+            (Effect*) self, file_name, error_status, indent);
+    }
+    const char* FreezeFrame_to_json_string(
+        FreezeFrame* self, OTIOErrorStatus* error_status, int indent)
+    {
+        return Effect_to_json_string((Effect*) self, error_status, indent);
+    }
+    _Bool
+    FreezeFrame_is_equivalent_to(FreezeFrame* self, SerializableObject* other)
+    {
+        return Effect_is_equivalent_to((Effect*) self, other);
+    }
+    FreezeFrame*
+    FreezeFrame_clone(FreezeFrame* self, OTIOErrorStatus* error_status)
+    {
+        return (FreezeFrame*) Effect_clone((Effect*) self, error_status);
+    }
+    const char* FreezeFrame_schema_name(FreezeFrame* self)
+    {
+        return Effect_schema_name((Effect*) self);
+    }
+    int FreezeFrame_schema_version(FreezeFrame* self)
+    {
+        return Effect_schema_version((Effect*) self);
     }
 #ifdef __cplusplus
 }

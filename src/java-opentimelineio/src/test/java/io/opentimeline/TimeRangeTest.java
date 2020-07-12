672e85aef9c1ce069dbb649ce277402742c3ff89
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-07-12 17:17:25 +0530

    Complete TimeRange tests

--- src/java-opentimelineio/src/test/java/io/opentimeline/TimeRangeTest.java
+++ src/java-opentimelineio/src/test/java/io/opentimeline/TimeRangeTest.java
@@ -2,6 +2,8 @@ package io.opentimeline;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.opentimeline.opentime.ErrorStatus;
+import io.opentimeline.opentime.IsDropFrameRate;
 import io.opentimeline.opentime.RationalTime;
 import io.opentimeline.opentime.TimeRange;
 import org.junit.jupiter.api.Test;
@@ -182,5 +184,187 @@ public class TimeRangeTest {
         tr_t = new TimeRange(tStart, tDur);
         assertFalse(tr_t.before(tr));
         assertFalse(tr.before(tr_t));
+    }
+
+    @Test
+    public void testBeforeRationalTime() {
+        RationalTime tAfter = new RationalTime(15, 25);
+        RationalTime tStart = new RationalTime(12, 25);
+        RationalTime tDur = new RationalTime(3, 25);
+        TimeRange tr = new TimeRange(tStart, tDur);
+
+        assertFalse(tr.before(tAfter));
+        assertFalse(tr.before(tStart));
+
+        tDur = new RationalTime(1.99, 25);
+        tr = new TimeRange(tStart, tDur);
+        assertTrue(tr.before(tAfter));
+    }
+
+    @Test
+    public void testMeets() {
+        RationalTime tStart = new RationalTime(12, 25);
+        RationalTime tDur = new RationalTime(3, 25);
+        TimeRange tr = new TimeRange(tStart, tDur);
+        tStart = new RationalTime(15, 25);
+        TimeRange tr_t = new TimeRange(tStart, tDur);
+
+        assertTrue(tr.meets(tr_t));
+        assertFalse(tr_t.meets(tr));
+
+        tStart = new RationalTime(14.99, 25);
+        tDur = new RationalTime(0, 25);
+        tr_t = new TimeRange(tStart, tDur);
+
+        assertTrue(tr_t.meets(tr_t));
+    }
+
+    @Test
+    public void testBeginsTimeRange() {
+        RationalTime tStart = new RationalTime(12, 25);
+        RationalTime tDur = new RationalTime(3, 25);
+        TimeRange tr = new TimeRange(tStart, tDur);
+        tDur = new RationalTime(5, 25);
+        TimeRange tr_t = new TimeRange(tStart, tDur);
+
+        assertTrue(tr.begins(tr_t));
+        assertFalse(tr_t.begins(tr));
+        assertFalse(tr.begins(tr));
+
+        tDur = new RationalTime(0, 25);
+        tr = new TimeRange(tStart, tDur);
+        assertTrue(tr.begins(tr_t));
+        assertFalse(tr.begins(tr));
+
+        tStart = new RationalTime(30, 25);
+        tr_t = new TimeRange(tStart, tDur);
+        assertFalse(tr.begins(tr_t));
+
+        tStart = new RationalTime(13, 25);
+        tr_t = new TimeRange(tStart, tDur);
+        tDur = new RationalTime(3, 25);
+        tStart = new RationalTime(12, 25);
+        tr = new TimeRange(tStart, tDur);
+        assertFalse(tr_t.begins(tr));
+    }
+
+    @Test
+    public void testBeginsRationalTime() {
+        RationalTime tEnd = new RationalTime(15, 25);
+        RationalTime tBefore = new RationalTime(11.9, 25);
+        RationalTime tStart = new RationalTime(12, 25);
+        RationalTime tDur = new RationalTime(3, 25);
+        TimeRange tr = new TimeRange(tStart, tDur);
+
+        assertTrue(tr.begins(tStart));
+        assertFalse(tr.begins(tEnd));
+        assertFalse(tr.begins(tBefore));
+    }
+
+    @Test
+    public void testFinishesTimeRange() {
+        RationalTime tStart = new RationalTime(12, 25);
+        RationalTime tDur = new RationalTime(3, 25);
+        TimeRange tr = new TimeRange(tStart, tDur);
+        tStart = new RationalTime(13, 25);
+        tDur = new RationalTime(2, 25);
+        TimeRange tr_t = new TimeRange(tStart, tDur);
+
+        assertTrue(tr_t.finishes(tr));
+        assertFalse(tr.finishes(tr_t));
+        assertFalse(tr.finishes(tr));
+
+        tDur = new RationalTime(1, 25);
+        tr_t = new TimeRange(tStart, tDur);
+        assertFalse(tr_t.finishes(tr));
+
+        tStart = new RationalTime(30, 25);
+        tr_t = new TimeRange(tStart, tDur);
+        assertFalse(tr_t.finishes(tr));
+
+        tStart = new RationalTime(15, 25);
+        tDur = new RationalTime(0, 25);
+        tr_t = new TimeRange(tStart, tDur);
+        assertTrue(tr_t.finishes(tr));
+    }
+
+    @Test
+    public void testFinishesRationalTime() {
+        RationalTime tAfter = new RationalTime(16, 25);
+        RationalTime tEnd = new RationalTime(15, 25);
+        RationalTime tStart = new RationalTime(12, 25);
+        RationalTime tDur = new RationalTime(3, 25);
+        TimeRange tr = new TimeRange(tStart, tDur);
+
+        assertTrue(tr.finishes(tEnd));
+        assertFalse(tr.finishes(tStart));
+        assertFalse(tr.finishes(tAfter));
+    }
+
+    @Test
+    public void testRangeFromStartEndTime() {
+        RationalTime tStart = new RationalTime(0, 25);
+        RationalTime tEnd = new RationalTime(12, 25);
+
+        TimeRange tr = TimeRange.rangeFromStartEndTime(tStart, tEnd);
+
+        assertTrue(tr.getStartTime().equals(tStart));
+        assertTrue(tr.getDuration().equals(tEnd));
+
+        assertTrue(tr.endTimeExclusive().equals(tEnd));
+        assertTrue(tr.endTimeInclusive().equals(tEnd.subtract(new RationalTime(1, 25))));
+
+        assertTrue(tr.equals(TimeRange.rangeFromStartEndTime(tr.getStartTime(), tr.endTimeExclusive())));
+    }
+
+    @Test
+    public void testAdjacentTimeRanges() {
+        double d1 = 0.3;
+        double d2 = 0.4;
+
+        TimeRange r1 = new TimeRange(new RationalTime(0, 1), new RationalTime(d1, 1));
+        TimeRange r2 = new TimeRange(r1.endTimeExclusive(), new RationalTime(d2, 1));
+        TimeRange full = new TimeRange(new RationalTime(0, 1), new RationalTime(d1 + d2, 1));
+
+        assertFalse(r1.overlaps(r2));
+        assertTrue(r1.extendedBy(r2).equals(full));
+    }
+
+    @Test
+    public void testDistantTimeRanges() {
+        double start = 0.1;
+        double d1 = 0.3;
+        double gap = 1.7;
+        double d2 = 0.4;
+        TimeRange r1 = new TimeRange(new RationalTime(start, 1), new RationalTime(d1, 1));
+        TimeRange r2 = new TimeRange(new RationalTime(start + gap + d1, 1), new RationalTime(d2, 1));
+        TimeRange full = new TimeRange(new RationalTime(start, 1), new RationalTime(d1 + gap + d2, 1));
+        assertFalse(r1.overlaps(r2));
+        assertTrue(full.equals(r1.extendedBy(r2)));
+        assertTrue(full.equals(r2.extendedBy(r1)));
+    }
+
+    @Test
+    public void testToTimecodeMixedRates() {
+        String timecode = "00:06:56:17";
+        ErrorStatus errorStatus = new ErrorStatus();
+        RationalTime t = RationalTime.fromTimecode(timecode, 24, errorStatus);
+
+        assertEquals(timecode, t.toTimecode(errorStatus));
+        assertEquals(timecode, t.toTimecode(24, IsDropFrameRate.InferFromRate, errorStatus));
+        assertNotEquals(timecode, t.toTimecode(12, IsDropFrameRate.InferFromRate, errorStatus));
+
+        RationalTime time1 = new RationalTime(24, 24);
+        RationalTime time2 = new RationalTime(1, 1);
+        assertEquals(time1.toTimecode(24, IsDropFrameRate.InferFromRate, errorStatus), time2.toTimecode(24, IsDropFrameRate.InferFromRate, errorStatus));
+    }
+
+    @Test
+    public void testToFramesMixedRates() {
+        int frame = 100;
+        RationalTime t = RationalTime.fromFrames(frame, 24);
+        assertEquals(frame, t.toFrames());
+        assertEquals(frame, t.toFrames(24));
+        assertNotEquals(frame, t.toFrames(12));
     }
 }

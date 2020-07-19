73aa2dd0bcfe74832237e3096c1602e7ca2b2d22
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-07-20 02:54:15 +0530

    Wrap Marker

---
+++ src/java-opentimelineio/src/main/cpp/io_opentimeline_opentimelineio_Marker.cpp
@@ -0,0 +1,83 @@
+#include <exceptions.h>
+#include <handle.h>
+#include <io_opentimeline_opentimelineio_Marker.h>
+#include <opentimelineio/marker.h>
+#include <opentimelineio/version.h>
+#include <utilities.h>
+
+/*
+ * Class:     io_opentimeline_opentimelineio_Marker
+ * Method:    initialize
+ * Signature: (Ljava/lang/String;[DLjava/lang/String;Lio/opentimeline/opentimelineio/AnyDictionary;)V
+ */
+JNIEXPORT void JNICALL
+Java_io_opentimeline_opentimelineio_Marker_initialize(
+    JNIEnv*      env,
+    jobject      thisObj,
+    jstring      name,
+    jdoubleArray markedRangeArray,
+    jstring      color,
+    jobject      metadata)
+{
+    std::string         nameStr     = env->GetStringUTFChars(name, 0);
+    opentime::TimeRange markedRange = timeRangeFromArray(env, markedRangeArray);
+    std::string         colorStr    = env->GetStringUTFChars(color, 0);
+    auto metadataHandle = getHandle<OTIO_NS::AnyDictionary>(env, metadata);
+    auto marker =
+        new OTIO_NS::Marker(nameStr, markedRange, colorStr, *metadataHandle);
+    setHandle(env, thisObj, marker);
+}
+
+/*
+ * Class:     io_opentimeline_opentimelineio_Marker
+ * Method:    getColor
+ * Signature: ()Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL
+Java_io_opentimeline_opentimelineio_Marker_getColor(
+    JNIEnv* env, jobject thisObj)
+{
+    auto thisHandle = getHandle<OTIO_NS::Marker>(env, thisObj);
+    return env->NewStringUTF(thisHandle->color().c_str());
+}
+
+/*
+ * Class:     io_opentimeline_opentimelineio_Marker
+ * Method:    setColor
+ * Signature: (Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL
+Java_io_opentimeline_opentimelineio_Marker_setColor(
+    JNIEnv* env, jobject thisObj, jstring color)
+{
+    auto thisHandle = getHandle<OTIO_NS::Marker>(env, thisObj);
+    thisHandle->set_color(env->GetStringUTFChars(color, 0));
+}
+
+/*
+ * Class:     io_opentimeline_opentimelineio_Marker
+ * Method:    getMarkedRangeNative
+ * Signature: ()[D
+ */
+JNIEXPORT jdoubleArray JNICALL
+Java_io_opentimeline_opentimelineio_Marker_getMarkedRangeNative(
+    JNIEnv* env, jobject thisObj)
+{
+    auto thisHandle = getHandle<OTIO_NS::Marker>(env, thisObj);
+    auto result     = thisHandle->marked_range();
+    return timeRangeToArray(env, result);
+}
+
+/*
+ * Class:     io_opentimeline_opentimelineio_Marker
+ * Method:    setMarkedRangeNative
+ * Signature: ([D)V
+ */
+JNIEXPORT void JNICALL
+Java_io_opentimeline_opentimelineio_Marker_setMarkedRangeNative(
+    JNIEnv* env, jobject thisObj, jdoubleArray markedRange)
+{
+    auto thisHandle = getHandle<OTIO_NS::Marker>(env, thisObj);
+    auto mr         = timeRangeFromArray(env, markedRange);
+    thisHandle->set_marked_range(mr);
+}
---
+++ src/java-opentimelineio/src/main/java/io/opentimeline/opentimelineio/Marker.java
@@ -0,0 +1,90 @@
+package io.opentimeline.opentimelineio;
+
+import io.opentimeline.opentime.TimeRange;
+
+public class Marker extends SerializableObjectWithMetadata {
+
+    public static class Color {
+        public static String pink = "PINK";
+        public static String red = "RED";
+        public static String orange = "ORANGE";
+        public static String yellow = "YELLOW";
+        public static String green = "GREEN";
+        public static String cyan = "CYAN";
+        public static String blue = "BLUE";
+        public static String purple = "PURPLE";
+        public static String magenta = "MAGENTA";
+        public static String black = "BLACK";
+        public static String white = "WHITE";
+    }
+
+    protected Marker() {
+    }
+
+    public Marker(String name, TimeRange markedRange, String color, AnyDictionary metadata) {
+        this.initObject(name, markedRange, color, metadata);
+    }
+
+    public Marker(MarkerBuilder markerBuilder) {
+        this.initObject(
+                markerBuilder.name, markerBuilder.markedRange,
+                markerBuilder.color, markerBuilder.metadata);
+    }
+
+    private void initObject(String name, TimeRange markedRange, String color, AnyDictionary metadata) {
+        this.className = this.getClass().getCanonicalName();
+        this.initialize(name, TimeRange.timeRangeToArray(markedRange), color, metadata);
+    }
+
+    private native void initialize(String name, double[] markedRange, String color, AnyDictionary metadata);
+
+    public static class MarkerBuilder {
+        private String name = "";
+        private TimeRange markedRange = new TimeRange();
+        private String color = Color.green;
+        private AnyDictionary metadata = new AnyDictionary();
+
+        public MarkerBuilder() {
+        }
+
+        public Marker.MarkerBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Marker.MarkerBuilder setMarkedRange(TimeRange markedRange) {
+            this.markedRange = markedRange;
+            return this;
+        }
+
+        public Marker.MarkerBuilder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Marker.MarkerBuilder setMetadata(AnyDictionary metadata) {
+            this.metadata = metadata;
+            return this;
+        }
+
+        public Marker build() {
+            return new Marker(this);
+        }
+    }
+
+    public native String getColor();
+
+    public native void setColor(String color);
+
+    public TimeRange getMarkedRange() {
+        return TimeRange.timeRangeFromArray(getMarkedRangeNative());
+    }
+
+    private native double[] getMarkedRangeNative();
+
+    public void setMarkedRange(TimeRange markedRange) {
+        setMarkedRangeNative(TimeRange.timeRangeToArray(markedRange));
+    }
+
+    private native void setMarkedRangeNative(double[] markedRange);
+}

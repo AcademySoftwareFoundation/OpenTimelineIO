f5f45e1703fb3caa6817cb29b5c363d33d055b2e
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-07-20 17:07:34 +0530

    Wrap ExternalReference

---
+++ src/java-opentimelineio/src/main/cpp/io_opentimeline_opentimelineio_ExternalReference.cpp
@@ -0,0 +1,57 @@
+#include <exceptions.h>
+#include <handle.h>
+#include <io_opentimeline_opentimelineio_ExternalReference.h>
+#include <opentimelineio/externalReference.h>
+#include <opentimelineio/optional.h>
+#include <opentimelineio/version.h>
+#include <utilities.h>
+
+/*
+ * Class:     io_opentimeline_opentimelineio_ExternalReference
+ * Method:    initialize
+ * Signature: (Ljava/lang/String;[DLio/opentimeline/opentimelineio/AnyDictionary;)V
+ */
+JNIEXPORT void JNICALL
+Java_io_opentimeline_opentimelineio_ExternalReference_initialize(
+    JNIEnv*      env,
+    jobject      thisObj,
+    jstring      targetURL,
+    jdoubleArray availableRangeArray,
+    jobject      metadata)
+{
+    std::string targetURLString = env->GetStringUTFChars(targetURL, 0);
+    OTIO_NS::optional<opentime::TimeRange> availableRange = OTIO_NS::nullopt;
+    if(env->GetArrayLength(availableRangeArray) != 0)
+    { availableRange = timeRangeFromArray(env, availableRangeArray); }
+    auto metadataHandle    = getHandle<OTIO_NS::AnyDictionary>(env, metadata);
+    auto externalReference = new OTIO_NS::ExternalReference(
+        targetURLString, availableRange, *metadataHandle);
+    setHandle(env, thisObj, externalReference);
+}
+
+/*
+ * Class:     io_opentimeline_opentimelineio_ExternalReference
+ * Method:    getTargetURL
+ * Signature: ()Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL
+Java_io_opentimeline_opentimelineio_ExternalReference_getTargetURL(
+    JNIEnv* env, jobject thisObj)
+{
+    auto thisHandle = getHandle<OTIO_NS::ExternalReference>(env, thisObj);
+    return env->NewStringUTF(thisHandle->target_url().c_str());
+}
+
+/*
+ * Class:     io_opentimeline_opentimelineio_ExternalReference
+ * Method:    setTargetURL
+ * Signature: (Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL
+Java_io_opentimeline_opentimelineio_ExternalReference_setTargetURL(
+    JNIEnv* env, jobject thisObj, jstring targetURL)
+{
+    auto thisHandle = getHandle<OTIO_NS::ExternalReference>(env, thisObj);
+    std::string targetURLStr = env->GetStringUTFChars(targetURL, 0);
+    thisHandle->set_target_url(targetURLStr);
+}
---
+++ src/java-opentimelineio/src/main/java/io/opentimeline/opentimelineio/ExternalReference.java
@@ -0,0 +1,65 @@
+package io.opentimeline.opentimelineio;
+
+import io.opentimeline.opentime.TimeRange;
+
+public class ExternalReference extends MediaReference {
+
+    protected ExternalReference() {
+    }
+
+    public ExternalReference(String targetURL, TimeRange availableRange, AnyDictionary metadata) {
+        this.initObject(targetURL, availableRange, metadata);
+    }
+
+    public ExternalReference(ExternalReference.ExternalReferenceBuilder mediaReferenceBuilder) {
+        this.initObject(
+                mediaReferenceBuilder.targetURL,
+                mediaReferenceBuilder.availableRange,
+                mediaReferenceBuilder.metadata);
+    }
+
+    private void initObject(String targetURL, TimeRange availableRange, AnyDictionary metadata) {
+        this.className = this.getClass().getCanonicalName();
+        if (availableRange != null) {
+            this.initialize(targetURL, TimeRange.timeRangeToArray(availableRange), metadata);
+        } else {
+            this.initialize(targetURL, new double[]{}, metadata);
+        }
+    }
+
+    private native void initialize(String name, double[] availableRange, AnyDictionary metadata);
+
+    public static class ExternalReferenceBuilder {
+        private String targetURL = "";
+        private TimeRange availableRange = null;
+        private AnyDictionary metadata = new AnyDictionary();
+
+        public ExternalReferenceBuilder() {
+        }
+
+        public ExternalReference.ExternalReferenceBuilder setTargetURL(String targetURL) {
+            this.targetURL = targetURL;
+            return this;
+        }
+
+        public ExternalReference.ExternalReferenceBuilder setAvailableRange(TimeRange availableRange) {
+            this.availableRange = availableRange;
+            return this;
+        }
+
+
+        public ExternalReference.ExternalReferenceBuilder setMetadata(AnyDictionary metadata) {
+            this.metadata = metadata;
+            return this;
+        }
+
+        public ExternalReference build() {
+            return new ExternalReference(this);
+        }
+    }
+
+    public native String getTargetURL();
+
+    public native void setTargetURL(String targetURL);
+
+}

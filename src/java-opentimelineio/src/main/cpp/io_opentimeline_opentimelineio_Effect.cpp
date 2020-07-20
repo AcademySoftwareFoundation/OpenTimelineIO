326ae6b323ccb69d6315a21ab500e7716c510dd5
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-07-20 21:15:38 +0530

    Wrap Effect

---
+++ src/java-opentimelineio/src/main/cpp/io_opentimeline_opentimelineio_Effect.cpp
@@ -0,0 +1,52 @@
+#include <exceptions.h>
+#include <handle.h>
+#include <io_opentimeline_opentimelineio_Effect.h>
+#include <opentimelineio/effect.h>
+#include <opentimelineio/version.h>
+#include <utilities.h>
+
+/*
+ * Class:     io_opentimeline_opentimelineio_Effect
+ * Method:    initialize
+ * Signature: (Ljava/lang/String;Ljava/lang/String;Lio/opentimeline/opentimelineio/AnyDictionary;)V
+ */
+JNIEXPORT void JNICALL
+Java_io_opentimeline_opentimelineio_Effect_initialize(
+    JNIEnv* env,
+    jobject thisObj,
+    jstring name,
+    jstring effectName,
+    jobject metadataObj)
+{
+    std::string nameStr       = env->GetStringUTFChars(name, 0);
+    std::string effectNameStr = env->GetStringUTFChars(effectName, 0);
+    auto metadataHandle = getHandle<OTIO_NS::AnyDictionary>(env, metadataObj);
+    auto effect = new OTIO_NS::Effect(nameStr, effectNameStr, *metadataHandle);
+    setHandle(env, thisObj, effect);
+}
+
+/*
+ * Class:     io_opentimeline_opentimelineio_Effect
+ * Method:    getEffectName
+ * Signature: ()Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL
+Java_io_opentimeline_opentimelineio_Effect_getEffectName(
+    JNIEnv* env, jobject thisObj)
+{
+    auto thisHandle = getHandle<OTIO_NS::Effect>(env, thisObj);
+    return env->NewStringUTF(thisHandle->effect_name().c_str());
+}
+
+/*
+ * Class:     io_opentimeline_opentimelineio_Effect
+ * Method:    setEffectName
+ * Signature: (Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL
+Java_io_opentimeline_opentimelineio_Effect_setEffectName(
+    JNIEnv* env, jobject thisObj, jstring effectName)
+{
+    auto thisHandle = getHandle<OTIO_NS::Effect>(env, thisObj);
+    thisHandle->set_effect_name(env->GetStringUTFChars(effectName, 0));
+}
---
+++ src/java-opentimelineio/src/main/java/io/opentimeline/opentimelineio/Effect.java
@@ -0,0 +1,55 @@
+package io.opentimeline.opentimelineio;
+
+public class Effect extends SerializableObjectWithMetadata {
+
+    protected Effect() {
+    }
+
+    public Effect(String name, String effectName, AnyDictionary metadata) {
+        this.initObject(name, effectName, metadata);
+    }
+
+    public Effect(Effect.EffectBuilder builder) {
+        this.initObject(builder.name, builder.effectName, builder.metadata);
+    }
+
+    private void initObject(String name, String effectName, AnyDictionary metadata) {
+        this.className = this.getClass().getCanonicalName();
+        this.initialize(name, effectName, metadata);
+    }
+
+    private native void initialize(String name, String effectName, AnyDictionary metadata);
+
+    public static class EffectBuilder {
+        private String name = "";
+        private String effectName = "";
+        private AnyDictionary metadata = new AnyDictionary();
+
+        public EffectBuilder() {
+        }
+
+        public Effect.EffectBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Effect.EffectBuilder setEffectName(String effectName) {
+            this.effectName = effectName;
+            return this;
+        }
+
+        public Effect.EffectBuilder setMetadata(AnyDictionary metadata) {
+            this.metadata = metadata;
+            return this;
+        }
+
+        public Effect build() {
+            return new Effect(this);
+        }
+    }
+
+    public native String getEffectName();
+
+    public native void setEffectName(String effectName);
+
+}

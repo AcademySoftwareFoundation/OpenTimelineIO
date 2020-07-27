5c53eb9ccb1ac48f2ea599f45c0d94ca86f76a66
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-07-27 21:07:18 +0530

    Add MediaReference test

---
+++ src/java-opentimelineio/src/test/java/io/opentimeline/MediaReferenceTest.java
@@ -0,0 +1,106 @@
+package io.opentimeline;
+
+import io.opentimeline.opentime.RationalTime;
+import io.opentimeline.opentime.TimeRange;
+import io.opentimeline.opentimelineio.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MediaReferenceTest {
+
+    @Test
+    public void testConstructor() {
+        try {
+            TimeRange tr = new TimeRange(
+                    new RationalTime(5, 24),
+                    new RationalTime(10, 24));
+            AnyDictionary metadata = new AnyDictionary();
+            metadata.put("show", new Any("OTIOTheMovie"));
+            MissingReference mr = new MissingReference.MissingReferenceBuilder()
+                    .setAvailableRange(tr)
+                    .setMetadata(metadata)
+                    .build();
+
+            assertTrue(mr.getAvailableRange().equals(tr));
+            mr.close();
+            mr = new MissingReference.MissingReferenceBuilder().build();
+            assertNull(mr.getAvailableRange());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSerialization() {
+
+        try {
+            MissingReference mr = new MissingReference.MissingReferenceBuilder().build();
+            Any mrAny = new Any(mr);
+            ErrorStatus errorStatus = new ErrorStatus();
+            String encoded = Serialization.serializeJSONToString(mrAny, errorStatus);
+            SerializableObject decoded = SerializableObject.fromJSONString(encoded, errorStatus);
+            assertTrue(decoded.isEquivalentTo(mr));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testFilepath() {
+        try {
+            ExternalReference mr = new ExternalReference.ExternalReferenceBuilder()
+                    .setTargetURL("/var/tmp/foo.mov")
+                    .build();
+            Any mrAny = new Any(mr);
+            ErrorStatus errorStatus = new ErrorStatus();
+            String encoded = Serialization.serializeJSONToString(mrAny, errorStatus);
+            SerializableObject decoded = SerializableObject.fromJSONString(encoded, errorStatus);
+            assertTrue(decoded.isEquivalentTo(mr));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testEq() {
+        try {
+            ExternalReference mr1 = new ExternalReference.ExternalReferenceBuilder()
+                    .setTargetURL("/var/tmp/foo.mov")
+                    .build();
+            ExternalReference mr2 = new ExternalReference.ExternalReferenceBuilder()
+                    .setTargetURL("/var/tmp/foo.mov")
+                    .build();
+            assertTrue(mr1.isEquivalentTo(mr2));
+
+            MissingReference bl = new MissingReference.MissingReferenceBuilder().build();
+            assertFalse(bl.isEquivalentTo(mr1));
+//            mr2.close(); //segfault
+            mr2 = new ExternalReference.ExternalReferenceBuilder()
+                    .setTargetURL("/var/tmp/foo2.mov")
+                    .build();
+            mr1 = new ExternalReference.ExternalReferenceBuilder()
+                    .setTargetURL("/var/tmp/foo.mov")
+                    .build();
+            assertFalse(mr1.isEquivalentTo(mr2));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testIsMissing() {
+        try {
+            ExternalReference mr = new ExternalReference.ExternalReferenceBuilder()
+                    .setTargetURL("/var/tmp/foo.mov")
+                    .build();
+            assertFalse(mr.isMissingReference());
+            MissingReference mr2 = new MissingReference.MissingReferenceBuilder()
+                    .build();
+            assertTrue(mr2.isMissingReference());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

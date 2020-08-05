1d1915794655e95ead73bb950feda43a92909c8d
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2020-08-05 19:52:19 +0530

    Initialize type_dispatch_table on first-reference using std::call_once

--- src/java-opentimelineio/src/main/include/utilities.h
+++ src/java-opentimelineio/src/main/include/utilities.h
@@ -112,20 +112,40 @@ trackVectorFromArray(JNIEnv *env, jobjectArray array) {
     return objectVector;
 }
 
-inline std::string getAnyType(const std::type_info &typeInfo) {
-    if (typeInfo == typeid(bool)) return "java.lang.Boolean";
-    else if (typeInfo == typeid(int)) return "java.lang.Integer";
-    else if (typeInfo == typeid(long)) return "java.lang.Long";
-    else if (typeInfo == typeid(double)) return "java.lang.Double";
-    else if (typeInfo == typeid(std::string)) return "java.lang.String";
-    else if (typeInfo == typeid(OTIO_NS::RationalTime)) return "io.opentimeline.opentime.RationalTime";
-    else if (typeInfo == typeid(OTIO_NS::TimeRange)) return "io.opentimeline.opentime.TimeRange";
-    else if (typeInfo == typeid(OTIO_NS::TimeTransform)) return "io.opentimeline.opentime.TimeTransform";
-    else if (typeInfo == typeid(OTIO_NS::AnyDictionary)) return "io.opentimeline.opentimelineio.AnyDictionary";
-    else if (typeInfo == typeid(OTIO_NS::AnyVector)) return "io.opentimeline.opentimelineio.AnyVector";
-    else if (typeInfo == typeid(OTIO_NS::SerializableObject::Retainer<>))
-        return "io.opentimeline.opentimelineio.SerializableObject";
-    else return "";
+inline std::string getAnyType(std::type_info const * typeInfo) {
+    static std::once_flag typeFlag;
+    static std::unique_ptr<std::map<std::type_info const *, std::string>> type_dispatch_table;
+    std::call_once(typeFlag, []() {
+        table = std::unique_ptr<std::map<std::type_info const *, std::string>>(
+                new std::map<std::type_info const *, std::string>());
+        (*type_dispatch_table)[&typeid(bool)] = "java.lang.Boolean";
+        (*type_dispatch_table)[&typeid(int)] = "java.lang.Integer";
+        (*type_dispatch_table)[&typeid(long)] = "java.lang.Long";
+        (*type_dispatch_table)[&typeid(double)] = "java.lang.Double";
+        (*type_dispatch_table)[&typeid(std::string)] = "java.lang.String";
+        (*type_dispatch_table)[&typeid(OTIO_NS::RationalTime)] = "io.opentimeline.opentime.RationalTime";
+        (*type_dispatch_table)[&typeid(OTIO_NS::TimeRange)] = "io.opentimeline.opentime.TimeRange";
+        (*type_dispatch_table)[&typeid(OTIO_NS::TimeTransform)] = "io.opentimeline.opentime.TimeTransform";
+        (*type_dispatch_table)[&typeid(OTIO_NS::AnyDictionary)] = "io.opentimeline.opentimelineio.AnyDictionary";
+        (*type_dispatch_table)[&typeid(OTIO_NS::AnyVector)] = "io.opentimeline.opentimelineio.AnyVector";
+        (*type_dispatch_table)[&typeid(OTIO_NS::SerializableObject::Retainer<>)] = "io.opentimeline.opentimelineio.SerializableObject";
+    });
+
+    return (*type_dispatch_table)[typeInfo];
+
+//    if (typeInfo == typeid(bool)) return "java.lang.Boolean";
+//    else if (typeInfo == typeid(int)) return "java.lang.Integer";
+//    else if (typeInfo == typeid(long)) return "java.lang.Long";
+//    else if (typeInfo == typeid(double)) return "java.lang.Double";
+//    else if (typeInfo == typeid(std::string)) return "java.lang.String";
+//    else if (typeInfo == typeid(OTIO_NS::RationalTime)) return "io.opentimeline.opentime.RationalTime";
+//    else if (typeInfo == typeid(OTIO_NS::TimeRange)) return "io.opentimeline.opentime.TimeRange";
+//    else if (typeInfo == typeid(OTIO_NS::TimeTransform)) return "io.opentimeline.opentime.TimeTransform";
+//    else if (typeInfo == typeid(OTIO_NS::AnyDictionary)) return "io.opentimeline.opentimelineio.AnyDictionary";
+//    else if (typeInfo == typeid(OTIO_NS::AnyVector)) return "io.opentimeline.opentimelineio.AnyVector";
+//    else if (typeInfo == typeid(OTIO_NS::SerializableObject::Retainer<>))
+//        return "io.opentimeline.opentimelineio.SerializableObject";
+//    else return "";
 }
 
 /* this deepcopies any */
@@ -135,7 +155,7 @@ anyFromNative(JNIEnv *env, OTIO_NS::any *native) {
     if (cls == NULL) return NULL;
 
 //    std::string anyType = _type_dispatch_table[&native->type()];
-    std::string anyType = getAnyType(native->type());
+    std::string anyType = getAnyType(&native->type());
     // Get the Method ID of the constructor which takes an otioNative
     jmethodID anyInit = env->GetMethodID(cls, "<init>", "(Lio/opentimeline/OTIONative;)V");
     if (NULL == anyInit) return NULL;

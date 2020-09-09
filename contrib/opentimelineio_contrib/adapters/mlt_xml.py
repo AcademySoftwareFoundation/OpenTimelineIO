483e0ceb22cc900eefd5a63039f7d5428c65e6d7
apetrynet <flehenrheener@gmail.com>
2020-09-09 22:40:19 +0200

    * renamed a few functions and variables * kept DRY a few places * added comments * added some doc strings

--- contrib/opentimelineio_contrib/adapters/mlt_xml.py
+++ contrib/opentimelineio_contrib/adapters/mlt_xml.py
@@ -60,7 +60,7 @@ playlists = []
 transitions = []
 
 
-def create_property(name, text=None, attrib=None):
+def create_property_element(name, text=None, attrib=None):
     property_e = et.Element('property', name=name)
     if text is not None:
         property_e.text = str(text)
@@ -79,52 +79,46 @@ def create_solid(color, length):
         attrib={'in': '0', 'out': str(length - 1)}
         )
 
-    color_e.append(create_property('length', length))
-    color_e.append(create_property('eof', 'pause'))
-    color_e.append(create_property('resource', color))
-    color_e.append(create_property('mlt_service', 'color'))
+    color_e.append(create_property_element('length', length))
+    color_e.append(create_property_element('eof', 'pause'))
+    color_e.append(create_property_element('resource', color))
+    color_e.append(create_property_element('mlt_service', 'color'))
 
     return color_e
 
 
-def get_producer(otio_clip, video_track=True):
+def get_producer(otio_item, video_track=True):
     target_url = None
     producer_e = None
     is_sequence = False
 
-    if isinstance(otio_clip, (otio.schema.Gap, otio.schema.Transition)):
+    if isinstance(otio_item, (otio.schema.Gap, otio.schema.Transition)):
         # Create a solid producer
         producer_e = create_solid(
             'black',
-            otio_clip.duration().value
+            otio_item.duration().value
         )
 
         id_ = producer_e.attrib['id']
 
-        # Add producer here since resource test below will fail.
-        producers['video'].setdefault(id_, producer_e)
-
-        # store producer in order list for insertion later
-        producers.setdefault('producer_order_', []).append(producer_e)
-
     else:
-        id_ = otio_clip.name
+        id_ = otio_item.name
 
-    if hasattr(otio_clip, 'media_reference') and otio_clip.media_reference:
-        id_ = otio_clip.media_reference.name or otio_clip.name
+    if hasattr(otio_item, 'media_reference') and otio_item.media_reference:
+        id_ = otio_item.media_reference.name or otio_item.name
 
-        if hasattr(otio_clip.media_reference, 'target_url'):
-            target_url = otio_clip.media_reference.target_url
+        if hasattr(otio_item.media_reference, 'target_url'):
+            target_url = otio_item.media_reference.target_url
 
-        elif hasattr(otio_clip.media_reference, 'abstract_target_url'):
+        elif hasattr(otio_item.media_reference, 'abstract_target_url'):
             is_sequence = True
-            target_url = otio_clip.media_reference.abstract_target_url(
+            target_url = otio_item.media_reference.abstract_target_url(
                 '%0{}d'.format(
-                    otio_clip.media_reference.frame_zero_padding
+                    otio_item.media_reference.frame_zero_padding
                 )
             )
             target_url += '?begin={}'.format(
-                otio_clip.media_reference.start_frame
+                otio_item.media_reference.start_frame
             )
 
     if producer_e is None:
@@ -147,12 +141,13 @@ def get_producer(otio_clip, video_track=True):
         target_url = id_
 
     property_e = producer.find('./property/[@name="resource"]')
-    if property_e is None:
-        resource = create_property(name='resource', text=target_url)
-        producer.append(resource)
+    if property_e is None or property_e.text == 'black':
+        if property_e is None:
+            resource = create_property_element(name='resource', text=target_url)
+            producer.append(resource)
 
         if is_sequence:
-            producer.append(create_property(name='mlt_service', text='pixbuf'))
+            producer.append(create_property_element(name='mlt_service', text='pixbuf'))
 
         # store producer in order list for insertion later
         producers.setdefault('producer_order_', []).append(producer)
@@ -179,8 +174,9 @@ def get_in_out_from_transition(transition):
     return in_, out_
 
 
-def create_transition(trans_track, name):
-    item_a, transition, item_b = trans_track
+def create_transition(trans_tuple, name):
+    # Expand parts of transition
+    item_a, transition, item_b = trans_tuple
 
     dur = transition.duration().value - 1
 
@@ -245,17 +241,17 @@ def create_transition(trans_track, name):
         id='transition_{}'.format(name),
         out=str(dur)
     )
-    trans_e.append(create_property('a_track', 0))
-    trans_e.append(create_property('b_track', 1))
-    trans_e.append(create_property('factory'))
-    trans_e.append(create_property('mlt_service', 'luma'))
+    trans_e.append(create_property_element('a_track', 0))
+    trans_e.append(create_property_element('b_track', 1))
+    trans_e.append(create_property_element('factory'))
+    trans_e.append(create_property_element('mlt_service', 'luma'))
 
     tractor_e.append(trans_e)
 
     return tractor_e
 
 
-def create_entry(producer, in_, out_):
+def create_entry_element(producer, in_, out_):
     clip_e = et.Element(
         'entry',
         producer=producer.attrib['id'],
@@ -272,12 +268,12 @@ def create_clip(item, producer):
     in_ = item.trimmed_range().start_time.value
     out_ = in_ + item.trimmed_range().duration.value
 
-    clip_e = create_entry(producer, in_, out_)
+    clip_e = create_entry_element(producer, in_, out_)
 
     return clip_e
 
 
-def create_blank(item):
+def create_blank_element(item):
     blank_e = et.Element(
         'blank',
         length=str(item.source_range.duration.value)
@@ -287,37 +283,55 @@ def create_blank(item):
 
 
 def apply_timewarp(item, item_e, effect):
+    """
+    Apply a time warp effect on a copy of a producer
+
+    :param item: source OTIO item in track
+    :param item_e: element tag to apply effect to
+    :param effect: OTIO effect object
+    :return:
+    """
+
     if item_e is None:
         return
 
+    # Create a copy of the producer
     orig_producer_e = get_producer(item)
     producer_e = deepcopy(orig_producer_e)
     id_ = None
 
     if effect.effect_name == 'FreezeFrame':
+        # Freeze frame will always use the first frame of the source_range as
+        # OTIO doesn't really have  any other way of indicating which frame was
+        # chosen to freeze
+
         id_ = '{}_freeze{}'.format(
             producer_e.attrib['id'],
             item.source_range.start_time.value
         )
 
         producer_e.attrib['id'] = id_
-        producer_e.append(create_property('mlt_service', 'hold'))
-        producer_e.append(create_property(
+        producer_e.append(create_property_element('mlt_service', 'hold'))
+        producer_e.append(create_property_element(
             'frame',
             str(item.source_range.start_time.value))
         )
 
     elif effect.effect_name == 'LinearTimeWarp':
-        id_ = ':'.join([str(effect.time_scalar), item_e.attrib.get('producer')])
+        id_ = ':'.join(
+            [str(effect.time_scalar), item_e.attrib.get('producer')]
+        )
         producer_e.attrib['id'] = id_
-        producer_e.append(create_property('mlt_service', 'timewarp'))
+        producer_e.append(create_property_element('mlt_service', 'timewarp'))
         resource_e = producer_e.find('./property/[@name="resource"]')
         resource_e.text = ':'.join([str(effect.time_scalar), resource_e.text])
 
+    # Add the new copy to the producers list
     if id_ not in producers['video']:
         producers['video'][id_] = producer_e
         producers['producer_order_'].append(producer_e)
 
+    # Swap the old producer with the new containing the effect
     item_e.attrib['producer'] = id_
 
 
@@ -337,7 +351,7 @@ def create_background_track(tracks, parent):
     )
     playlists.append(playlist_e)
 
-    playlist_e.append(create_entry(bg_e, 0, length - 1))
+    playlist_e.append(create_entry_element(bg_e, 0, length - 1))
 
     parent.append(
         et.Element('track', producer=playlist_e.attrib['id'])
@@ -351,7 +365,10 @@ def assemble_track(track, track_index, parent):
     )
     playlists.append(playlist_e)
 
+    # Transitions use track elements as children
     element_type = 'track'
+
+    # Playlists use entry
     if parent.tag == 'playlist':
         element_type = 'entry'
 
@@ -364,7 +381,7 @@ def assemble_track(track, track_index, parent):
     if hasattr(track, 'kind'):
         is_audio_track = track.kind == 'Audio'
 
-    # iterate over items in track
+    # Iterate over items in track, expanding transitions
     expanded_track = otio.algorithms.track_with_expanded_transitions(track)
     for item in expanded_track:
         item_e = None
@@ -373,8 +390,7 @@ def assemble_track(track, track_index, parent):
             producer_e = get_producer(item)
 
             if is_audio_track:
-                # TODO consider using resource.text
-                # Skip adding duplicate audio clips for matching video
+                # Skip adding duplicate audio source for matching video
                 if producer_e.attrib['id'] in producers['video']:
                     continue
 
@@ -382,10 +398,13 @@ def assemble_track(track, track_index, parent):
             playlist_e.append(item_e)
 
         elif isinstance(item, otio.schema.Gap):
-            item_e = create_blank(item)
+            item_e = create_blank_element(item)
             playlist_e.append(item_e)
 
         elif isinstance(item, tuple):
+            # Since we expanded transitions in the track the come as tuples
+            # containing (ClipA_t, Transition, ClipB_t)
+
             transition_e = create_transition(
                 item,
                 'transition_tractor{}'.format(len(transitions))
@@ -403,28 +422,31 @@ def assemble_track(track, track_index, parent):
                 )
             )
 
-            # Continue as there's no need to preform the tests below
+            # Continue as transitions have no effects, see test below
             continue
 
         elif 'Stack' in item.schema_name():
             assemble_track(item, track_index, playlist_e)
 
-        # Check for time effects on item
+        # Check for effects on item
         for effect in item.effects:
+            # We only support certain time effects for now
             if isinstance(effect, otio.schema.TimeEffect):
                 apply_timewarp(item, item_e, effect)
 
 
-def assemble_timeline(tracks, level=0):
-    tractor_e = et.Element('tractor', id='tractor{}'.format(level))
+def assemble_timeline(tracks):
+    # We gather tracks in tractors. This is the "main one"
+    tractor_e = et.Element('tractor', id='tractor0')
     multitrack_e = et.SubElement(
         tractor_e,
         'multitrack',
-        attrib={'id': 'multitrack{}'.format(level)}
+        attrib={'id': 'multitrack0'}
     )
 
     root.append(tractor_e)
 
+    # This is to make sure there is a solid background if tracks contain gaps
     create_background_track(tracks, multitrack_e)
 
     for track_index, track in enumerate(tracks):
@@ -437,7 +459,7 @@ def rate_fraction_from_float(rate):
     good rates where possible. This will do fuzzy matching of
     23.98 to 24000/1001, for instance.
 
-    Function by Eric Reinecke
+    Thanks! @reinecke
     """
 
     # Whole numbers are easy
@@ -458,25 +480,53 @@ def rate_fraction_from_float(rate):
     return Fraction(rate)
 
 
-def create_profile_element(input_otio):
-    fractional = rate_fraction_from_float(input_otio.global_start_time.rate)
+def update_profile_element(profile_element, profile_data):
+    if isinstance(profile_data, otio.opentime.RationalTime):
+        fractional = rate_fraction_from_float(profile_data.rate)
+        profile_data = dict(
+            frame_rate_den=str(fractional.denominator),
+            frame_rate_num=str(fractional.numerator)
+        )
+
+    elif not isinstance(profile_data, dict):
+        raise ValueError(
+            'Only pass global_start_time as RationalTime or'
+            'a dict containing profile related key/value pairs.'
+        )
+
+    profile_element.attrib.update(profile_data)
+
+
+def create_profile_element():
     profile_e = et.Element(
         'profile',
-        decsription='automatic',
-        frame_rate_den=str(fractional.denominator),
-        frame_rate_num=str(fractional.numerator)
+        decsription='automatic'
     )
 
     return profile_e
 
 
-def write_to_string(input_otio):
-    profile_e = None
+def write_to_string(input_otio, **profile_data):
+    """
+
+    :param input_otio: Timeline, Track, Stack or Clip
+    :param profile_data: Properties passed to the profile tag describing
+    the format, frame rate, colorspace and so on. If a passed Timeline has
+    `global_start_time` set, the frame rate will be set automatically.
+    Please check MLT website for more info on profiles.
+
+    :return: MLT formatted XML
+    :rtype: `str`
+    """
+
+    profile_e = create_profile_element()
+    if profile_data:
+        update_profile_element(profile_e, profile_data)
 
     if isinstance(input_otio, otio.schema.Timeline):
         tracks = input_otio.tracks
         if input_otio.global_start_time:
-            profile_e = create_profile_element(input_otio)
+            update_profile_element(profile_e, input_otio.global_start_time)
 
     elif isinstance(input_otio, otio.schema.Track):
         stack = otio.schema.Stack()
@@ -500,7 +550,10 @@ def write_to_string(input_otio):
             "Not {}".format(type(input_otio))
         )
 
+    # This is the part that iterates over the "tracks" and constructs elements
     assemble_timeline(tracks)
+
+    # Below we add elements in an orderly fashion
 
     # Add producers to root
     for producer in producers['producer_order_']:
@@ -514,9 +567,10 @@ def write_to_string(input_otio):
     for playlist in playlists:
         root.insert(-1, playlist)
 
-    if profile_e is not None:
-        root.insert(0, profile_e)
+    # Add profile to the root of tree
+    root.insert(0, profile_e)
 
+    # Render the XML
     tree = minidom.parseString(et.tostring(root, 'utf-8'))
 
     return tree.toprettyxml(indent="    ")

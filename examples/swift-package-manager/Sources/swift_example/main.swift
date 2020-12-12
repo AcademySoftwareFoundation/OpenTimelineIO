39b0af665bba47831dd345f8c52b15f5d60e913e
David Baraff <nosuch@nowhere>
2020-12-12 15:13:37 -0800

    adding example

---
+++ examples/swift-package-manager/Sources/swift_example/main.swift
@@ -0,0 +1,22 @@
+import Opentimelineio
+
+func main() {
+    let sowm = SerializableObjectWithMetadata()
+    let clip = Clip()
+
+    sowm.metadata["anInt"] = 1
+    sowm.metadata["aString"] = "foo"
+    sowm.metadata["aVector"] = Metadata.Vector(arrayLiteral: 3, "abc", clip)
+    sowm.name = "sowm"
+
+    let sowm2 = try! sowm.clone() as! SerializableObjectWithMetadata
+    assert(sowm2.isEquivalent(to: sowm))
+
+    let sowm3 = SerializableObjectWithMetadata(name: sowm.name, metadata: sowm.metadata)
+    let sowm4 = SerializableObjectWithMetadata(name: sowm3.name, metadata: sowm3.metadata.map { $0 })
+    assert(sowm3.isEquivalent(to: sowm4))
+
+    print(try! sowm.toJSON())
+}
+
+main()

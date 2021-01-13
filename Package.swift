8626b1539d396dbd88186b71d500d25c7f10d734
Nick <nporcino@pixar.com>
2021-01-12 16:36:25 -0800

    Simplify Package.swift to eliminate need for symlinks

--- Package.swift
+++ Package.swift
@@ -8,65 +8,60 @@ let package = Package(
     platforms: [.macOS(.v10_13),
 		.iOS(.v11)],
     products: [
-        .library(name: "OpenTimelineIO_CXX", targets: ["OpenTimelineIO_CXX"]),
         .library(name: "OpenTime_CXX", targets: ["OpenTime_CXX"]),
+        .library(name: "OpenTimelineIO_CXX", targets: ["OpenTimelineIO_CXX"]),
         .library(name: "OpenTimelineIO", targets: ["OpenTimelineIO"])
     ],
     dependencies: [],
     targets: [
-	// Not a "real" library: exposes any/any.hpp needed by OpenTimelineIO_CXX
-	// and C++ clients of OpenTimelineIO_CXX.
-        .target(name: "any",
-		path: "src/spm-build/any/Sources",
-		exclude: ["any/any.hpp"],
-		sources: ["."],
-		publicHeadersPath: "."),
-
-	// Not a "real" library: exposes nonstd/optional.hpp needed by OpenTimelineIO_CXX
-	// and C++ clients of OpenTimelineIO_CXX.
-        .target(name: "optionallite",
-		path: "src/spm-build/optional-lite/Sources",
-		exclude: ["include/nonstd/optional.hpp"],
-		sources: ["."],
-		publicHeadersPath: "include"),
-
-	// public target
+		// public target
         .target(name: "OpenTime_CXX",
-		path: "src/spm-build/opentime/Sources",
-		exclude: ["opentime/CMakeLists.txt"],
-		sources: ["opentime"],
-		publicHeadersPath: "."),
+			path: "src/opentime",
+			exclude: ["CMakeLists.txt"], 
+			sources: ["."],
+			publicHeadersPath: ".",
+			cxxSettings: [  .headerSearchPath("..")]
+			),
 
-	// public target
+		// public target
         .target(name: "OpenTimelineIO_CXX",
-		dependencies: ["OpenTime_CXX", "any", "optionallite"],
-		path: "src/spm-build/opentimelineio/Sources",
-		exclude: ["opentimelineio/main.cpp",
-	      		  "opentimelineio/CMakeLists.txt"],
-		sources: ["opentimelineio"],
-		publicHeadersPath: ".",
-		cxxSettings: [CXXSetting.headerSearchPath("../../rapidjson/include")]),
+			dependencies: ["OpenTime_CXX"],
+			path: "src/opentimelineio",
+			exclude: ["main.cpp", "CMakeLists.txt"],
+			sources: ["."],
+			publicHeadersPath: ".",
+			cxxSettings: [  .headerSearchPath("../deps/rapidjson/include"), 
+							.headerSearchPath("../deps"), 
+							.headerSearchPath("../deps/optional-lite/include"),
+							.headerSearchPath("..")]
+			),
 
-	.target(name: "OpenTimelineIO_objc",
-		dependencies: ["OpenTimelineIO_CXX"],
-		path: "./src/swift-opentimelineio/Sources",
-		exclude: ["swift"],
-		sources: ["objc"],
-		publicHeadersPath: "objc/include"),
+		// private target
+		.target(name: "OpenTimelineIO_objc",
+			dependencies: ["OpenTimelineIO_CXX"],
+			path: "src/swift-opentimelineio/Sources/objc",
+			sources: ["."],
+			publicHeadersPath: "include",
+			cxxSettings: [  .headerSearchPath("../../../deps/rapidjson/include"), 
+							.headerSearchPath("../../../deps"), 
+							.headerSearchPath("../../../deps/optional-lite/include"),
+							.headerSearchPath("../../.."),
+							.headerSearchPath(".")]
+			),
 
-	// public target
-	.target(name: "OpenTimelineIO",
-		dependencies: ["OpenTimelineIO_objc"],
-		path: "./src/swift-opentimelineio/Sources",
-		exclude: ["objc"],
-		sources: ["swift"]),
+		// public target
+		.target(name: "OpenTimelineIO",
+			dependencies: ["OpenTimelineIO_objc"],
+			path: "./src/swift-opentimelineio/Sources",
+			exclude: ["objc"],
+			sources: ["swift"]),
 
-	.testTarget(name: "OpenTimelineIOTests",
-                    dependencies: ["OpenTimelineIO"],
-		    path: "./src/swift-opentimelineio/Tests",
-		    sources: ["OpenTimelineIOTests"],
-		    resources: [.process("data")])
+		.testTarget(name: "OpenTimelineIOTests",
+			dependencies: ["OpenTimelineIO"],
+			path: "./src/swift-opentimelineio/Tests/OpenTimelineIOTests",
+			sources: ["."],
+			resources: [.process("data")])
 
     ],
-    cxxLanguageStandard: CXXLanguageStandard.cxx11
+    cxxLanguageStandard: .cxx11
 )
--- src/spm-build/optional-lite/Sources/optional.cpp
+++
@@ -1,5 +0,0 @@
-/*
- * This file exists only to placate the Swift Package Manager,
- * and has no other functional purpose.
- */
-static int unused_optional_dummy = -1;

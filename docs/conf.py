39508cc1e990a0e7661ed4d103b44bed123a52cf
Sergio Rojas <hisergiorojas@protonmail.com>
2021-02-07 15:35:40 -0800

    fix typo

--- docs/conf.py
+++ docs/conf.py
@@ -149,7 +149,7 @@ html_static_path = ['_static']
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
- html_extra_path = ['../doxygen/output/html']
+html_extra_path = ['../doxygen/output/html']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.

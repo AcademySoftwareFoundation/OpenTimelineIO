9a13f952d135799c3096305a29f1858792790fc3
ssteinbach <ssteinbach@users.noreply.github.com>
2021-03-04 19:19:13 -0800

    almost there...

--- tests/test_windows_path.py
+++ tests/test_windows_path.py
@@ -92,7 +92,7 @@ class TestWindows(unittest.TestCase):
         full_path = os.path.abspath(
             file_bundle_utils.filepath_from_url(MEDIA_EXAMPLE_PATH_URL_ABS)
         )
-        if sys.platform == "windows":
+        if sys.platform.lower() == "windows":
             self.assertEqual(full_path.count("D:"), 1)
         else:
             self.assertNotIn("D:", full_path)

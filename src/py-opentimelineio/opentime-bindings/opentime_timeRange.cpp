4706744e6209291e9eba920dc5967d8462445e71
Viraj <55682570+Viraj-Rana008@users.noreply.github.com>
2021-06-13 12:44:00 +0530

    Update opentime_timeRange.cpp

--- src/py-opentimelineio/opentime-bindings/opentime_timeRange.cpp
+++ src/py-opentimelineio/opentime-bindings/opentime_timeRange.cpp
@@ -38,24 +38,24 @@ void opentime_timeRange_bindings(py::module m) {
         .def_property_readonly("start_time", &TimeRange::start_time)
         .def_property_readonly("duration", &TimeRange::duration)
         .def("end_time_inclusive", &TimeRange::end_time_inclusive, R"docstring(
-The time the last sample that contains data in the TimeRange.
+The time of the last sample containing data in the TimeRange.
 
-If the TimeRange goes from (0,24) w/ duration (10,24), this will be 
+If the TimeRange starts at (0,24) with duration (10,24), this will be 
 (9,24)
 
-If the TimeRange goes from (0,24) w/ duration (10.5, 24): 
+If the TimeRange starts at (0,24) with duration (10.5, 24): 
 (10, 24)
 
-In other words, the last frame with data (however fractional).
+In other words, the last frame with data, even if the last frame is fractional.
 )docstring")
         .def("end_time_exclusive", &TimeRange::end_time_exclusive, R"docstring(
 Time of the first sample outside the time range.
 
 If Start frame is 10 and duration is 5, then end_time_exclusive is 15,
-even though the last time with data in this range is 14.
+because the last time with data in this range is 14.
 
 If Start frame is 10 and duration is 5.5, then end_time_exclusive is 
-15.5, even though the last time with data in this range is 15.
+15.5, because the last time with data in this range is 15.
 )docstring")
         .def("duration_extended_by", &TimeRange::duration_extended_by, "other"_a)
         .def("extended_by", &TimeRange::extended_by, "other"_a, R"docstring(Construct a new TimeRange that is  this one extended by other.)docstring")

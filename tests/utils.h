bcd0544c2475d75aab7bfca3fdd32d10968e3a27
Darby Johnston <darbyjohnston@yahoo.com>
2021-11-27 15:27:59 -0800

    Add epsilon for double comparison

--- tests/utils.h
+++ tests/utils.h
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <cmath>
 #include <functional>
 #include <string>
 #include <vector>
@@ -25,11 +26,34 @@ assertEqual(T const& a, T const& b)
     assert(a == b);
 }
 
+// We are not testing values outside of one million seconds.
+// At one million second, and double precision, the smallest
+// resolvable number that can be added to one million and return
+// a new value one million + epsilon is 5.82077e-11.
+//
+// This was calculated by searching iteratively for epsilon
+// around 1,000,000, with epsilon starting from 1 and halved
+// at every iteration, until epsilon when added to 1,000,000
+// resulted in 1,000,000.
+constexpr double double_epsilon = 5.82077e-11;
+
+inline void
+assertEqual(double a, double b)
+{
+    assert(std::abs(a - b) <= double_epsilon);
+}
+
 template <typename T>
 inline void
 assertNotEqual(T const& a, T const& b)
 {
     assert(a != b);
+}
+
+inline void
+assertNotEqual(double a, double b)
+{
+    assert(std::abs(a - b) > double_epsilon);
 }
 
 class Tests

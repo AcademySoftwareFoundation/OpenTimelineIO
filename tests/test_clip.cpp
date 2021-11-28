e63a4cf20395d3f8392d55c7c943bd342c3898e4
Darby Johnston <darbyjohnston@yahoo.com>
2021-11-27 17:07:14 -0800

    Formatting

--- tests/test_clip.cpp
+++ tests/test_clip.cpp
@@ -8,52 +8,58 @@
 namespace otime = opentime::OPENTIME_VERSION;
 namespace otio = opentimelineio::OPENTIMELINEIO_VERSION;
 
-Tests::AddTest test_cons("test_clip", "test_cons", [] {
-    std::string         name = "test";
-    otime::RationalTime rt(5, 24);
-    otime::TimeRange    tr(rt, rt);
+Tests::AddTest test_cons(
+    "test_clip",
+    "test_cons",
+    [] {
+        std::string         name = "test";
+        otime::RationalTime rt(5, 24);
+        otime::TimeRange    tr(rt, rt);
 
-    otio::SerializableObject::Retainer<otio::ExternalReference> mr(
-        new otio::ExternalReference);
-    mr->set_available_range(otime::TimeRange(rt, otime::RationalTime(10, 24)));
-    mr->set_target_url("/var/tmp/test.mov");
+        otio::SerializableObject::Retainer<otio::ExternalReference> mr(
+            new otio::ExternalReference);
+        mr->set_available_range(otime::TimeRange(rt, otime::RationalTime(10, 24)));
+        mr->set_target_url("/var/tmp/test.mov");
 
-    otio::SerializableObject::Retainer<otio::Clip> cl(new otio::Clip);
-    cl->set_name(name);
-    cl->set_media_reference(mr);
-    cl->set_source_range(tr);
-    assertEqual(cl->name(), name);
-    assertEqual(cl->source_range().value(), tr);
+        otio::SerializableObject::Retainer<otio::Clip> cl(new otio::Clip);
+        cl->set_name(name);
+        cl->set_media_reference(mr);
+        cl->set_source_range(tr);
+        assertEqual(cl->name(), name);
+        assertEqual(cl->source_range().value(), tr);
 
-    std::string encoded = cl->to_json_string();
-    otio::SerializableObject::Retainer<otio::SerializableObject> decoded(
-        otio::SerializableObject::from_json_string(encoded));
-    assertTrue(cl->is_equivalent_to(*decoded));
-});
+        std::string encoded = cl->to_json_string();
+        otio::SerializableObject::Retainer<otio::SerializableObject> decoded(
+            otio::SerializableObject::from_json_string(encoded));
+        assertTrue(cl->is_equivalent_to(*decoded));
+    });
 
-Tests::AddTest test_ranges("test_clip", "test_ranges", [] {
-    otime::TimeRange tr(
-        // 1 hour in at 24 fps
-        otime::RationalTime(86400, 24),
-        otime::RationalTime(200, 24));
+Tests::AddTest test_ranges(
+    "test_clip",
+    "test_ranges",
+    [] {
+        otime::TimeRange tr(
+            // 1 hour in at 24 fps
+            otime::RationalTime(86400, 24),
+            otime::RationalTime(200, 24));
 
-    otio::SerializableObject::Retainer<otio::Clip> cl(new otio::Clip("test_clip"));
-    otio::SerializableObject::Retainer<otio::ExternalReference> mr(
-        new otio::ExternalReference);
-    mr->set_target_url("/var/tmp/test.mov");
-    mr->set_available_range(tr);
-    cl->set_media_reference(mr);
-    assertEqual(cl->duration(), cl->trimmed_range().duration());
-    assertEqual(cl->duration(), tr.duration());
-    assertEqual(cl->trimmed_range(), tr);
-    assertEqual(cl->available_range(), tr);
+        otio::SerializableObject::Retainer<otio::Clip> cl(new otio::Clip("test_clip"));
+        otio::SerializableObject::Retainer<otio::ExternalReference> mr(
+            new otio::ExternalReference);
+        mr->set_target_url("/var/tmp/test.mov");
+        mr->set_available_range(tr);
+        cl->set_media_reference(mr);
+        assertEqual(cl->duration(), cl->trimmed_range().duration());
+        assertEqual(cl->duration(), tr.duration());
+        assertEqual(cl->trimmed_range(), tr);
+        assertEqual(cl->available_range(), tr);
 
-    cl->set_source_range(otime::TimeRange(
-        // 1 hour + 100 frames
-        otime::RationalTime(86500, 24),
-        otime::RationalTime(50, 24)));
-    assertNotEqual(cl->duration(), tr.duration());
-    assertNotEqual(cl->trimmed_range(), tr);
-    assertEqual(cl->duration(), cl->source_range()->duration());
-    assertEqual(cl->trimmed_range(), cl->source_range().value());
-});
+        cl->set_source_range(otime::TimeRange(
+            // 1 hour + 100 frames
+            otime::RationalTime(86500, 24),
+            otime::RationalTime(50, 24)));
+        assertNotEqual(cl->duration(), tr.duration());
+        assertNotEqual(cl->trimmed_range(), tr);
+        assertEqual(cl->duration(), cl->source_range()->duration());
+        assertEqual(cl->trimmed_range(), cl->source_range().value());
+    });
--- tests/test_opentime.cpp
+++ tests/test_opentime.cpp
@@ -4,28 +4,37 @@
 
 namespace otime = opentime::OPENTIME_VERSION;
 
-Tests::AddTest test_create("test_opentime", "test_create", [] {
-    double              t_val = 30.2;
-    otime::RationalTime t(t_val);
-    assertEqual(t.value(), t_val);
+Tests::AddTest test_create(
+    "test_opentime",
+    "test_create",
+    [] {
+        double              t_val = 30.2;
+        otime::RationalTime t(t_val);
+        assertEqual(t.value(), t_val);
 
-    t = otime::RationalTime();
-    assertEqual(t.value(), 0.0);
-    assertEqual(t.rate(), 1.0);
-});
+        t = otime::RationalTime();
+        assertEqual(t.value(), 0.0);
+        assertEqual(t.rate(), 1.0);
+    });
 
-Tests::AddTest test_equality("test_opentime", "test_equality", [] {
-    otime::RationalTime t1(30.2);
-    assertEqual(t1, t1);
-    otime::RationalTime t2(30.2);
-    assertEqual(t1, t2);
-});
+Tests::AddTest test_equality(
+    "test_opentime",
+    "test_equality",
+    [] {
+        otime::RationalTime t1(30.2);
+        assertEqual(t1, t1);
+        otime::RationalTime t2(30.2);
+        assertEqual(t1, t2);
+    });
 
-Tests::AddTest test_inequality("test_opentime", "test_inequality", [] {
-    otime::RationalTime t1(30.2);
-    assertEqual(t1, t1);
-    otime::RationalTime t2(33.2);
-    assertNotEqual(t1, t2);
-    otime::RationalTime t3(30.2);
-    assertFalse(t1 != t3);
-});
+Tests::AddTest test_inequality(
+    "test_opentime",
+    "test_inequality",
+    [] {
+        otime::RationalTime t1(30.2);
+        assertEqual(t1, t1);
+        otime::RationalTime t2(33.2);
+        assertNotEqual(t1, t2);
+        otime::RationalTime t3(30.2);
+        assertFalse(t1 != t3);
+    });

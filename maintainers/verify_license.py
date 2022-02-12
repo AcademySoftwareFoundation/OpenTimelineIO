5e9164c3d6eb1b99c94a77c427f2611473787084
Joshua Minor <joshm@pixar.com>
2022-02-11 16:18:07 -0800

    Added verify_license.py script to help ensure all source files have the right license header.

---
+++ maintainers/verify_license.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the OpenTimelineIO project
+
+__doc__ = """The utility script checks to make sure that all of the source
+files in the OpenTimelineIO project have the correct license header."""
+
+import os
+import sys
+
+licenses = {
+    ".py": """# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the OpenTimelineIO project
+""",
+    ".cpp": """// SPDX-License-Identifier: Apache-2.0
+// Copyright Contributors to the OpenTimelineIO project
+""",
+    ".h": """// SPDX-License-Identifier: Apache-2.0
+// Copyright Contributors to the OpenTimelineIO project
+"""
+}
+
+yes = 0
+no = 0
+total = 0
+
+for root, dirs, files in os.walk("."):
+    for filename in files:
+        fullpath = os.path.join(root, filename)
+        for ext, license in licenses.items():
+            if filename.endswith(ext):
+                total += 1
+                try:
+                    content = open(fullpath, 'r').read()
+                except:
+                    print("ERROR: Unable to read file:", fullpath)
+                    # raise
+                    continue
+                if len(content)>10 and license not in content:
+                    print("MISSING: {}".format(fullpath))
+                    no += 1
+                else:
+                    yes += 1
+
+print("{} of {} files have the correct license.".format(yes, total))
+
+if no != 0:
+    print("ERROR: {} files do NOT have the correct license.".format(no))
+    sys.exit(1)
+

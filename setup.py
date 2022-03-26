c8e97427a28745e7b4a330510120b9a8b8e59387
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2022-03-26 13:16:03 -0400

    Add Python 3.10 to CI

--- setup.py
+++ setup.py
@@ -290,6 +290,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Operating System :: OS Independent',
         'Natural Language :: English',
     ],

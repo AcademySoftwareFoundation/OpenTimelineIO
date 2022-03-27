0f133ba8454e290bf55e35790dd0e58ce4434dc0
Jean-Christophe Morin <38703886+JeanChristopheMorinPerso@users.noreply.github.com>
2022-03-26 20:49:04 -0400

    Fix missing init metadata (#1251)
    * fix a regression where the dunder attributes weren't being preprended to __init__.py.
    
    * Don't install Python files from cmake if building with setuptools
    
    * Don't build C++ tests if building from setuptools
    
    * Add new OTIO_INSTALL_PYTHON_MODULES CMake variable to control when to install pure Python modules/files/packages
    
    Co-authored-by: Nick Porcino <meshula@hotmail.com>

--- setup.py
+++ setup.py
@@ -91,6 +91,10 @@ class OTIO_build_ext(setuptools.command.build_ext.build_ext):
             '-DOTIO_SHARED_LIBS:BOOL=OFF',
             '-DCMAKE_BUILD_TYPE=' + self.build_config,
             '-DOTIO_PYTHON_INSTALL_DIR=' + install_dir,
+            # turn off the C++ tests during a Python build
+            '-DBUILD_TESTING:BOOL=OFF',
+            # Python modules wil be installed by setuptools.
+            '-DOTIO_INSTALL_PYTHON_MODULES:BOOL=OFF',
         ]
 
         if platform.system() == "Windows":

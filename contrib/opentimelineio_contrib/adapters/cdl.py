3a06941f4d64e44e7aa8f0bf65e3584743d084fc
josh-mission <josh@missiondigital.co.uk>
2022-08-02 10:14:48 +0100

    minor change to cdl adapter description
    Signed-off-by: josh-mission <josh@missiondigital.co.uk>

--- contrib/opentimelineio_contrib/adapters/cdl.py
+++ contrib/opentimelineio_contrib/adapters/cdl.py
@@ -4,7 +4,8 @@
 """
 CDL Export Adapter
 This simple adapter allows users to export a collection of .cdl files from an OTIO timeline.
-The ColorCorrection ID within the .cdl will use the reel name/Tape of the clip, while the file itself will be named using the timeline event name.
+The ColorCorrection Node ID within the .cdl will use the CMX_3600 reel name/Tape of the clip, 
+while the file itself will be named using the timeline event name.
 
 To use: otio.adapters.write_to_file(timeline, cdl_output_directory, adapter_name='cdl')
 """

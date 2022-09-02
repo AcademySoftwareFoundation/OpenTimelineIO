103679d8c494cb6ab3b8f8e1c77c5954ddb34452
ssteinbach <ssteinbach@users.noreply.github.com>
2022-09-02 10:45:20 -0700

    update docs for better RTD readability
    Signed-off-by: ssteinbach <ssteinbach@users.noreply.github.com>

--- src/py-opentimelineio/opentimelineio/versioning.py
+++ src/py-opentimelineio/opentimelineio/versioning.py
@@ -8,35 +8,38 @@ from . import (
 
 def full_map():
     """Return the full map of schema version sets, including core and plugins.
-
     Organized as follows:
-    ```
-    {
-        "FAMILY_NAME": {
-            "LABEL": {
-                "SchemaName": schemaversion,
-                "Clip": 2,
-                "Timeline": 3,
-                ...
+
+    .. code-block:: python
+
+        {
+            "FAMILY_NAME": {
+                "LABEL": {
+                    "SchemaName": schemaversion,
+                    "Clip": 2,
+                    "Timeline": 3,
+                    ...
+                }
             }
         }
-    }
-    ```
+
 
     The "OTIO_CORE" family is always provided and represents the built in
     schemas defined in the C++ core.
     IE:
-    ```
-    {
+
+    .. code-block:: python
+
+        {
             "OTIO_CORE": {
                 "0.15.0": {
                     "Clip": 2,
                     ...
                 }
             }
-    }
-    ```
+        }
     """
+
     result = copy.deepcopy(plugins.ActiveManifest().version_manifests)
     result.update(
         {
@@ -47,6 +50,25 @@ def full_map():
 
 
 def fetch_map(family, label):
+    """Fetch the version map for the given family and label.  OpenTimelineIO
+    includes a built in family called "OTIO_CORE", this is compiled into the
+    C++ core and represents the core interchange schemas of OpenTimelineIO.
+
+    Users may define more family/label/schema:version mappings by way of the
+    version manifest plugins.
+
+    Returns a dictionary mapping Schema name to schema version, like:
+
+    .. code-block:: python
+
+        {
+            "Clip": 2,
+            "Timeline": 1,
+            ...
+        }
+
+    """
+
     if family == "OTIO_CORE":
         src = core.release_to_schema_version_map()
     else:

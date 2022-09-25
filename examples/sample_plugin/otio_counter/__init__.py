79ecefe7e0f224c8c7ff95f7fe450848a79a5fb5
ssteinbach <ssteinbach@users.noreply.github.com>
2022-09-25 13:10:58 -0700

    add license to examples

--- examples/sample_plugin/otio_counter/__init__.py
+++ examples/sample_plugin/otio_counter/__init__.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the OpenTimelineIO project
 import pkg_resources
 
 from opentimelineio.plugins import manifest
--- examples/sample_plugin/otio_counter/adapter.py
+++ examples/sample_plugin/otio_counter/adapter.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the OpenTimelineIO project
 import opentimelineio as otio
 
 """

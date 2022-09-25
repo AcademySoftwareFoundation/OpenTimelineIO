5815995007eb9c703d0138eca81541bbfa9563a7
ssteinbach <ssteinbach@users.noreply.github.com>
2022-09-25 12:44:00 -0700

    clean up verify license

--- maintainers/verify_license.py
+++ maintainers/verify_license.py
@@ -6,6 +6,7 @@
 __doc__ = """The utility script checks to make sure that all of the source
 files in the OpenTimelineIO project have the correct license header."""
 
+import argparse
 import os
 import sys
 
@@ -30,31 +31,82 @@ licenses = {
 """
 }
 
-yes = 0
-no = 0
-total = 0
+# dependencies and build dir do not need to be checked
+SKIP_DIRS = [
+    os.path.join("src", "deps"),
+    "build",
+    ".git",
+]
 
-for root, dirs, files in os.walk("."):
-    for filename in files:
-        fullpath = os.path.join(root, filename)
-        for ext, license in licenses.items():
-            if filename.endswith(ext):
-                total += 1
-                try:
-                    content = open(fullpath, 'r').read()
-                except Exception as ex:
-                    print("ERROR: Unable to read file: {}\n{}".format(
-                        fullpath, ex))
-                    # raise
-                    continue
-                if len(content) > 10 and license not in content:
-                    print("MISSING: {}".format(fullpath))
-                    no += 1
-                else:
-                    yes += 1
 
-print("{} of {} files have the correct license.".format(yes, total))
+def _parsed_args():
+    """ parse commandline arguments with argparse """
 
-if no != 0:
-    print("ERROR: {} files do NOT have the correct license.".format(no))
-    sys.exit(1)
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        '-s',
+        '--start-dir',
+        default='.',
+        type=str,
+        help=("Directory to start searching for files in.")
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    correct_license = 0
+    incorrect_license = 0
+    total = 0
+
+    args = _parsed_args()
+
+    for root, dirs, files in os.walk(args.start_dir):
+        for filename in files:
+            # make sure the dependencies aren't checked
+            if any(d in root for d in SKIP_DIRS):
+                continue
+            fullpath = os.path.join(root, filename)
+            for ext, lic in licenses.items():
+                if filename.endswith(ext):
+                    total += 1
+                    try:
+                        content = open(fullpath, 'r').read()
+                    except Exception as ex:
+                        sys.stderr.write(
+                            "ERROR: Unable to read file: {}\n{}".format(
+                                fullpath,
+                                ex
+                            )
+                        )
+                        continue
+
+                    if len(content) > 10 and lic not in content:
+                        print("MISSING: {}".format(fullpath))
+                        incorrect_license += 1
+                    else:
+                        correct_license += 1
+
+    print(
+        "{} of {} files have the correct license.".format(
+            correct_license,
+            total
+        )
+    )
+
+    if incorrect_license != 0:
+        sys.stderr.write(
+            "ERROR: {} files do NOT have the correct license.\n".format(
+                incorrect_license
+            )
+        )
+        return False
+
+    return True
+
+if __name__ == "__main__":
+    if not main():
+        sys.exit(1)

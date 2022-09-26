68e011477c55338ce62e086f8ee3dd2805c3d08f
ssteinbach <ssteinbach@users.noreply.github.com>
2022-09-26 10:01:17 -0700

    adding in bump version number script

---
+++ maintainers/bump_version_number.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the OpenTimelineIO project
+
+__doc__ = """Manage and apply the version in the OTIO_VERSION.json file"""
+
+import argparse
+import sys
+import json
+
+OTIO_VERSION_JSON_PATH = "OTIO_VERSION.json"
+
+TARGET_FILES = [
+    "setup.py"
+]
+
+
+def version():
+    with open(OTIO_VERSION_JSON_PATH, 'r') as fi:
+        return json.load(fi)['version']
+
+
+def _parsed_args():
+    parser = argparse.ArgumentParser(
+        description='Fetch a list of contributors for a given GitHub repo.'
+    )
+
+    op_grp = parser.add_mutually_exclusive_group(required=True)
+    op_grp.add_argument(
+        "-i",
+        "--increment",
+        type=str,
+        default=None,
+        choices=("major", "minor", "patch"),
+        help="Increment either the major or minor version number."
+    )
+    op_grp.add_argument(
+        "-s",
+        "--set",
+        type=str,
+        default=None,
+        nargs=3,
+        help="Set the version string, in the form of MAJOR MINOR PATCH"
+    )
+    op_grp.add_argument(
+        "-q",
+        "--query",
+        default=False,
+        action="store_true",
+        help="Query/print the current version without changing it"
+    )
+    parser.add_argument(
+        "-d",
+        "--dryrun",
+        default=False,
+        action="store_true",
+        help="Perform actions but modify no files on disk."
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = _parsed_args()
+
+    major, minor, patch = (int(v) for v in version())
+
+    if args.increment == "major":
+        major += 1
+        minor = 0
+        patch = 0
+    elif args.increment == "minor":
+        minor += 1
+        patch = 0
+    elif args.increment == "patch":
+        patch += 1
+    elif args.set:
+        major, minor, patch = args.set
+    elif args.query:
+        print(".".join(str(v) for v in (major, minor, patch)))
+        return
+
+    for fp in TARGET_FILES:
+        with open(fp, 'r') as fi:
+            content = fi.read()
+
+        if args.add:
+            modified = add_suffix(content, version)
+        elif args.remove:
+            modified = remove_suffix(content, version)
+
+        if modified and not args.dryrun:
+            with open(fp, 'w') as fo:
+                fo.write(content)
+            print("Wrote modified {}.".format(fp))
+
+
+def add_suffix(content, version):
+    if version not in content:
+        sys.stderr.write(
+            "Version {} not found, suffix may have already been "
+            "added.\n".format(version)
+        )
+        return False
+
+    print("adding suffix, version will be: {}".format(version + ".dev1"))
+    content.replace(version, version + ".dev1")
+    return True
+
+
+def remove_suffix(content, version):
+    if version + '.dev1' not in content:
+        sys.stderr.write(
+            "Version+Suffix {} not found, suffix may have already been "
+            "removed.\n".format(version + '.dev1')
+        )
+        return False
+
+    content.replace(version + ' .dev1', version)
+    return True
+
+
+if __name__ == "__main__":
+    main()

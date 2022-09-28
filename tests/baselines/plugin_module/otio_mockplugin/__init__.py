6b9bec0399349ac88bfa360f9fb2b9226932ba6c
ssteinbach <ssteinbach@users.noreply.github.com>
2022-09-28 10:47:57 -0700

    couple more comments and things to clean up

--- tests/baselines/plugin_module/otio_mockplugin/__init__.py
+++ tests/baselines/plugin_module/otio_mockplugin/__init__.py
@@ -20,13 +20,9 @@ a non-standard json file path.
 
 
 def plugin_manifest():
-    # XXX: in python3.5 resource_string returns a 'bytes' object, but
-    #      json.loads requires a string, not bytes (only in 3.5 -- 2.7 and 3.6
-    #      seem to both be working).  Luckily .decode() seems to work in both
-    #      python3 and python2, so this *should* work for both versions.
     return manifest.manifest_from_string(
         pkg_resources.resource_string(
             __name__,
             'unusually_named_plugin_manifest.json'
-        ).decode('utf-8')
+        )
     )

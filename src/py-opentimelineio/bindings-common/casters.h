647f8385ad9898aa8088c6b181cca7b5b8839289
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2022-10-11 21:54:29 -0400

    Add comment for caster
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- src/py-opentimelineio/bindings-common/casters.h
+++ src/py-opentimelineio/bindings-common/casters.h
@@ -6,6 +6,10 @@ using nonstd::optional;
 using nonstd::nullopt_t;
 
 namespace pybind11 { namespace detail {
+    // Caster for converting to/from nonstd::optional.
+    // Pybind11 supports optional types when C++17 is used.
+    // So for now we have to create the casters manually.
+    // See https://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html#c-17-library-containers
     template<typename T> struct type_caster<optional<T>>
         : public optional_caster<optional<T>> {};
 

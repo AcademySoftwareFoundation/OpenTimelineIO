cebb94522861ce020001749bc3dd8384a7e689c4
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2022-11-05 21:39:19 -0400

    Clean init of AnyVectorProxy
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- src/py-opentimelineio/opentimelineio-bindings/otio_anyVector.cpp
+++ src/py-opentimelineio/opentimelineio-bindings/otio_anyVector.cpp
@@ -20,13 +20,10 @@ void otio_any_vector_bindings(py::module m) {
     py::class_<AnyVectorProxy>(m, "AnyVector")
         .def(py::init<>())
         .def(py::init([](const py::iterable &it) {
-            auto v = new AnyVector();
-            for (py::handle h : it) {
-                v->push_back(py_to_any(h));
-            }
-
-            AnyVectorProxy* a = new AnyVectorProxy(v->get_or_create_mutation_stamp());
-            return a;
+            auto v = py_to_cpp(it);
+            auto proxy = new AnyVectorProxy;
+            proxy->fetch_any_vector().swap(*v.get_or_create_mutation_stamp()->any_vector);
+            return proxy;
         }))
         .def("__internal_getitem__", &AnyVectorProxy::get_item, "index"_a)
         .def("__internal_setitem__", &AnyVectorProxy::set_item, "index"_a, "item"_a)

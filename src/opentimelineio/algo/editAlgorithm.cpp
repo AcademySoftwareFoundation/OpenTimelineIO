cf31e9c60b175b440e2561b2c9f92670919b7ca3
Gonzalo Garramuño <ggarra13@gmail.com>
2023-08-19 04:14:00 -0300

    WIP algo.
    Signed-off-by: Gonzalo Garramuño <ggarra13@gmail.com>

--- src/opentimelineio/algo/editAlgorithm.cpp
+++ src/opentimelineio/algo/editAlgorithm.cpp
@@ -60,6 +60,7 @@ overwrite(
     Item*            fill_template,
     ErrorStatus*     error_status)
 {
+    std::cerr << "overwrite at " << range << std::endl;
     const TimeRange composition_range = composition->trimmed_range();
     if (range.start_time() >= composition_range.end_time_exclusive())
     {

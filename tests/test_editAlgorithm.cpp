9ce758a6b87e50ed72b7316dca1f43489ba10db4
Gonzalo Garramuño <ggarra13@gmail.com>
2023-08-20 08:10:56 -0300

    Broken find_children.
    Signed-off-by: Gonzalo Garramuño <ggarra13@gmail.com>

--- tests/test_editAlgorithm.cpp
+++ tests/test_editAlgorithm.cpp
@@ -7,6 +7,8 @@
 #include <opentimelineio/clip.h>
 #include <opentimelineio/algo/editAlgorithm.h>
 #include <opentimelineio/gap.h>
+#include <opentimelineio/stack.h>
+#include <opentimelineio/timeline.h>
 #include <opentimelineio/track.h>
 #include <opentimelineio/transition.h>
 
@@ -2504,6 +2506,83 @@ main(int argc, char** argv)
             }); 
     });
     
+    // Add a shorter clip in gap as Sequence reference point
+    tests.add_test("test_edit_fill_7", [] {
+
+        test_edit_fill(
+            TimeRange(
+                RationalTime(10.0, 24.0),
+                RationalTime(5.0, 24.0)),
+            RationalTime(20.0, 24.0),
+            ReferencePoint::Sequence,
+            // Clip in Track Ranges
+            {
+                TimeRange(
+                    RationalTime(0.0, 24.0),
+                    RationalTime(20.0, 24.0)),
+                TimeRange(
+                    RationalTime(20.0, 24.0),
+                    RationalTime(5.0, 24.0)),
+                TimeRange(
+                    RationalTime(25.0, 24.0),
+                    RationalTime(25.0, 24.0)),
+                TimeRange(
+                    RationalTime(50.0, 24.0),
+                    RationalTime(20.0, 24.0))
+            },
+            // Clip Ranges
+            {
+                TimeRange(
+                    RationalTime(0.0, 24.0),
+                    RationalTime(20.0, 24.0)),
+                TimeRange(
+                    RationalTime(10.0, 24.0),
+                    RationalTime(5.0, 24.0)),
+                TimeRange(
+                    RationalTime(10.0, 24.0),
+                    RationalTime(25.0, 24.0)),
+                TimeRange(
+                    RationalTime(5.0, 24.0),
+                    RationalTime(20.0, 24.0))
+            }); 
+    });
+
+    // Find children seems broken
+    tests.add_test("test_find_children", [] {
+        
+        // Create a timeline, stack and two tracks with one clip each.
+        otio::SerializableObject::Retainer<otio::Clip> video_clip = new otio::Clip(
+            "video_0",
+            nullptr,
+            otio::TimeRange(
+                otio::RationalTime(0.0, 30.0),
+                otio::RationalTime(704.0, 30.0)));
+        otio::SerializableObject::Retainer<otio::Clip> audio_clip = new otio::Clip(
+            "audio_0",
+            nullptr,
+            otio::TimeRange(
+                otio::RationalTime(5.0, 24.0),
+                otio::RationalTime(20.0, 24.0)));
+        otio::SerializableObject::Retainer<otio::Track> video_track = new otio::Track("Video");
+        otio::SerializableObject::Retainer<otio::Track> audio_track = new otio::Track("Audio");
+        otio::SerializableObject::Retainer<otio::Stack> stack = new otio::Stack();
+        otio::SerializableObject::Retainer<otio::Timeline> timeline = new otio::Timeline();
+        video_track->append_child(video_clip);
+        audio_track->append_child(audio_clip);
+
+        stack->append_child(video_track);
+        stack->append_child(audio_track);
+
+        timeline->set_tracks(stack);
+
+        RationalTime time(704.0, 30.0);
+        RationalTime one_frame(1.0, 30.0);
+        TimeRange range(time, one_frame);
+        otio::ErrorStatus errorStatus;
+        auto items = timeline->find_children(&errorStatus, range);
+        assert(!otio::is_error(errorStatus));
+        assert(!items.empty());
+    });
     
     tests.run(argc, argv);
     return 0;

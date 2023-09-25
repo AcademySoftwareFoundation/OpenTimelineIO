fcac9d6e95d1b0a1b843ae75e40ad7bb76975a44
Doug Halley <douglascomet@gmail.com>
2023-09-24 22:56:55 -0400

    Updated docstring to elaborate on the updated functionality of `filepath_from_url`
    Signed-off-by: Doug Halley <douglascomet@gmail.com>

--- src/py-opentimelineio/opentimelineio/url_utils.py
+++ src/py-opentimelineio/opentimelineio/url_utils.py
@@ -38,7 +38,17 @@ def url_from_filepath(fpath):
 
 
 def filepath_from_url(urlstr):
-    """ Take a url and return a filepath """
+    """
+    Take an url and return a filepath.
+
+    URLs can either be encoded according to the `RFC 3986`_ standard or not.
+    Additionally, Windows mapped paths need to be accounted for when processing a
+    URL; however, there are `ongoing discussions`_ about how to best handle this within
+    Python. This function is meant to cover all of these scenarios in the interim.
+
+    .. _RFC 3986: https://tools.ietf.org/html/rfc3986#section-2.1
+    .. _ongoing discussions: https://discuss.python.org/t/file-uris-in-python/15600
+    """
 
     parsed_result = urlparse.urlparse(urlstr)
 

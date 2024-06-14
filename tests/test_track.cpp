6e153d8c881e1577f5e5983e31ac64f3a1324d0a
Darby Johnston <darbyjohnston@yahoo.com>
2024-06-14 10:51:37 -0700

    Fix find_children search range with stacks
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- tests/test_track.cpp
+++ tests/test_track.cpp
@@ -108,6 +108,40 @@ main(int argc, char** argv)
         assertEqual(result[1].value, cl1.value);
     });
 
+    tests.add_test(
+        "test_find_children_stack", [] {
+        using namespace otio;
+        SerializableObject::Retainer<Clip> video_clip = new Clip(
+            "video_0",
+            nullptr,
+            TimeRange(RationalTime(0.0, 30.0), RationalTime(700.0, 30.0)));
+        SerializableObject::Retainer<Clip> audio_clip = new Clip(
+            "audio_0",
+            nullptr,
+            TimeRange(RationalTime(0.0, 30.0), RationalTime(704.0, 30.0)));
+        SerializableObject::Retainer<Track> video_track = new Track("Video");
+        SerializableObject::Retainer<Track> audio_track = new Track("Audio");
+        SerializableObject::Retainer<Stack> stack = new Stack();
+        video_track->append_child(video_clip);
+        audio_track->append_child(audio_clip);
+        stack->append_child(video_track);
+        stack->append_child(audio_track);
+
+        RationalTime      time(703.0, 30.0);
+        RationalTime      one_frame(1.0, 30.0);
+        TimeRange         range(time, one_frame);
+        otio::ErrorStatus err;
+        auto              items = stack->find_children(&err, range);
+        assertFalse(is_error(err));
+        assertEqual(items.size(), 2);
+        assertTrue(
+            std::find(items.begin(), items.end(), audio_clip.value) !=
+            items.end());
+        assertTrue(
+            std::find(items.begin(), items.end(), audio_track.value) !=
+            items.end());
+    });
+
     tests.run(argc, argv);
     return 0;
 }

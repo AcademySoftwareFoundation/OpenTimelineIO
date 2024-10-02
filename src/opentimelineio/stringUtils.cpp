987d00a7aa9ec7210af95763f6e57496960fa970
furby™ <wabi.film@gmail.com>
2024-10-02 01:29:03 -0600

    Deprecate c++ demangling. (#1800)
    Signed-off-by: furby™ <devs@wabi.foundation>

--- src/opentimelineio/stringUtils.cpp
+++ src/opentimelineio/stringUtils.cpp
@@ -2,30 +2,9 @@
 // Copyright Contributors to the OpenTimelineIO project
 
 #include "opentimelineio/serializableObject.h"
-#if defined(__GNUC__) || defined(__clang__)
-#    include <cstdlib>
-#    include <cxxabi.h>
-#    include <memory>
-#else
-#    include <typeinfo>
-#endif
-
-namespace {
-#if defined(__GNUC__) || defined(__clang__)
-std::string
-cxxabi_type_name_for_error_mesage(const char* name)
-{
-    int status = -4; // some arbitrary value to eliminate the compiler warning
-
-    std::unique_ptr<char, void (*)(void*)> res{
-        abi::__cxa_demangle(name, NULL, NULL, &status),
-        std::free
-    };
-
-    return (status == 0) ? res.get() : name;
-}
-#endif
-} // namespace
+#include <cstdlib>
+#include <memory>
+#include <typeinfo>
 
 namespace opentimelineio { namespace OPENTIMELINEIO_VERSION {
 
@@ -41,12 +20,7 @@ type_name_for_error_message(std::type_info const& t)
         return "None";
     }
 
-#if defined(__GNUC__) || defined(__clang__)
-    return ::cxxabi_type_name_for_error_mesage(t.name());
-#else
-    // On Windows std::type_info.name() returns a human readable string.
     return t.name();
-#endif
 }
 
 std::string

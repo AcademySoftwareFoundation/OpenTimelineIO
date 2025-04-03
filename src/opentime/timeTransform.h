d87606277f415bb66e1877a8caf6c10bbeb381e4
Darby Johnston <darbyjohnston@yahoo.com>
2025-04-02 17:28:52 -0700

    Add Doxygen comments to opentime (#1866)
    * Add Doxygen comments
    
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- src/opentime/timeTransform.h
+++ src/opentime/timeTransform.h
@@ -10,9 +10,11 @@
 
 namespace opentime { namespace OPENTIME_VERSION {
 
+/// @brief One-dimensional time transform.
 class TimeTransform
 {
 public:
+    /// @brief Construct a new transform with an optional offset, scale, and rate.
     explicit constexpr TimeTransform(
         RationalTime offset = RationalTime{},
         double       scale  = 1,
@@ -22,12 +24,17 @@ public:
         , _rate{ rate }
     {}
 
+    /// @brief Return the offset.
     constexpr RationalTime offset() const noexcept { return _offset; }
 
+    /// @brief Return the scale.
     constexpr double scale() const noexcept { return _scale; }
 
+    /// @brief Return the rate.
     constexpr double rate() const noexcept { return _rate; }
 
+    /// @brief Apply the transform to a TimeRange. The transformed TimeRange is
+    /// returned.
     constexpr TimeRange applied_to(TimeRange other) const noexcept
     {
         return TimeRange::range_from_start_end_time(
@@ -35,6 +42,8 @@ public:
             applied_to(other.end_time_exclusive()));
     }
 
+    /// @brief Apply the transform to another TimeTransform. The transformed
+    /// TimeTransform is returned.
     constexpr TimeTransform applied_to(TimeTransform other) const noexcept
     {
         return TimeTransform{ _offset + other._offset,
@@ -42,6 +51,8 @@ public:
                               _rate > 0 ? _rate : other._rate };
     }
 
+    /// @brief Apply the transform to a RationalTime. The transformed
+    /// RationalTime is returned.
     constexpr RationalTime applied_to(RationalTime other) const noexcept
     {
         RationalTime result{ RationalTime{ other._value * _scale, other._rate }
@@ -50,6 +61,7 @@ public:
         return target_rate > 0 ? result.rescaled_to(target_rate) : result;
     }
 
+    /// @brief Return whether two transforms are equal.
     friend constexpr bool
     operator==(TimeTransform lhs, TimeTransform rhs) noexcept
     {
@@ -57,6 +69,7 @@ public:
                && lhs._rate == rhs._rate;
     }
 
+    /// @brief Return whether two transforms are not equal.
     friend constexpr bool
     operator!=(TimeTransform lhs, TimeTransform rhs) noexcept
     {

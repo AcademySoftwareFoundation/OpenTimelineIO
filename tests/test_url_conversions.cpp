2f7af4fb2c8adab63c28d34883e05a89c18fbb36
Darby Johnston <darbyjohnston@yahoo.com>
2025-05-20 12:16:52 -0700

    Refactoring
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- tests/test_url_conversions.cpp
+++ tests/test_url_conversions.cpp
@@ -16,31 +16,32 @@ main(int argc, char** argv)
 {
     Tests tests;
 
-    const std::filesystem::path sample_data_dir = std::filesystem::current_path() / "sample_data";
-    const std::string screening_example_path = (sample_data_dir / "screening_example.otio").u8string();
-    const std::string media_example_path_rel = "OpenTimelineIO@3xDark.png";
-    const std::string media_example_path_url_rel = otio::url_from_filepath(media_example_path_rel);
-    const std::string media_example_path_abs = (sample_data_dir / "OpenTimelineIO@3xLight.png").u8string();
-    const std::string media_example_path_url_abs = otio::url_from_filepath(media_example_path_abs);
+    std::filesystem::path const sample_data_dir = std::filesystem::current_path() / "sample_data";
+    std::string const screening_example_path = (sample_data_dir / "screening_example.otio").u8string();
 
-    const std::string windows_encoded_url = "file://host/S%3a/path/file.ext";
-    const std::string windows_drive_url = "file://S:/path/file.ext";
-    const std::string windows_drive_path = "S:/path/file.ext";
+    std::string const media_example_path_rel = "OpenTimelineIO@3xDark.png";
+    std::string const media_example_path_url_rel = otio::url_from_filepath(media_example_path_rel);
+    std::string const media_example_path_abs = (sample_data_dir / "OpenTimelineIO@3xLight.png").u8string();
+    std::string const media_example_path_url_abs = otio::url_from_filepath(media_example_path_abs);
 
-    const std::string windows_encoded_unc_url = "file://unc/path/sub%20dir/file.ext";
-    const std::string windows_unc_url = "file://unc/path/sub dir/file.ext";
-    const std::string windows_unc_path = "//unc/path/sub dir/file.ext";
+    std::string const windows_encoded_url = "file://host/S%3a/path/file.ext";
+    std::string const windows_drive_url = "file://S:/path/file.ext";
+    std::string const windows_drive_path = "S:/path/file.ext";
 
-    const std::string posix_localhost_url = "file://localhost/path/sub dir/file.ext";
-    const std::string posix_encoded_url = "file:///path/sub%20dir/file.ext";
-    const std::string posix_url = "file:///path/sub dir/file.ext";
-    const std::string posix_path = "/path/sub dir/file.ext";
+    std::string const windows_encoded_unc_url = "file://unc/path/sub%20dir/file.ext";
+    std::string const windows_unc_url = "file://unc/path/sub dir/file.ext";
+    std::string const windows_unc_path = "//unc/path/sub dir/file.ext";
+
+    std::string const posix_localhost_url = "file://localhost/path/sub dir/file.ext";
+    std::string const posix_encoded_url = "file:///path/sub%20dir/file.ext";
+    std::string const posix_url = "file:///path/sub dir/file.ext";
+    std::string const posix_path = "/path/sub dir/file.ext";
 
     tests.add_test(
         "test_roundtrip_abs",
         [media_example_path_abs, media_example_path_url_abs] {
             assertEqual(media_example_path_url_abs.substr(0, 7), std::string("file://"));
-            const std::string filepath = otio::filepath_from_url(media_example_path_url_abs);
+            std::string const filepath = otio::filepath_from_url(media_example_path_url_abs);
             assertEqual(filepath, media_example_path_abs);
         });
 
@@ -48,15 +49,15 @@ main(int argc, char** argv)
         "test_roundtrip_rel",
         [media_example_path_rel, media_example_path_url_rel] {
             assertNotEqual(media_example_path_url_rel.substr(0, 7), std::string("file://"));
-            const std::string filepath = otio::filepath_from_url(media_example_path_url_rel);
+            std::string const filepath = otio::filepath_from_url(media_example_path_url_rel);
             assertEqual(filepath, media_example_path_rel);
         });
 
     tests.add_test(
         "test_windows_urls",
         [windows_encoded_url, windows_drive_url, windows_drive_path] {
-            for (const auto url : { windows_encoded_url, windows_drive_url }) {
-                const std::string filepath = otio::filepath_from_url(url);
+            for (auto const url : { windows_encoded_url, windows_drive_url }) {
+                std::string const filepath = otio::filepath_from_url(url);
                 assertEqual(filepath, windows_drive_path);
             }
         });
@@ -64,8 +65,8 @@ main(int argc, char** argv)
     tests.add_test(
         "test_windows_unc_urls",
         [windows_encoded_unc_url, windows_unc_url, windows_unc_path] {
-            for (const auto url : { windows_encoded_unc_url, windows_unc_url }) {
-                const std::string filepath = otio::filepath_from_url(url);
+            for (auto const url : { windows_encoded_unc_url, windows_unc_url }) {
+                std::string const filepath = otio::filepath_from_url(url);
                 assertEqual(filepath, windows_unc_path);
             }
         });
@@ -73,8 +74,8 @@ main(int argc, char** argv)
     tests.add_test(
         "test_posix_urls",
         [posix_encoded_url, posix_url, posix_localhost_url, posix_path] {
-            for (const auto url : { posix_encoded_url, posix_url }) {
-                const std::string filepath = otio::filepath_from_url(url);
+            for (auto const url : { posix_encoded_url, posix_url }) {
+                std::string const filepath = otio::filepath_from_url(url);
                 assertEqual(filepath, posix_path);
             }
         });
@@ -84,7 +85,7 @@ main(int argc, char** argv)
         [] {
             // See github issue #1817 - when a relative URL has only one name after
             // the "." (ie ./blah but not ./blah/blah)
-            const std::string filepath = otio::filepath_from_url(
+            std::string const filepath = otio::filepath_from_url(
                 std::filesystem::path(".") / std::filesystem::path("docs"));
             assertEqual(filepath, std::string("docs"));
         });

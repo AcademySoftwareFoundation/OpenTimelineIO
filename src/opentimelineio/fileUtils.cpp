2cd358541fe2addcc2530fb2a2a168046ee6e894
Darby Johnston <darbyjohnston@yahoo.com>
2025-06-14 12:31:18 -0700

    Refactoring
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- src/opentimelineio/fileUtils.cpp
+++ src/opentimelineio/fileUtils.cpp
@@ -3,23 +3,8 @@
 
 #include "opentimelineio/fileUtils.h"
 
-#include <cstring>
-#include <vector>
-
-#if defined(_WINDOWS)
-#if !defined(WIN32_LEAN_AND_MEAN)
-#define WIN32_LEAN_AND_MEAN
-#endif // WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <combaseapi.h>
-#if defined(min)
-#undef min
-#endif // min
-#else // _WINDOWS
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <unistd.h>
-#endif // _WINDOWS
+#include <cstdio>
+#include <filesystem>
 
 namespace opentimelineio { namespace OPENTIMELINEIO_VERSION {
 
@@ -31,78 +16,11 @@ to_unix_separators(std::string const& path)
     return result;
 }
 
-#if defined(_WIN32) || defined(_WIN64)
-
 std::string create_temp_dir()
 {
-    std::string out;
-
-    // Get the temporary directory.
-    char path[MAX_PATH];
-    DWORD r = GetTempPath(MAX_PATH, path);
-    if (r)
-    {
-        out = std::string(path);
-
-        // Replace path separators.
-        for (size_t i = 0; i < out.size(); ++i)
-        {
-            if ('\\' == out[i])
-            {
-                out[i] = '/';
-            }
-        }
-
-        // Create a unique name from a GUID.
-        GUID guid;
-        CoCreateGuid(&guid);
-        const uint8_t* guidP = reinterpret_cast<const uint8_t*>(&guid);
-        for (int i = 0; i < 16; ++i)
-        {
-            char buf[3] = "";
-            sprintf_s(buf, 3, "%02x", guidP[i]);
-            out += buf;
-        }
-
-        // Create a unique directory.
-        CreateDirectory(out.c_str(), NULL);
-    }
-
+    std::string const out(std::tmpnam(nullptr));
+    std::filesystem::create_directory(out);
     return out;
 }
-
-#else // _WIN32 || _WIN64
-
-std::string create_temp_dir()
-{
-    // Find the temporary directory.
-    std::string path;
-    char* env = nullptr;
-    if ((env = getenv("TEMP"))) path = env;
-    else if ((env = getenv("TMP"))) path = env;
-    else if ((env = getenv("TMPDIR"))) path = env;
-    else
-    {
-        for (const auto& i : { "/tmp", "/var/tmp", "/usr/tmp" })
-        {
-            struct stat buffer;   
-            if (0 == stat(i, &buffer))
-            {
-                path = i;
-                break;
-            }
-        }
-    }
-
-    // Create a unique directory.
-    path = path + "/XXXXXX";
-    const size_t size = path.size();
-    std::vector<char> buf(size + 1);
-    memcpy(buf.data(), path.c_str(), size);
-    buf[size] = 0;
-    return mkdtemp(buf.data());
-}
-
-#endif // _WIN32 || _WIN64
 
 }} // namespace opentimelineio::OPENTIMELINEIO_VERSION

078231e1d17b95b1828bd1e4e11554660c1b9917
Eugen Sendroiu <esendroiu>
2025-09-17 11:04:42 +0200

    Revert "Revert python changes"
    This reverts commit b0efcde07f517b229021f5699d3b952c1beffdec.

--- src/py-opentimelineio/opentimelineio/schema/__init__.py
+++ src/py-opentimelineio/opentimelineio/schema/__init__.py
@@ -32,6 +32,11 @@ from .. _otio import (
     VideoPosition,
     VideoRotate,
     VideoRoundCorners,
+    VideoBrightness,
+    VideoContrast,
+    VideoSaturation,
+    VideoLightness,
+    VideoColorTemperature,
 )
 
 MarkerColor = Marker.Color
@@ -93,4 +98,10 @@ __all__ = [
     'VideoRotate',
     'VideoRoundCorners',
     'VideoScale',
+    'VideoRoundCorners',
+    'VideoBrightness',
+    'VideoContrast',
+    'VideoSaturation',
+    'VideoLightness',
+    'VideoColorTemperature',
 ]

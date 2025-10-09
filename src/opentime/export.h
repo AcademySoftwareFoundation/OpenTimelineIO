142eaeee4f81b28865041f912edefea8770d4c6b
Darby Johnston <darbyjohnston@yahoo.com>
2025-10-08 17:34:53 -0700

    Refactoring
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- src/opentime/export.h
+++ src/opentime/export.h
@@ -3,35 +3,38 @@
 
 #pragma once
 
+// For an explanation of how these export defines work, see:
+// https://github.com/PixarAnimationStudios/OpenUSD/blob/dev/pxr/base/arch/export.h
 #if defined(_WINDOWS)
 #   if defined(__GNUC__) && __GNUC__ >= 4 || defined(__clang__)
-#       define ARCH_EXPORT __attribute__((dllexport))
-#       define ARCH_IMPORT __attribute__((dllimport))
-#       define ARCH_HIDDEN
-#       define ARCH_EXPORT_TYPE
+#       define OPENTIMELINIO_EXPORT __attribute__((dllexport))
+#       define OPENTIMELINIO_IMPORT __attribute__((dllimport))
+#       define OPENTIMELINIO_HIDDEN
+#       define OPENTIMELINIO_EXPORT_TYPE
 #   else
-#       define ARCH_EXPORT __declspec(dllexport)
-#       define ARCH_IMPORT __declspec(dllimport)
-#       define ARCH_HIDDEN
-#       define ARCH_EXPORT_TYPE
+#       define OPENTIMELINIO_EXPORT __declspec(dllexport)
+#       define OPENTIMELINIO_IMPORT __declspec(dllimport)
+#       define OPENTIMELINIO_HIDDEN
+#       define OPENTIMELINIO_EXPORT_TYPE
 #   endif
 #elif defined(__GNUC__) && __GNUC__ >= 4 || defined(__clang__)
-#   define ARCH_EXPORT __attribute__((visibility("default")))
-#   define ARCH_IMPORT
-#   define ARCH_HIDDEN __attribute__((visibility("hidden")))
+#   define OPENTIMELINIO_EXPORT __attribute__((visibility("default")))
+#   define OPENTIMELINIO_IMPORT
+#   define OPENTIMELINIO_HIDDEN __attribute__((visibility("hidden")))
 #   if defined(__clang__)
-#       define ARCH_EXPORT_TYPE __attribute__((type_visibility("default")))
+#       define OPENTIMELINIO_EXPORT_TYPE __attribute__((type_visibility("default")))
 #   else
-#       define ARCH_EXPORT_TYPE __attribute__((visibility("default")))
+#       define OPENTIMELINIO_EXPORT_TYPE __attribute__((visibility("default")))
 #   endif
 #else
-#   define ARCH_EXPORT
-#   define ARCH_IMPORT
-#   define ARCH_HIDDEN
-#   define ARCH_EXPORT_TYPE
+#   define OPENTIMELINIO_EXPORT
+#   define OPENTIMELINIO_IMPORT
+#   define OPENTIMELINIO_HIDDEN
+#   define OPENTIMELINIO_EXPORT_TYPE
 #endif
-#define ARCH_EXPORT_TEMPLATE(type, ...)
-#define ARCH_IMPORT_TEMPLATE(type, ...) extern template type ARCH_IMPORT __VA_ARGS__
+#define OPENTIMELINIO_EXPORT_TEMPLATE(type, ...)
+#define OPENTIMELINIO_IMPORT_TEMPLATE(type, ...) \
+    extern template type OPENTIMELINIO_IMPORT __VA_ARGS__
 
 #if defined(OPENTIME_STATIC)
 #   define OPENTIME_API
@@ -41,15 +44,19 @@
 #   define OPENTIME_LOCAL
 #else
 #   if defined(OPENTIME_EXPORTS)
-#       define OPENTIME_API ARCH_EXPORT
-#       define OPENTIME_API_TYPE ARCH_EXPORT_TYPE
-#       define OPENTIME_API_TEMPLATE_CLASS(...) ARCH_EXPORT_TEMPLATE(class, __VA_ARGS__)
-#       define OPENTIME_API_TEMPLATE_STRUCT(...) ARCH_EXPORT_TEMPLATE(struct, __VA_ARGS__)
+#       define OPENTIME_API OPENTIMELINIO_EXPORT
+#       define OPENTIME_API_TYPE OPENTIMELINIO_EXPORT_TYPE
+#       define OPENTIME_API_TEMPLATE_CLASS(...) \
+            OPENTIMELINIO_EXPORT_TEMPLATE(class, __VA_ARGS__)
+#       define OPENTIME_API_TEMPLATE_STRUCT(...) \
+            OPENTIMELINIO_EXPORT_TEMPLATE(struct, __VA_ARGS__)
 #   else
-#       define OPENTIME_API ARCH_IMPORT
-#       define OPENTIME_API_TYPE ARCH_IMPORT_TYPE
-#       define OPENTIME_API_TEMPLATE_CLASS(...) ARCH_IMPORT_TEMPLATE(class, __VA_ARGS__)
-#       define OPENTIME_API_TEMPLATE_STRUCT(...) ARCH_IMPORT_TEMPLATE(struct, __VA_ARGS__)
+#       define OPENTIME_API OPENTIMELINIO_IMPORT
+#       define OPENTIME_API_TYPE OPENTIMELINIO_IMPORT_TYPE
+#       define OPENTIME_API_TEMPLATE_CLASS(...) \
+            OPENTIMELINIO_IMPORT_TEMPLATE(class, __VA_ARGS__)
+#       define OPENTIME_API_TEMPLATE_STRUCT(...) \
+            OPENTIMELINIO_IMPORT_TEMPLATE(struct, __VA_ARGS__)
 #   endif
-#   define OPENTIME_LOCAL ARCH_HIDDEN
+#   define OPENTIME_LOCAL OPENTIMELINIO_HIDDEN
 #endif
--- src/opentimelineio/export.h
+++ src/opentimelineio/export.h
@@ -13,15 +13,19 @@
 #   define OTIO_LOCAL
 #else
 #   if defined(OTIO_EXPORTS)
-#       define OTIO_API ARCH_EXPORT
-#       define OTIO_API_TYPE ARCH_EXPORT_TYPE
-#       define OTIO_API_TEMPLATE_CLASS(...) ARCH_EXPORT_TEMPLATE(class, __VA_ARGS__)
-#       define OTIO_API_TEMPLATE_STRUCT(...) ARCH_EXPORT_TEMPLATE(struct, __VA_ARGS__)
+#       define OTIO_API OPENTIMELINIO_EXPORT
+#       define OTIO_API_TYPE OPENTIMELINIO_EXPORT_TYPE
+#        define OTIO_API_TEMPLATE_CLASS(...) \
+            OPENTIMELINIO_EXPORT_TEMPLATE(class, __VA_ARGS__)
+#        define OTIO_API_TEMPLATE_STRUCT(...) \
+            OPENTIMELINIO_EXPORT_TEMPLATE(struct, __VA_ARGS__)
 #   else
-#       define OTIO_API ARCH_IMPORT
-#       define OTIO_API_TYPE ARCH_IMPORT_TYPE
-#       define OTIO_API_TEMPLATE_CLASS(...) ARCH_IMPORT_TEMPLATE(class, __VA_ARGS__)
-#       define OTIO_API_TEMPLATE_STRUCT(...) ARCH_IMPORT_TEMPLATE(struct, __VA_ARGS__)
+#       define OTIO_API OPENTIMELINIO_IMPORT
+#       define OTIO_API_TYPE OPENTIMELINIO_IMPORT_TYPE
+#       define OTIO_API_TEMPLATE_CLASS(...) \
+            OPENTIMELINIO_IMPORT_TEMPLATE(class, __VA_ARGS__)
+#       define OTIO_API_TEMPLATE_STRUCT(...) \
+            OPENTIMELINIO_IMPORT_TEMPLATE(struct, __VA_ARGS__)
 #   endif
-#   define OTIO_LOCAL ARCH_HIDDEN
+#   define OTIO_LOCAL OPENTIMELINIO_HIDDEN
 #endif

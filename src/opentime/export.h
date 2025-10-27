c34086a14125a8ca5be61e58bac955bc2a9e7d5a
Darby Johnston <darbyjohnston@yahoo.com>
2025-10-27 11:13:14 -0700

    Fix typo
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- src/opentime/export.h
+++ src/opentime/export.h
@@ -7,38 +7,38 @@
 // https://github.com/PixarAnimationStudios/OpenUSD/blob/dev/pxr/base/arch/export.h
 #if defined(_WINDOWS)
 #   if defined(__GNUC__) && __GNUC__ >= 4 || defined(__clang__)
-#       define OPENTIMELINIO_EXPORT __attribute__((dllexport))
-#       define OPENTIMELINIO_IMPORT __attribute__((dllimport))
-#       define OPENTIMELINIO_HIDDEN
-#       define OPENTIMELINIO_EXPORT_TYPE
-#       define OPENTIMELINIO_IMPORT_TYPE
+#       define OPENTIMELINEIO_EXPORT __attribute__((dllexport))
+#       define OPENTIMELINEIO_IMPORT __attribute__((dllimport))
+#       define OPENTIMELINEIO_HIDDEN
+#       define OPENTIMELINEIO_EXPORT_TYPE
+#       define OPENTIMELINEIO_IMPORT_TYPE
 #   else
-#       define OPENTIMELINIO_EXPORT __declspec(dllexport)
-#       define OPENTIMELINIO_IMPORT __declspec(dllimport)
-#       define OPENTIMELINIO_HIDDEN
-#       define OPENTIMELINIO_EXPORT_TYPE
-#       define OPENTIMELINIO_IMPORT_TYPE
+#       define OPENTIMELINEIO_EXPORT __declspec(dllexport)
+#       define OPENTIMELINEIO_IMPORT __declspec(dllimport)
+#       define OPENTIMELINEIO_HIDDEN
+#       define OPENTIMELINEIO_EXPORT_TYPE
+#       define OPENTIMELINEIO_IMPORT_TYPE
 #   endif
 #elif defined(__GNUC__) && __GNUC__ >= 4 || defined(__clang__)
-#   define OPENTIMELINIO_EXPORT __attribute__((visibility("default")))
-#   define OPENTIMELINIO_IMPORT
-#   define OPENTIMELINIO_HIDDEN __attribute__((visibility("hidden")))
+#   define OPENTIMELINEIO_EXPORT __attribute__((visibility("default")))
+#   define OPENTIMELINEIO_IMPORT
+#   define OPENTIMELINEIO_HIDDEN __attribute__((visibility("hidden")))
 #   if defined(__clang__)
-#       define OPENTIMELINIO_EXPORT_TYPE __attribute__((type_visibility("default")))
+#       define OPENTIMELINEIO_EXPORT_TYPE __attribute__((type_visibility("default")))
 #   else
-#       define OPENTIMELINIO_EXPORT_TYPE __attribute__((visibility("default")))
+#       define OPENTIMELINEIO_EXPORT_TYPE __attribute__((visibility("default")))
 #   endif
-#   define OPENTIMELINIO_IMPORT_TYPE
+#   define OPENTIMELINEIO_IMPORT_TYPE
 #else
-#   define OPENTIMELINIO_EXPORT
-#   define OPENTIMELINIO_IMPORT
-#   define OPENTIMELINIO_HIDDEN
-#   define OPENTIMELINIO_EXPORT_TYPE
-#   define OPENTIMELINIO_IMPORT_TYPE
+#   define OPENTIMELINEIO_EXPORT
+#   define OPENTIMELINEIO_IMPORT
+#   define OPENTIMELINEIO_HIDDEN
+#   define OPENTIMELINEIO_EXPORT_TYPE
+#   define OPENTIMELINEIO_IMPORT_TYPE
 #endif
-#define OPENTIMELINIO_EXPORT_TEMPLATE(type, ...)
-#define OPENTIMELINIO_IMPORT_TEMPLATE(type, ...) \
-    extern template type OPENTIMELINIO_IMPORT __VA_ARGS__
+#define OPENTIMELINEIO_EXPORT_TEMPLATE(type, ...)
+#define OPENTIMELINEIO_IMPORT_TEMPLATE(type, ...) \
+    extern template type OPENTIMELINEIO_IMPORT __VA_ARGS__
 
 #if defined(OPENTIME_STATIC)
 #   define OPENTIME_API
@@ -48,19 +48,19 @@
 #   define OPENTIME_LOCAL
 #else
 #   if defined(OPENTIME_EXPORTS)
-#       define OPENTIME_API OPENTIMELINIO_EXPORT
-#       define OPENTIME_API_TYPE OPENTIMELINIO_EXPORT_TYPE
+#       define OPENTIME_API OPENTIMELINEIO_EXPORT
+#       define OPENTIME_API_TYPE OPENTIMELINEIO_EXPORT_TYPE
 #       define OPENTIME_API_TEMPLATE_CLASS(...) \
-            OPENTIMELINIO_EXPORT_TEMPLATE(class, __VA_ARGS__)
+            OPENTIMELINEIO_EXPORT_TEMPLATE(class, __VA_ARGS__)
 #       define OPENTIME_API_TEMPLATE_STRUCT(...) \
-            OPENTIMELINIO_EXPORT_TEMPLATE(struct, __VA_ARGS__)
+            OPENTIMELINEIO_EXPORT_TEMPLATE(struct, __VA_ARGS__)
 #   else
-#       define OPENTIME_API OPENTIMELINIO_IMPORT
-#       define OPENTIME_API_TYPE OPENTIMELINIO_IMPORT_TYPE
+#       define OPENTIME_API OPENTIMELINEIO_IMPORT
+#       define OPENTIME_API_TYPE OPENTIMELINEIO_IMPORT_TYPE
 #       define OPENTIME_API_TEMPLATE_CLASS(...) \
-            OPENTIMELINIO_IMPORT_TEMPLATE(class, __VA_ARGS__)
+            OPENTIMELINEIO_IMPORT_TEMPLATE(class, __VA_ARGS__)
 #       define OPENTIME_API_TEMPLATE_STRUCT(...) \
-            OPENTIMELINIO_IMPORT_TEMPLATE(struct, __VA_ARGS__)
+            OPENTIMELINEIO_IMPORT_TEMPLATE(struct, __VA_ARGS__)
 #   endif
-#   define OPENTIME_LOCAL OPENTIMELINIO_HIDDEN
+#   define OPENTIME_LOCAL OPENTIMELINEIO_HIDDEN
 #endif

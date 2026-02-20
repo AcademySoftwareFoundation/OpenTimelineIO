7c23875254b315ed77cdf015e3def5d3b8d4811c
Darby Johnston <darbyjohnston@yahoo.com>
2026-02-20 15:02:20 -0800

    Move the version number into the source code
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

---
+++ src/opentime/version.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Contributors to the OpenTimelineIO project
+
+#pragma once
+
+#define OPENTIME_VERSION_MAJOR 0
+#define OPENTIME_VERSION_MINOR 19
+#define OPENTIME_VERSION_PATCH 0
+#define OPENTIME_VERSION v0_19_0
+#define OPENTIME_VERSION_NS v0_19
+
+namespace opentime {
+namespace OPENTIME_VERSION_NS {
+}
+
+using namespace OPENTIME_VERSION_NS;
+} // namespace opentime

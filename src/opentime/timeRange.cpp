db96b64942939f57a66e0036f6c76277012a78ee
Darby Johnston <darbyjohnston@yahoo.com>
2026-03-29 09:35:21 -0700

    Fix DEFAULT_EPSILON_s visiblity
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- src/opentime/timeRange.cpp
+++ src/opentime/timeRange.cpp
@@ -2,3 +2,9 @@
 // Copyright Contributors to the OpenTimelineIO project
 
 #include "opentime/timeRange.h"
+
+namespace opentime { namespace OPENTIME_VERSION_NS {
+
+const double DEFAULT_EPSILON_s = 1.0 / (2 * 192000.0);
+
+}} // namespace opentime::OPENTIME_VERSION_NS

507527d8c6dc7d63d5d9d93ef478bb3872e10aa0
Darby Johnston <darbyjohnston@yahoo.com>
2026-05-08 11:32:22 -0700

    Check result
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- tests/test_otioz.py
+++ tests/test_otioz.py
@@ -17,23 +17,29 @@ import opentimelineio.test_utils as otio_test_utils
 class OTIOZTester(unittest.TestCase, otio_test_utils.OTIOAssertions):
     def test_round_trip(self):
         with tempfile.TemporaryDirectory() as temp_dir:
+
+            # Create a timeline
             tl = otio.schema.Timeline()
             tr = otio.schema.Track()
             tl.tracks.append(tr)
             cl = otio.schema.Clip()
             tr.append(cl)
+
+            # Add a media reference
             ref = otio.schema.ExternalReference("video.mov")
             cl.media_reference = ref
-
             pathlib.Path(os.path.join(temp_dir, ref.target_url)).touch()
 
+            # Write to otioz
             otioz_path = os.path.join(temp_dir, "round_trip.otioz")
             otio.adapters.write_to_file(
                 tl,
                 otioz_path,
                 relative_media_path=temp_dir)
 
-            otio.adapters.read_from_file(otioz_path)
+            # Read from otiod
+            result = otio.adapters.read_from_file(otioz_path)
+            self.assertIsNotNone(result)
 
 
 if __name__ == "__main__":
